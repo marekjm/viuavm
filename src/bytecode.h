2c4c9a4e7a32eb886cc268a14b20a01e646e0baa
Marek Marecki <marekjm at ozro dot pw>
2014-12-17 21:16:59 +0100

    Updated bytecode header

--- src/bytecode.h
+++ src/bytecode.h
@@ -3,8 +3,6 @@
 
 #pragma once
 
-#include <vector>
-
 enum Bytecode {
     ISTORE,
     IADD,
@@ -13,33 +11,6 @@ enum Bytecode {
 
     BRANCH,
     HALT,
-};
-
-
-class Instruction {
-    public:
-        Bytecode which;     // type of instruction
-        void** registers;   // addresses of register operands
-        int* locals;
-        int operands;       // number of operands
-
-        static Instruction instance(Bytecode w, int o);
-
-        Instruction local(int, int);
-
-        Instruction(Bytecode w, int o = 1): which(w), operands(o) {
-            registers = new void*[o];
-            locals = new int[o];
-        }
-};
-
-
-class Program {
-    std::vector<Instruction> instructions;
-
-    public:
-        Program& push(Instruction i);
-        std::vector<Instruction> getInstructionVector();
 };
 
 #endif
