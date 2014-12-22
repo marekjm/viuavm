3a660dc7e0387e5a712d54276a8dfb455f6bce64
Marek Marecki <marekjm at ozro dot pw>
2014-12-22 23:44:17 +0100

    Updated bytecode header

--- src/bytecode.h
+++ src/bytecode.h
@@ -4,34 +4,46 @@
 #pragma once
 
 enum Bytecode {
-    ISTORE,     // ISTORE <register> <number>
-    IADD,       // IADD <register-a> <register-b> <register-result> (a + b)
-    ISUB,       // ISUB <register-a> <register-b> <register-result> (a - b)
-    IMUL,       // IMUL <register-a> <register-b> <register-result> (a * b)
-    IDIV,       // IDIV <register-a> <register-b> <register-result> (a / b)
-    IINC,       // IINC <register-index>    (increment integer at given register)
-    IDEC,       // IDEC <register-index>    (decrement integer at given register)
+    ISTORE,
+    IADD,
+    ISUB,
+    IMUL,
+    IDIV,
+    IINC,
+    IDEC,
 
-    ILT,        // ILT  <reg-a> <reg-b> <reg-result> (a < b)
-    ILTE,       // ILTE <reg-a> <reg-b> <reg-result> (a <= b)
-    IGT,        // IGT  <reg-a> <reg-b> <reg-result> (a > b)
-    IGTE,       // IGTE <reg-a> <reg-b> <reg-result> (a >= b)
-    IEQ,        // IEQ  <reg-a> <reg-b> <reg-result> (a == b)
+    ILT,
+    ILTE,
+    IGT,
+    IGTE,
+    IEQ,
+
+    BSTORE,
+    BADD,
+    BSUB,
+    BINC,
+    BDEC,
+
+    BLT,
+    BLTE,
+    BGT,
+    BGTE,
+    BEQ,
 
     STRSTORE,
     STRADD,
     STREQ,
 
-    PRINT,      // PRINT <register-index>
+    PRINT,
 
-    BRANCH,     // BRANCH <bytecode-index>
-    BRANCHIF,   // BRANCHIF <reg-bool> <bytecode-index-if-true> <bytecode-index-if-false>
+    BRANCH,
+    BRANCHIF,
 
-    RET,        // RET <reg-index>
-    END,        // END  (end, e.g. function call)
+    RET,
+    END,
 
-    PASS,       // "do nothing" instruction
-    HALT,       // HALT
+    PASS,
+    HALT,
 };
 
 #endif
