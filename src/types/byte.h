4f1f8fb8f7929a39793598e205efd77c3d561ab3
Marek Marecki <marekjm at ozro dot pw>
2014-12-22 21:37:28 +0100

    Added header for byte and unsigned byte types

---
+++ src/types/byte.h
@@ -0,0 +1,49 @@
+#ifndef TATANKA_TYPES_BYTE_H
+#define TATANKA_TYPES_BYTE_H
+
+#pragma once
+
+#include <string>
+#include <sstream>
+#include "object.h"
+
+
+class Byte : public Object {
+    char byte_;
+
+    public:
+        std::string type() const {
+            return "Byte";
+        }
+        std::string str() const {
+            std::ostringstream s;
+            s << byte_;
+            return s.str();
+        }
+
+        char& value() { return byte; }
+
+        Byte(char b = 0): byte_(b) {}
+};
+
+
+class UnsignedByte : public Byte {
+    unsigned char ubyte_;
+
+    public:
+        std::string type() const {
+            return "UnsignedByte";
+        }
+        std::string str() const {
+            std::ostringstream s;
+            s << ubyte_;
+            return s.str();
+        }
+
+        unsigned char& value() { return ubyte_; }
+
+        UnsignedByte(unsigned char b = 0): ubyte_(b) {}
+};
+
+
+#endif
