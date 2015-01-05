b52edfd55f471c1ba75a1693aaef30df94639394
Marek Marecki <marekjm@ozro.pw>
2015-01-05 17:30:51 +0100

    Fixed: a type, signed/unsigned comparisons

--- src/support/string.cpp
+++ src/support/string.cpp
@@ -21,10 +21,10 @@ namespace str {
 
     bool isnum(const std::string& s) {
         /*  Returns true if s contains only numerical characters.
-         *  Regex equvallent: `^[0-9]+$`
+         *  Regex equivalent: `^[0-9]+$`
          */
         bool num = false;
-        for (int i = 0; i < s.size(); ++i) {
+        for (unsigned i = 0; i < s.size(); ++i) {
             switch (s[i]) {
                 case '0':
                 case '1':
@@ -56,10 +56,12 @@ namespace str {
         ostringstream part;
         part.str("");
 
-        if (e < 0) { e = (s.size() + e + 1); }
+        unsigned end;
+        if (e < 0) { end = unsigned(s.size() + e + 1); }
+        else { end = unsigned(e); }
 
-        for (; b < s.size() and b < e; ++b) {
-            part << s[b];
+        for (unsigned i = unsigned(b); i < s.size() and i < end; ++i) {
+            part << s[i];
         }
 
         return part.str();
@@ -72,7 +74,7 @@ namespace str {
 
         string str = (ignore_leading_ws ? lstrip(s) : s);
 
-        for (int i = 0; i < str.size(); ++i) {
+        for (unsigned i = 0; i < str.size(); ++i) {
             if (str[i] == *" " or str[i] == *"\t" or str[i] == *"\v" or str[i] == *"\n") break;
             chnk << str[i];
         }
@@ -83,7 +85,7 @@ namespace str {
     string lstrip(const string& s) {
         /*  Removes whitespace from left side of the string.
          */
-        int i = 0;
+        unsigned i = 0;
         while (i < s.size()) {
             if (s[i] == *" " or s[i] == *"\t" or s[i] == *"\v" or s[i] == *"\n") {
                 ++i;
@@ -97,7 +99,7 @@ namespace str {
 
     unsigned lshare(const string& s, const string& w) {
         unsigned share = 0;
-        for (int i = 0; i < s.size() and i < w.size(); ++i) {
+        for (unsigned i = 0; i < s.size() and i < w.size(); ++i) {
             if (s[i] == w[i]) {
                 ++share;
             } else {
@@ -115,7 +117,7 @@ namespace str {
         char closing = '"';
 
         encoded << closing;
-        for (int i = 0; i < s.size(); ++i) {
+        for (unsigned i = 0; i < s.size(); ++i) {
             if (s[i] == closing) { encoded << "\\"; }
             encoded << s[i];
         }
