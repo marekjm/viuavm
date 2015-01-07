24be826518a441f94e64d4975b1bd422809298bd
Marek Marecki <marekjm at ozro dot pw>
2015-01-07 21:51:40 +0100

    Sample program to get absolute value of a number uses named registers

--- sample/asm/abs.asm
+++ sample/asm/abs.asm
@@ -4,20 +4,24 @@
 ;
 ; the purpose of this program is to find an absolute value of an integer
 
+.name: 1 number
+.name: 2 zero
+.name: 3 is_negative
+
 ; store the int, of which we want to get an absolute value
-istore 1 -17
+istore number -17
 
 ; compare the int to zero
-istore 2 0
-ilt 1 2 3
+istore zero 0
+ilt number zero is_negative
 
 ; if the int is less than zero, multiply it by -1
 ; else, branch directly to print instruction
-branch 3 4 :final_print
+branch is_negative 4 :final_print
 istore 4 -1
 imul 1 4
 
 .mark: final_print
-print 1
+print number
 
 halt
