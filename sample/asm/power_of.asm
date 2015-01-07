0248c8db3931db08080e0fe0a35cbbe1508f5ecf
Marek Marecki <marekjm at ozro dot pw>
2015-01-07 22:02:06 +0100

    Updated power_of sample program

--- sample/asm/power_of.asm
+++ sample/asm/power_of.asm
@@ -1,33 +1,46 @@
 ; purpose of this program is to compute n-th power of a number
 
-; in first register we store the base number
-istore 1 4
+.name: 1 base
+.name: 2 exponent
+.name: 3 zero
+.name: 6 result
 
-; in second register we store the power
-istore 2 3
+; store operands of the power-of operation
+istore base 4
+istore exponent 3
 
-; store zero - we need it to compare the power, and as a countr for loop
-istore 3 0
+; store zero - we need it to compare the exponent to it
+istore zero 0
 
-; if the power is equal to zero, store 1 in first register and jump to print
-ieq 2 3 4
+; if the exponent is equal to zero, store 1 in first register and jump to print
+ieq exponent zero 4
+
+; invert so we can use short form of branch instruction
 not 4
-branch 4 8
-istore 6 1
+branch 4 :algorithm
+istore result 1
 jump :final_print
 
-; now, we multiply in loop
-istore 5 1
-; in register 6, store the integer that can be found in register 1
-istore 6 @1
+; now, we multiply in a loop
+.mark: algorithm
+.name: 5 counter
+istore counter 1
+; in register 6, store the base of power as
+; we will need it for multiplication
+istore result @base
+
 .mark: loop
-ilt 5 2 4
+ilt counter exponent 4
 branch 4 12 :final_print
-imul 1 6 6
+; TODO: this imul should use short form
+imul base result result
 pass
-iinc 5
+iinc counter
 jump :loop
 
+; print the result
 .mark: final_print
-print 6
+print result
+
+; and halt the program
 halt
