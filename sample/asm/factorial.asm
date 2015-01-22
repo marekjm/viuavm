91b896341f2f2543013fb8f7e4f19bb05a0ea043
Marek Marecki <marekjm@ozro.pw>
2015-01-22 14:03:32 +0100

    Added sample factorial program

---
+++ sample/asm/factorial.asm
@@ -0,0 +1,23 @@
+.def: factorial 0
+    imul 2 1
+    idec 1
+    ; if counter is equal to zero
+    ieq 1 3 4
+    branch 4 :finish
+    call factorial
+    .mark: finish
+    end
+.end
+
+.def: main 0
+    .name: 1 number
+    istore number 5
+    istore 2 1
+    istore 3 0
+    call factorial
+    print 2
+    end
+.end
+
+call main
+halt
