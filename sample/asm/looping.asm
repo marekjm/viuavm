81212dacd396c7e0bc3772e636a52decd77646e7
Marek Marecki <marekjm@ozro.pw>
2015-01-25 13:07:03 +0100

    Fixed looping.asm sample

--- sample/asm/looping.asm
+++ sample/asm/looping.asm
@@ -1,15 +1,10 @@
 ; This script is just a simple loop.
 ; It displays numbers from 0 to 10.
 
-; TODO: this could cause errors if scopes are implemented for functions as
-; these two variables would not go into local function scope
-; solution: `scope` instruction for scope switching (global/local) and
-; possibly up-scoping - if register at given index is null in local scope,
-; look for it in global registers
-istore 1 0
-istore 2 10
-
 .def: main 0
+    istore 1 0
+    istore 2 10
+
     ; mark the beginning of the loop
     .mark: loop
     ilt 1 2 3
