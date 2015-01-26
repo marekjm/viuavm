8a8ed856c88e337f15cf6d42b47cfd8373d744e7
Marek Marecki <marekjm@ozro.pw>
2015-01-26 23:40:06 +0100

    Fixed recursive function call test

--- tests/tests.py
+++ tests/tests.py
@@ -331,7 +331,7 @@ class FunctionTests(unittest.TestCase):
         self.assertEqual([i for i in range(9, -1, -1)], [int(i) for i in output.strip().splitlines()])
         self.assertEqual(0, excode)
 
-    def testRecursiveCallFunctionSupport(self):
+    def testLocalRegistersSupport(self):
         name = 'local_registers.asm'
         assembly_path = os.path.join(FunctionTests.PATH, name)
         compiled_path = os.path.join(COMPILED_SAMPLES_PATH, (name + '.bin'))
