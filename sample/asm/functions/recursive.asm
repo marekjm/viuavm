f88dffc072bbde9e5da005ca44fa35e2c4e0f73f
Marek Marecki <marekjm@ozro.pw>
2015-01-26 01:52:26 +0100

    Fixed broken test (and made it run in the first place - as two tests were named the same by accident)

--- sample/asm/functions/recursive.asm
+++ sample/asm/functions/recursive.asm
@@ -1,16 +1,19 @@
 .def: rec 0
+    arg 0 1
+    istore 2 0
     idec 1
     ilt 1 2 3
     branch 3 :break_rec
     print 1
-    frame 0
+    frame 1
+    param 0 1
     call rec
     .mark: break_rec
     end
 .end
 
 istore 1 10
-istore 2 0
-frame 0
+frame 1
+param 0 1
 call rec
 halt
