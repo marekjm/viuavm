3feb8a13a9bd75f217df6832b48cec92beaa947a
Marek Marecki <marekjm@ozro.pw>
2015-01-31 01:08:45 +0100

    CPU frontend can read and load function id-to-address mapping

--- src/front/cpu.cpp
+++ src/front/cpu.cpp
@@ -55,9 +55,36 @@ int main(int argc, char* argv[]) {
             return 1;
         }
 
+        uint16_t function_ids_section_size = 0;
+        char buffer[16];
+        in.read(buffer, sizeof(uint16_t));
+        function_ids_section_size = *((uint16_t*)buffer);
+        if (debug) { cout << "debug: function mapping section: " << function_ids_section_size << " bytes" << endl; }
+
+        char *buffer_function_ids = new char[function_ids_section_size];
+        in.read(buffer_function_ids, function_ids_section_size);
+        char *function_ids_map = buffer_function_ids;
+
+        map<string, uint16_t> function_address_mapping;
+
+        int i = 0;
+        string fn_name;
+        uint16_t fn_address;
+        while (i < function_ids_section_size) {
+            fn_name = string(function_ids_map);
+            i += fn_name.size() + 1;  // one for null character
+            fn_address = *((uint16_t*)(buffer_function_ids+i));
+            i += sizeof(uint16_t);
+            function_ids_map = buffer_function_ids+i;
+            function_address_mapping[fn_name] = fn_address;
+
+            if (debug) {
+                cout << "debug: function id-to-address mapping: " << fn_name << " @ byte " << fn_address << endl;
+            }
+        }
+
         uint16_t bytes;
         uint16_t starting_instruction;
-        char buffer[16];
 
         in.read(buffer, 16);
         if (!in) {
@@ -67,6 +94,7 @@ int main(int argc, char* argv[]) {
         } else {
             bytes = *((uint16_t*)buffer);
         }
+        if (debug) { cout << "debug: bytecode size: " << bytes << " bytes" << endl; }
 
         in.read(buffer, 16);
         if (!in) {
@@ -76,6 +104,7 @@ int main(int argc, char* argv[]) {
         } else {
             starting_instruction = *((uint16_t*)buffer);
         }
+        if (debug) { cout << "debug: first executable instruction at byte " << starting_instruction << endl; }
 
         byte* bytecode = new byte[bytes];
         in.read((char*)bytecode, bytes);
@@ -84,14 +113,20 @@ int main(int argc, char* argv[]) {
             cout << "fatal: an error occued during bytecode loading: cannot read instructions" << endl;
             if (str::endswith(filename, ".asm")) { cout << NOTE_LOADED_ASM << endl; }
             return 1;
-        } else {
-            starting_instruction = *((uint16_t*)buffer);
         }
         in.close();
 
         // run the bytecode
         CPU cpu;
         cpu.debug = debug;
+        for (auto p : function_address_mapping) {
+            if (debug) {
+                cout << "debug: ";
+                cout << "mapped function '" << p.first << "' to address " << p.second << " in CPU" << endl;
+            }
+            cpu.mapfunction(p.first, p.second);
+        }
+
         ret_code = cpu.load(bytecode).bytes(bytes).eoffset(starting_instruction).run();
     } else {
         cout << "wudoo VM, version " << VERSION << endl;
