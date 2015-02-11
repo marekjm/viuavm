f195a991aed81c18dc97fdc9ada89e49d983a930
Marek Marecki <marekjm@ozro.pw>
2015-02-11 17:40:41 +0100

    Sample file functions/definition.asm ported to new way of assembling

--- sample/asm/functions/definition.asm
+++ sample/asm/functions/definition.asm
@@ -4,15 +4,10 @@
     end
 .end
 
+; set alternative main function
 .main: foo
 
 ; these instructions are just to prove that offsets work correctly
 istore 1 0
 iinc 1
 ref 1 2
-
-; this call should pring 42 to the console
-frame 0
-call foo
-
-halt
