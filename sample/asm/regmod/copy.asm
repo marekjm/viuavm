b7f240ebb5b7ff74438be8cc271697a8f0488788
Marek Marecki <marekjm@ozro.pw>
2015-02-11 21:40:51 +0100

    Updated samples for copy, move and swap instructions

--- sample/asm/regmod/copy.asm
+++ sample/asm/regmod/copy.asm
@@ -1,10 +1,8 @@
 ; This script checks if the copy instruction works correctly.
 ; The instruction should copy the value of one register into another.
 .def: main 0
+    istore 1 1
+    copy 1 2
+    print 2
     end
 .end
-
-istore 1 1
-copy 1 2
-print 2
-halt
--- sample/asm/regmod/swap.asm
+++ sample/asm/regmod/swap.asm
@@ -1,21 +1,19 @@
 .def: main 0
+    .name: 1 zero
+    .name: 2 one
+
+    ; store zero and one in registers
+    istore zero 0
+    istore one 1
+
+    ; swap objects so that register "zero" now contains 1, and
+    ; register "one" contains 0
+    swap zero one
+
+    ; print 0 and 1
+    ; this should actually print 1 and 0
+    print zero
+    print one
+
     end
 .end
-
-.name: 1 zero
-.name: 2 one
-
-; store zero and one in registers
-istore zero 0
-istore one 1
-
-; swap objects so that register "zero" now contains 1, and
-; register "one" contains 0
-swap zero one
-
-; print 0 and 1
-; this should actually print 1 and 0
-print zero
-print one
-
-halt
