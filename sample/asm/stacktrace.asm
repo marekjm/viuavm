c3503919338be5aab4687d8ac7172edfd81f4a14
Marek Marecki <marekjm@ozro.pw>
2015-02-18 18:43:53 +0100

    Added example stacktrace-throwing program

---
+++ sample/asm/stacktrace.asm
@@ -0,0 +1,23 @@
+.def: foo 0
+    print 1
+    end
+.end
+
+.def: bar 0
+    frame 0
+    call foo
+    end
+.end
+
+.def: baz 0
+    frame 0
+    call bar
+    end
+.end
+
+.def: main 1
+    frame 0
+    call baz 0
+    izero 0
+    end
+.end
