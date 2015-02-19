687c04b2c339326edf5d3f215af78846f778f75f
Marek Marecki <marekjm@ozro.pw>
2015-02-19 18:57:29 +0100

    Better stack traces (details about last frame)

--- src/front/cpu.cpp
+++ src/front/cpu.cpp
@@ -175,6 +175,25 @@ int main(int argc, char* argv[]) {
         }
         cout << "exception in function '" << trace.back()->function_name << "': ";
         cout << return_exception << ": " << return_message << endl;
+
+        Frame* last = trace.back();
+        if (last->registers_size) {
+            cout << "    non-empty registers (out of " << last->registers_size << "):" << endl;
+            for (int r = 0; r < last->registers_size; ++r) {
+                if (last->registers[r] == 0) { continue; }
+                cout << "      registers[" << i << "]: ";
+                cout << '<' << last->registers[r]->type() << "> " << last->registers[r]->str() << endl;
+            }
+        }
+
+        if (last->arguments_size) {
+            cout << "    non-empty arguments (out of " << last->arguments_size << "):" << endl;
+            for (int r = 0; r < last->arguments_size; ++r) {
+                if (last->arguments[r] == 0) { continue; }
+                cout << "      arguments[" << i << "]: ";
+                cout << '<' << last->arguments[r]->type() << "> " << last->arguments[r]->str() << endl;
+            }
+        }
     }
 
     return ret_code;
