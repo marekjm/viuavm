d3a270c7db3c7daac53227a4b3375bce729a9e07
Marek Marecki <marek.marecki@posbit.pl>
2015-02-26 09:02:39 +0100

    Improved sample static register set program

--- sample/asm/static.asm
+++ sample/asm/static.asm
@@ -12,39 +12,31 @@
     branch 2 :increase
 
     ; these two instructions are executed only when 1 register was null
-    istore 1 42
+    istore 1 0
     jump :report
 
     .mark: increase
     iinc 1
-
-    .mark: report
-    print 1
 
     istore 3 10
     ; integer at 1 is less than ten
     ilt 1 3 3
     ; integer at 1 is *at least* ten
     not 3
-    branch 3 
+    branch 3 :finish
+    .mark: report
+    print 1
+    frame 0
+    call counter 0
+
+    .mark: finish
     end
 .end
 
 .def: main 1
-    istore 1 0
-    istore 2 10
-
-    .mark: loop
-    ilt 1 2 3
-    not 3
-
-    branch 3 :finish
     frame 0
     call counter 0
-    iinc 1
-    jump :loop
 
-    .mark: finish
     izero 0
     end
 .end
