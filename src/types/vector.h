665e8baa9805b9d06534a27ffaf711ca66a0021f
Marek Marecki <marekjm@ozro.pw>
2015-03-06 14:52:26 +0100

    Add: vector type

---
+++ src/types/vector.h
@@ -0,0 +1,49 @@
+#ifndef WUDOO_TYPE_VECTOR_H
+#define WUDOO_TYPE_VECTOR_H
+
+#pragma once
+
+#include <string>
+#include <vector>
+#include <stringstream>
+#include "object.h"
+
+
+class Vector : public Object {
+    /** Vector type.
+     */
+    std::vector<Object*> internal_object;
+
+    public:
+        std::string type() const {
+            return "Vector";
+        }
+        std::string str() const {
+            std::ostringstream oss;
+            oss << "[";
+            for (unsigned i = 0; i < internal_object.size(); ++i) {
+                oss << internal_object[i]->str() << (i < internal_object.size()-1 ? ", " : "");
+            }
+            oss << "]";
+            return oss.str();
+        }
+        bool boolean() const {
+            return internal_object.size() != 0;
+        }
+
+        Object* copy() const {
+            return new Vector();
+        }
+
+        std::vector<Object*>& value() { return internal_object; }
+
+        Vector(): {}
+        Vector(const std::vector<Object*>& v): {
+            for (unsigned i = 0; i < v.size(); ++i) {
+                internal_object.push_back(v[i]->copy());
+            }
+        }
+};
+
+
+#endif
