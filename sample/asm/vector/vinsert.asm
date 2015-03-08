8b9403b80042108475b24ffbb69c8fe9d5fc8be2
Marek Marecki <marekjm@ozro.pw>
2015-03-08 21:53:17 +0100

    Updated vinsert sample
    It now correctly handles VATed objects, i.e. empties their registers
    before reassigning them.

--- sample/asm/vector/vinsert.asm
+++ sample/asm/vector/vinsert.asm
@@ -16,6 +16,9 @@
     vinsert 1 durr 1
     vinsert 1 ima 2
 
+    ; this works OK
+    ; this instruction is here for debugging - uncomment it to see the vector
+    ;print 1
 
     .name: 6 len
     .name: 7 counter
@@ -29,10 +32,17 @@
     .mark: inside
     vat 1 9 @counter
     print 9
+    ; empty must be done or second print would fail with a segfault!
+    ; VATed registers are references!
+    empty 9
     iinc counter
     jump :loop
 
     .mark: break
+
+    ; see comment for first vector-print
+    ; it is for debugging - this should work without segfault
+    ;print 1
 
     izero 0
     end
