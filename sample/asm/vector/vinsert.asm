eb7eee3fd2844def14a49c06332703839eb0b968
Marek Marecki <marekjm@ozro.pw>
2015-03-08 01:47:41 +0100

    Updated sample vinsert program

--- sample/asm/vector/vinsert.asm
+++ sample/asm/vector/vinsert.asm
@@ -4,22 +4,17 @@
     .name: 4 ima
     .name: 5 sheep
 
-    ;strstore hurr "Hurr"
-    ;strstore durr "durr"
-    ;strstore ima "Ima"
-    ;strstore sheep "sheep"
-
-    istore hurr 1
-    istore durr 2
-    istore ima 3
-    istore sheep 4
+    strstore hurr "Hurr"
+    strstore durr "durr"
+    strstore ima "Im'a"
+    strstore sheep "sheep!"
 
     vec 1
 
-    vpush 1 sheep
-    vpush 1 sheep
-    vpush 1 sheep
-    vpush 1 sheep
+    vinsert 1 sheep
+    vinsert 1 hurr
+    vinsert 1 durr 1
+    vinsert 1 ima 2
 
 
     .name: 6 len
