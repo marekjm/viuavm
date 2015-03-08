5d5e0cff7cef1a36685375338adb5f97539feccb
Marek Marecki <marekjm@ozro.pw>
2015-03-08 01:07:46 +0100

    Implemented VINSERT instruction

--- src/types/vector.cpp
+++ src/types/vector.cpp
@@ -1,19 +1,25 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <stdexcept>
 #include "object.h"
 #include "vector.h"
 using namespace std;
 
 
-Object* Vector::insert(int pos, Object* object) {
-    // FIXME: implement
-    return this;
+Object* Vector::insert(int index, Object* object) {
+    if (index < 0) { index = (internal_object.size()+index); }
+    if ((index < 0) or (index >= (int)internal_object.size())) {
+        throw std::out_of_range("vector index out of range");
+    }
+    vector<Object*>::iterator it = (internal_object.begin()+index);
+    internal_object.insert(it, object);
+    return object;
 }
 
 Object* Vector::push(Object* object) {
     internal_object.push_back(object);
-    return this;
+    return object;
 }
 
 Object* Vector::pop(int index) {
