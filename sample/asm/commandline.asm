2f4dde42785ad2d83522d278d67b7c6040041a8e
Marek Marecki <marekjm@ozro.pw>
2015-03-09 02:38:25 +0100

    Add: commandline sample program

---
+++ sample/asm/commandline.asm
@@ -0,0 +1,24 @@
+.def: main 1
+    arg 0 1
+
+    .name: 2 len
+    .name: 3 counter
+
+    vlen 1 len
+    istore counter 0
+
+    .mark: loop
+    ilt counter len 4
+    branch 4 :inside :break
+    .mark: inside
+    vat 1 5 @counter
+    print 5
+    empty 5
+    iinc counter
+    jump :loop
+
+    .mark: break
+
+    izero 0
+    end
+.end
