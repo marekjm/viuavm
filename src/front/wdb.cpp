ef4b3d73ce90cc37bbf26cc491a5ee09a7ed0e9b
Marek Marecki <marekjm@ozro.pw>
2015-03-12 19:35:56 +0100

    First, crude debugger

---
+++ src/front/wdb.cpp
@@ -0,0 +1,281 @@
+#include <cstdlib>
+#include <cstdint>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "../version.h"
+#include "../support/string.h"
+#include "../cpu/debugger.h"
+#include "../program.h"
+using namespace std;
+
+
+const char* NOTE_LOADED_ASM = "note: seems like you have loaded an .asm file which cannot be run on CPU without prior compilation";
+
+
+// MISC FLAGS
+bool SHOW_HELP = false;
+bool SHOW_VERSION = false;
+bool VERBOSE = false;
+bool DEBUG = false;
+bool STEP_BY_STEP = false;
+
+
+// WARNING FLAGS
+bool WARNING_ALL = false;
+
+
+// ERROR FLAGS
+bool ERROR_ALL = false;
+
+
+int main(int argc, char* argv[]) {
+    // setup command line arguments vector
+    vector<string> args;
+    string option;
+    for (int i = 1; i < argc; ++i) {
+        option = string(argv[i]);
+        if (option == "--help") {
+            SHOW_HELP = true;
+            continue;
+        } else if (option == "--version") {
+            SHOW_VERSION = true;
+            continue;
+        } else if (option == "--verbose") {
+            VERBOSE = true;
+            continue;
+        } else if (option == "--debug") {
+            DEBUG = true;
+            continue;
+        } else if (option == "--Wall") {
+            WARNING_ALL = true;
+            continue;
+        } else if (option == "--Eall") {
+            ERROR_ALL = true;
+            continue;
+        } else if (option == "--step") {
+            STEP_BY_STEP = true;
+            continue;
+        }
+        args.push_back(argv[i]);
+    }
+
+    if (SHOW_HELP or SHOW_VERSION) {
+        cout << "wudoo VM virtual machine, version " << VERSION << endl;
+        if (SHOW_HELP) {
+            cout << "    --analyze          - to display information about loaded bytecode but not run it" << endl;
+            cout << "    --debug <infile>   - to run a program in debug mode (shows debug output)" << endl;
+            cout << "    --help             - to display this message" << endl;
+            cout << "    --step <infile>    - to run a program in stepping mode (pauses after each instruction, implies debug mode for CPU)" << endl;
+        }
+        return 0;
+    }
+
+    if (args.size() == 0) {
+        cout << "fatal: no input file" << endl;
+        return 1;
+    }
+
+    string filename = "";
+    filename = args[0];
+
+    if (!filename.size()) {
+        cout << "fatal: no file to run" << endl;
+        return 1;
+    }
+
+    cout << "message: running \"" << filename << "\"" << endl;
+
+    ifstream in(filename, ios::in | ios::binary);
+
+    if (!in) {
+        cout << "fatal: file could not be opened: " << filename << endl;
+        return 1;
+    }
+
+    uint16_t function_ids_section_size = 0;
+    char buffer[16];
+    in.read(buffer, sizeof(uint16_t));
+    function_ids_section_size = *((uint16_t*)buffer);
+    if (VERBOSE or DEBUG) { cout << "message: function mapping section: " << function_ids_section_size << " bytes" << endl; }
+
+    /*  The code below extracts function id-to-address mapping.
+     */
+    map<string, uint16_t> function_address_mapping;
+    char *buffer_function_ids = new char[function_ids_section_size];
+    in.read(buffer_function_ids, function_ids_section_size);
+    char *function_ids_map = buffer_function_ids;
+
+    int i = 0;
+    string fn_name;
+    uint16_t fn_address;
+    while (i < function_ids_section_size) {
+        fn_name = string(function_ids_map);
+        i += fn_name.size() + 1;  // one for null character
+        fn_address = *((uint16_t*)(buffer_function_ids+i));
+        i += sizeof(uint16_t);
+        function_ids_map = buffer_function_ids+i;
+        function_address_mapping[fn_name] = fn_address;
+
+        cout << "debug: function id-to-address mapping: " << fn_name << " @ byte " << fn_address << endl;
+    }
+    delete[] buffer_function_ids;
+
+
+    uint16_t bytes;
+
+    in.read(buffer, 16);
+    if (!in) {
+        cout << "fatal: an error occued during bytecode loading: cannot read size" << endl;
+        if (str::endswith(filename, ".asm")) { cout << NOTE_LOADED_ASM << endl; }
+        return 1;
+    } else {
+        bytes = *((uint16_t*)buffer);
+    }
+    cout << "message: bytecode size: " << bytes << " bytes" << endl;
+
+    uint16_t starting_instruction = function_address_mapping["__entry"];
+    cout << "message: first executable instruction at byte " << starting_instruction << endl;
+
+    byte* bytecode = new byte[bytes];
+    in.read((char*)bytecode, bytes);
+
+    if (!in) {
+        cout << "fatal: an error occued during bytecode loading: cannot read instructions" << endl;
+        if (str::endswith(filename, ".asm")) { cout << NOTE_LOADED_ASM << endl; }
+        return 1;
+    }
+    in.close();
+
+    int ret_code = 0;
+    string return_exception = "", return_message = "";
+    // run the bytecode
+    CPU cpu;
+    cpu.debug = true;
+    cpu.stepping = true;
+    for (auto p : function_address_mapping) { cpu.mapfunction(p.first, p.second); }
+
+    vector<string> cmdline_args;
+    for (int i = 1; i < argc; ++i) {
+        cmdline_args.push_back(argv[i]);
+    }
+
+    cpu.commandline_arguments = cmdline_args;
+
+    cpu.load(bytecode).bytes(bytes).eoffset(starting_instruction);
+
+    string HELP(
+        "SYNTAX:\n"
+        "\n"
+        "  Commands are written as follows: `:<command>[operand]`.\n"
+        "  If operand is enclosed in <triangular> braces it is mandatory.\n"
+        "  If operand is enclosed in [square] braces it is optional.\n"
+        "\n"
+        "  Commands cannot be chained, i.e. `:t18s` does not mean \"execute 18 instructions and"
+        "  print stack trace\" - it is an error.\n"
+        "\n"
+        "COMMANDS:\n"
+        "\n"
+        "  help                 - display help\n"
+        "  quit                 - quit debugger\n"
+        "  conf <key> <value>   - set configuration variable (for the run)\n"
+        "  :i                   - create initial frame\n"
+        "  :b                   - set instruction counter to 0 and go to the beginning of the bytecode\n"
+        "  :p                   - prepare CPU to run (alias for :i<CR>:b)\n"
+        "  :t[n]                - execute [n] instructions\n"
+        "  :s                   - print stack trace\n"
+        "  :f                   - print last frame on stack trace\n"
+        "  :u                   - print current registers\n"
+        "  :j<n>                - jump to <n> bytecode\n"
+        "  .                    - repeat last command\n"
+    );
+
+    string command(""), lastcommand("");
+    while (true) {
+        cout << ">>> ";
+        getline(cin, command);
+
+        command = str::lstrip(command);
+
+        if (command == ".") {
+            command = lastcommand;
+        }
+
+        if (command == "") {
+            // do nothing
+            continue;
+        } else if (str::startswith(command, ":i")) {
+            cpu.iframe();
+        } else if (str::startswith(command, ":b")) {
+            cpu.instruction_counter = 0;
+            cpu.begin();
+        } else if (str::startswith(command, ":p")) {
+            cpu.iframe();
+            cpu.begin();
+        } else if (str::startswith(command, ":t")) {
+            int n = 1;
+            if (str::isnum(str::chunk(str::sub(command, 2)))) {
+                n = stoi(str::chunk(str::sub(command, 2)));
+            }
+            while ((n--) > 0) {
+                if (cpu.tick() == 0) {
+                    break;
+                }
+            }
+        } else if (str::startswith(command, ":s")) {
+            vector<Frame*> trace = cpu.trace();
+            for (unsigned i = 1; i < trace.size(); ++i) {
+                cout << "  " << trace[i]->function_name << "\n";
+            }
+        } else if (str::startswith(command, ":f")) {
+            Frame* frame = cpu.trace().back();
+
+            for (int r = 0; r < frame->registers_size; ++r) {
+                if (frame->registers[r] == 0) { continue; }
+                cout << "    registers[" << r << "]: ";
+                cout << '<' << frame->registers[r]->type() << "> " << frame->registers[r]->str() << endl;
+            }
+            cout << endl;
+
+            for (int r = 0; r < frame->arguments_size; ++r) {
+                if (frame->arguments[r] == 0) { continue; }
+                cout << "    arguments[" << r << "]: ";
+                cout << '<' << frame->arguments[r]->type() << "> " << frame->arguments[r]->str() << endl;
+            }
+        } else if (str::startswith(command, ":u")) {
+            for (int r = 0; r < cpu.uregisters_size; ++r) {
+                if (cpu.uregisters[r] == 0) { continue; }
+                cout << "    uregisters[" << r << "]: ";
+                cout << '<' << cpu.uregisters[r]->type() << "> " << cpu.uregisters[r]->str() << endl;
+            }
+        } else if (str::startswith(command, ":j")) {
+            int n = 1;
+            if (str::isnum(str::chunk(str::sub(command, 2)))) {
+                n = stoi(str::chunk(str::sub(command, 2)));
+            } else {
+                cout << "invalid syntax" << endl;
+                continue;
+            }
+            cpu.instruction_pointer = (cpu.bytecode+n);
+        } else if (str::startswith(command, ":c")) {
+            cout << cpu.counter() << endl;
+        } else if (str::startswith(command, "conf")) {
+            command = str::sub(command, 4);
+            string key = str::chunk(command);
+            string value = str::lstrip(str::sub(str::lstrip(command), key.size()));
+            cout << key << " = '" << value << "'" << endl;
+        } else if (command == "help") {
+            cout << HELP << endl;
+        } else if (command == "quit") {
+            break;
+        } else {
+            cout << "unknown command: " << command << endl;
+            continue;
+        }
+
+        lastcommand = command;
+    }
+
+    return ret_code;
+}
