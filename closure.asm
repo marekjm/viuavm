475fef55e5cf1a85a1827e425aea53b05d2e6d40
Marek Marecki <marekjm@ozro.pw>
2015-03-30 22:07:38 +0200

    New code for closure (still leaking memory!)

--- closure.asm
+++ closure.asm
@@ -1,29 +1,43 @@
-.def: print_args_closure 0
+.def: foo 0
+    ; one is bound from main
     print 1
     end
 .end
 
-.def: print_args 1
-    arg 0 1
-    izero 0
+.def: returns_closure 1
+    istore 1 42
+    closure foo 2
+    move 2 0
     end
 .end
 
 .def: main 0
-    ; get the vector with command line operands
-    arg 0 1
-
     ; call function that returns the closure
-    frame 1
-    param 0 1
-    call print_args 2
+    frame 0
+    call returns_closure 1
 
     ; create frame for our closure and
     ; call it
-    closure print_args_closure 3
     clframe 0
-    clcall 3 0
+    clcall 1 0
 
     izero 0
     end
 .end
+
+
+; // equivallent JavaScript code
+;
+; function returns_closure() {
+;     var x = 42;
+;     function foo() {
+;         print(x);
+;     }
+;     return foo;
+; }
+;
+; function main() {
+;     var bar = returns_closure();
+;     bar();
+;     return 0;
+; }
