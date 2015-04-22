85cccdc88f8eaa9abda011416c2415e4cdfe916b
Marek Marecki <marekjm@ozro.pw>
2015-04-22 21:46:01 +0200

    First (untested) implementation of excall instruction

--- src/cpu/instr/general.cpp
+++ src/cpu/instr/general.cpp
@@ -503,6 +503,74 @@ byte* CPU::jump(byte* addr) {
     return target;
 }
 
+
+byte* CPU::excall(byte* addr) {
+    /** Run excall instruction.
+     */
+    string call_name = string(addr);
+    byte* call_address = bytecode+function_addresses.at(call_name);
+    addr += call_name.size();
+
+    // save return address for frame
+    byte* return_address = (addr + sizeof(bool) + sizeof(int));
+
+    if (frame_new == 0) {
+        throw "external function call without a frame: use `frame 0' in source code if the function takes no parameters";
+    }
+    // set function name and return address
+    frame_new->function_name = call_name;
+    frame_new->return_address = return_address;
+
+    frame_new->resolve_return_value_register = *(bool*)addr;
+    pointer::inc<bool, byte>(addr);
+    frame_new->place_return_value_in = *(int*)addr;
+
+    pushFrame();
+
+    if (external_functions.count(call_name) == 0) {
+        throw ("call to unregistered external function: " + call_name);
+    }
+
+    /* FIXME: second parameter should be a pointer to static registers or
+     *        0 if function does not have static registers registered
+     * FIXME: should external functions always have static registers allocated?
+     */
+    Object* returned = 0;
+    returned = (*external_functions.at(call_name))(frames.end(), 0, regset);
+
+    bool returned_is_reference = false;
+    int return_value_register = frames.back()->place_return_value_in;
+    bool resolve_return_value_register = frames.back()->resolve_return_value_register;
+    if (return_value_register != 0) {
+        // we check in 0. register because it's reserved for return values
+        if (uregset->at(0) == 0) {
+            throw "return value requested by frame but external function did not set return register";
+        }
+        if (uregset->isflagged(0, REFERENCE)) {
+            returned = uregset->get(0);
+            returned_is_reference = true;
+        } else {
+            returned = uregset->get(0)->copy();
+        }
+    }
+
+    dropFrame();
+
+    // place return value
+    if (returned and frames.size() > 0) {
+        if (resolve_return_value_register) {
+            return_value_register = static_cast<Integer*>(fetch(return_value_register))->value();
+        }
+        place(return_value_register, returned);
+        if (returned_is_reference) {
+            uregset->flag(return_value_register, REFERENCE);
+        }
+    }
+
+    return return_address;
+}
+
+
 byte* CPU::branch(byte* addr) {
     /*  Run branch instruction.
      */
