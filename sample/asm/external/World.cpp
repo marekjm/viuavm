c25b85004c50eb31bfaca0c28225daff3730baec
Marek Marecki <marekjm@ozro.pw>
2015-04-26 00:01:04 +0200

    Add missing external import sample files

---
+++ sample/asm/external/World.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "../../../src/types/object.h"
+#include "../../../src/cpu/frame.h"
+#include "../../../src/cpu/registerset.h"
+#include "../../../src/exmodule.h"
+using namespace std;
+
+
+Object* hello(Frame*, RegisterSet*, RegisterSet*) {
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
+
+const char* function_names[] = {
+    "print_hello",
+    NULL,
+};
+const ExternalFunction* function_pointers[] = {
+    &hello,
+    NULL,
+};
+
+
+extern "C" const char** exports_names() {
+    return function_names;
+}
+extern "C" const ExternalFunction** exports_pointers() {
+    return function_pointers;
+}
---
+++ sample/asm/external/hello_world.asm
@@ -0,0 +1,9 @@
+.def: main 1
+    eximport "World"
+
+    frame 0
+    excall World.print_hello 0
+
+    izero 0
+    end
+.end
