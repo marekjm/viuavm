408840fb3805d1fc42aa037de482800bf11966d9
Marek Marecki <marekjm@ozro.pw>
2015-05-15 21:50:56 +0200

    Add stringifyFunctionInvocation() function

---
+++ src/printutils.cpp
@@ -0,0 +1,21 @@
+#include <sstream>
+#include <string>
+#include "types/object.h"
+#include "cpu/frame.h"
+using namespace std;
+
+string stringifyFunctionInvocation(const Frame* frame) {
+    ostringstream oss;
+    oss << frame->function_name << '/' << frame->args->size();
+    oss << '(';
+    for (unsigned i = 0; i < frame->args->size(); ++i) {
+        Object* optr = frame->args->at(i);
+        if (optr == 0) { continue; }
+        oss << optr->repr();
+        if (i < (frame->args->size()-1)) {
+            oss << ", ";
+        }
+    }
+    oss << ')';
+    return oss.str();
+}
