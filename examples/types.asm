d10f3b43d15b5b33cbe6736d36338560027f9bbd
Marek Marecki <marekjm@ozro.pw>
2015-06-02 23:12:01 +0200

    Rewrite of the example typesystem.so-using program

--- examples/types.asm
+++ examples/types.asm
@@ -1,52 +1,68 @@
-.block: handle_Exception
-    strstore 2 "cannot obtain real type of object, assuming: Object"
-    print 2
-    strstore 2 "Object"
-    leave
-.end
-
-.block: handle_LinkException
-    strstore 2 "failed to link 'typesystem' module, assuming object type is 'Object'"
-    print 2
-    strstore 2 "Object"
-    leave
-.end
-
-.block: get_typeof
-    eximport "typesystem"
-
-    frame 1
-    paref 0 1
-    excall typesystem.typeof 2
-
-    leave
-.end
-
 .def: typeof 1
     arg 0 1
-
-    tryframe
-    catch "LinkException" handle_LinkException
-    catch "Exception" handle_Exception
-    try get_typeof
-
+    frame 1
+    param 0 1
+    excall typesystem.typeof 2
     move 2 0
     end
+.end
+
+.def: inheritanceChain 1
+    arg 0 1
+    frame 1
+    param 0 1
+    excall typesystem.inheritanceChain 2
+    move 2 0
+    end
+.end
+
+.def: bases 1
+    arg 0 1
+    frame 1
+    param 0 1
+    excall typesystem.bases 2
+    move 2 0
+    end
+.end
+
+
+.block: handle_LinkException
+    pull 2
+    print 2
+    halt
+.end
+
+.block: use
+    eximport "typesystem"
+    leave
 .end
 
 .def: main 1
     istore 1 42
     print 1
 
-    ;tryframe
-    ;catch "Exception" handle_Exception
-    ;try get_typeof
+    ; placeholder, for now it is not possible to use strings from registers
+    strstore 2 "typesystem"
+    tryframe
+    catch "LinkException" handle_LinkException
+    catch "Exception" handle_LinkException
+    try use
 
-    ; print typeof
     frame 1
     paref 0 1
     call typeof 2
     print 2
+
+    frame 1
+    paref 0 1
+    call bases 2
+    print 2
+
+    frame 1
+    paref 0 1
+    call inheritanceChain 2
+    print 2
+
 
     izero 0
     end
