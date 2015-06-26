31564e639d42622099c5843341cc72d963950ebb
Marek Marecki <marekjm@ozro.pw>
2015-06-26 22:18:30 +0200

    Reorganized code in assembler frontend

--- src/front/asm.cpp
+++ src/front/asm.cpp
@@ -650,146 +650,7 @@ map<string, uint16_t> mapFunctionAddresses(uint16_t& starting_instruction, const
 }
 
 
-
-bool usage(const char* program, bool SHOW_HELP, bool SHOW_VERSION, bool VERBOSE) {
-    if (SHOW_HELP or (SHOW_VERSION and VERBOSE)) {
-        cout << "Viua VM assembler, version ";
-    }
-    if (SHOW_HELP or SHOW_VERSION) {
-        cout << VERSION << endl;
-    }
-    if (SHOW_HELP) {
-        cout << "\nUSAGE:\n";
-        cout << "    " << program << " [option...] [-o <outfile>] <infile> [<linked-file>...]\n" << endl;
-        cout << "OPTIONS:\n";
-        cout << "    " << "-V, --version            - show version\n"
-             << "    " << "-h, --help               - display this message\n"
-             << "    " << "-v, --verbose            - show verbose output\n"
-             << "    " << "-d, --debug              - show debugging output\n"
-             << "    " << "    --scream             - show so much debugging output it becomes noisy\n"
-             << "    " << "-W, --Wall               - warn about everything\n"
-             << "    " << "    --Wmissin-end        - warn about missing 'end' instruction at the end of functions\n"
-             << "    " << "    --Wempty-function    - warn about empty functions\n"
-             << "    " << "    --Wopless-frame      - warn about frames without operands\n"
-             << "    " << "-E, --Eall               - treat all warnings as errors\n"
-             << "    " << "    --Emissing-end       - treat missing 'end' instruction at the end of function as error\n"
-             << "    " << "    --Eempty-function    - treat empty function as error\n"
-             << "    " << "    --Eopless-frame      - treat frames without operands as errors\n"
-             << "    " << "-c, --lib                - assemble as a library\n"
-             ;
-    }
-
-    return (SHOW_HELP or SHOW_VERSION);
-}
-
-int main(int argc, char* argv[]) {
-    // setup command line arguments vector
-    vector<string> args;
-    string option;
-
-    string filename(""), compilename("");
-
-    for (int i = 1; i < argc; ++i) {
-        option = string(argv[i]);
-        if (option == "--help" or option == "-h") {
-            SHOW_HELP = true;
-            continue;
-        } else if (option == "--version" or option == "-V") {
-            SHOW_VERSION = true;
-            continue;
-        } else if (option == "--verbose" or option == "-v") {
-            VERBOSE = true;
-            continue;
-        } else if (option == "--debug" or option == "-d") {
-            DEBUG = true;
-            continue;
-        } else if (option == "--scream") {
-            SCREAM = true;
-            continue;
-        } else if (option == "--lib" or option == "-c") {
-            AS_LIB = true;
-            continue;
-        } else if (option == "--Wall" or option == "-W") {
-            WARNING_ALL = true;
-            continue;
-        } else if (option == "--Eall" or option == "-E") {
-            ERROR_ALL = true;
-            continue;
-        } else if (option == "--Wmissing-end") {
-            WARNING_MISSING_END = true;
-            continue;
-        } else if (option == "--Wempty-function") {
-            WARNING_EMPTY_FUNCTION_BODY = true;
-            continue;
-        } else if (option == "--Wopless-frame") {
-            WARNING_OPERANDLESS_FRAME = true;
-            continue;
-        } else if (option == "--Wglobals-in-lib") {
-            WARNING_GLOBALS_IN_LIB = true;
-            continue;
-        } else if (option == "--Emissing-end") {
-            ERROR_MISSING_END = true;
-            continue;
-        } else if (option == "--Eempty-function") {
-            ERROR_EMPTY_FUNCTION_BODY = true;
-            continue;
-        } else if (option == "--Eopless-frame") {
-            ERROR_OPERANDLESS_FRAME = true;
-            continue;
-        } else if (option == "--Eglobals-in-lib") {
-            ERROR_GLOBALS_IN_LIB = true;
-            continue;
-        } else if (option == "--out" or option == "-o") {
-            if (i < argc-1) {
-                compilename = string(argv[++i]);
-            } else {
-                cout << "error: option '" << argv[i] << "' requires an argument: filename" << endl;
-                exit(1);
-            }
-            continue;
-        }
-        args.push_back(argv[i]);
-    }
-
-    if (usage(argv[0], SHOW_HELP, SHOW_VERSION, VERBOSE)) { return 0; }
-
-    int ret_code = 0;
-
-    if (args.size() == 0) {
-        cout << "fatal: no input file" << endl;
-        return 1;
-    }
-
-
-    ////////////////////////////////
-    // FIND FILENAME AND COMPILENAME
-    filename = args[0];
-    if (!filename.size()) {
-        cout << "fatal: no file to assemble" << endl;
-        return 1;
-    }
-
-    if (compilename == "") {
-        if (AS_LIB) {
-            compilename = (filename + ".wlib");
-        } else {
-            compilename = "a.out";
-        }
-    }
-
-    if (VERBOSE or DEBUG) {
-        cout << "message: assembling \"" << filename << "\" to \"" << compilename << "\"" << endl;
-    }
-
-
-    //////////////////////////////////////////
-    // GATHER LINKS OBTAINED FROM COMMAND LINE
-    vector<string> commandline_given_links;
-    for (unsigned i = 1; i < args.size(); ++i) {
-        commandline_given_links.push_back(args[i]);
-    }
-
-
+int generate(const string& filename, string& compilename, const vector<string>& commandline_given_links) {
     ////////////////
     // READ LINES IN
     ifstream in(filename, ios::in | ios::binary);
@@ -1501,6 +1362,147 @@ int main(int argc, char* argv[]) {
 
     out.write((const char*)program_bytecode, bytes);
     out.close();
+
+    return 0;
+}
+
+
+bool usage(const char* program, bool SHOW_HELP, bool SHOW_VERSION, bool VERBOSE) {
+    if (SHOW_HELP or (SHOW_VERSION and VERBOSE)) {
+        cout << "Viua VM assembler, version ";
+    }
+    if (SHOW_HELP or SHOW_VERSION) {
+        cout << VERSION << endl;
+    }
+    if (SHOW_HELP) {
+        cout << "\nUSAGE:\n";
+        cout << "    " << program << " [option...] [-o <outfile>] <infile> [<linked-file>...]\n" << endl;
+        cout << "OPTIONS:\n";
+        cout << "    " << "-V, --version            - show version\n"
+             << "    " << "-h, --help               - display this message\n"
+             << "    " << "-v, --verbose            - show verbose output\n"
+             << "    " << "-d, --debug              - show debugging output\n"
+             << "    " << "    --scream             - show so much debugging output it becomes noisy\n"
+             << "    " << "-W, --Wall               - warn about everything\n"
+             << "    " << "    --Wmissin-end        - warn about missing 'end' instruction at the end of functions\n"
+             << "    " << "    --Wempty-function    - warn about empty functions\n"
+             << "    " << "    --Wopless-frame      - warn about frames without operands\n"
+             << "    " << "-E, --Eall               - treat all warnings as errors\n"
+             << "    " << "    --Emissing-end       - treat missing 'end' instruction at the end of function as error\n"
+             << "    " << "    --Eempty-function    - treat empty function as error\n"
+             << "    " << "    --Eopless-frame      - treat frames without operands as errors\n"
+             << "    " << "-c, --lib                - assemble as a library\n"
+             ;
+    }
+
+    return (SHOW_HELP or SHOW_VERSION);
+}
+
+int main(int argc, char* argv[]) {
+    // setup command line arguments vector
+    vector<string> args;
+    string option;
+
+    string filename(""), compilename("");
+
+    for (int i = 1; i < argc; ++i) {
+        option = string(argv[i]);
+        if (option == "--help" or option == "-h") {
+            SHOW_HELP = true;
+            continue;
+        } else if (option == "--version" or option == "-V") {
+            SHOW_VERSION = true;
+            continue;
+        } else if (option == "--verbose" or option == "-v") {
+            VERBOSE = true;
+            continue;
+        } else if (option == "--debug" or option == "-d") {
+            DEBUG = true;
+            continue;
+        } else if (option == "--scream") {
+            SCREAM = true;
+            continue;
+        } else if (option == "--lib" or option == "-c") {
+            AS_LIB = true;
+            continue;
+        } else if (option == "--Wall" or option == "-W") {
+            WARNING_ALL = true;
+            continue;
+        } else if (option == "--Eall" or option == "-E") {
+            ERROR_ALL = true;
+            continue;
+        } else if (option == "--Wmissing-end") {
+            WARNING_MISSING_END = true;
+            continue;
+        } else if (option == "--Wempty-function") {
+            WARNING_EMPTY_FUNCTION_BODY = true;
+            continue;
+        } else if (option == "--Wopless-frame") {
+            WARNING_OPERANDLESS_FRAME = true;
+            continue;
+        } else if (option == "--Wglobals-in-lib") {
+            WARNING_GLOBALS_IN_LIB = true;
+            continue;
+        } else if (option == "--Emissing-end") {
+            ERROR_MISSING_END = true;
+            continue;
+        } else if (option == "--Eempty-function") {
+            ERROR_EMPTY_FUNCTION_BODY = true;
+            continue;
+        } else if (option == "--Eopless-frame") {
+            ERROR_OPERANDLESS_FRAME = true;
+            continue;
+        } else if (option == "--Eglobals-in-lib") {
+            ERROR_GLOBALS_IN_LIB = true;
+            continue;
+        } else if (option == "--out" or option == "-o") {
+            if (i < argc-1) {
+                compilename = string(argv[++i]);
+            } else {
+                cout << "error: option '" << argv[i] << "' requires an argument: filename" << endl;
+                exit(1);
+            }
+            continue;
+        }
+        args.push_back(argv[i]);
+    }
+
+    if (usage(argv[0], SHOW_HELP, SHOW_VERSION, VERBOSE)) { return 0; }
+
+    if (args.size() == 0) {
+        cout << "fatal: no input file" << endl;
+        return 1;
+    }
+
+    ////////////////////////////////
+    // FIND FILENAME AND COMPILENAME
+    filename = args[0];
+    if (!filename.size()) {
+        cout << "fatal: no file to assemble" << endl;
+        return 1;
+    }
+
+    if (compilename == "") {
+        if (AS_LIB) {
+            compilename = (filename + ".wlib");
+        } else {
+            compilename = "a.out";
+        }
+    }
+
+    if (VERBOSE or DEBUG) {
+        cout << "message: assembling \"" << filename << "\" to \"" << compilename << "\"" << endl;
+    }
+
+
+    //////////////////////////////////////////
+    // GATHER LINKS OBTAINED FROM COMMAND LINE
+    vector<string> commandline_given_links;
+    for (unsigned i = 1; i < args.size(); ++i) {
+        commandline_given_links.push_back(args[i]);
+    }
+
+    int ret_code = generate(filename, compilename, commandline_given_links);
 
     return ret_code;
 }
