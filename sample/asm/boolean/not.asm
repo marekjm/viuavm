5f4145b76e24ff38ab5ec7f11257f337af276bf7
Marek Marecki <marekjm@ozro.pw>
2015-07-02 19:13:11 +0200

    Add sample files for boolean instructions

---
+++ sample/asm/boolean/not.asm
@@ -0,0 +1,22 @@
+.function: boolean
+    arg 0 1
+    not 1
+    not 1
+    move 1 0
+    end
+.end
+
+.function: main 1
+    izero 1
+
+    frame 1
+    param 0 1
+    call boolean 1
+
+    print 1
+    not 1
+    print 1
+
+    izero 0
+    end
+.end
