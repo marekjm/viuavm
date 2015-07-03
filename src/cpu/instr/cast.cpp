17f2208c572ee8158d9845d7e8e9920c42cdbda0
Marek Marecki <marekjm@ozro.pw>
2015-07-03 22:48:10 +0200

    Better variable name

--- src/cpu/instr/cast.cpp
+++ src/cpu/instr/cast.cpp
@@ -13,27 +13,27 @@ using namespace std;
 byte* CPU::itof(byte* addr) {
     /*  Run itof instruction.
      */
-    bool first_operand_ref, destination_register_ref;
-    int first_operand_index, destination_register_index;
+    bool casted_object_ref, destination_register_ref;
+    int casted_object_index, destination_register_index;
 
     destination_register_ref = *((bool*)addr);
     pointer::inc<bool, byte>(addr);
     destination_register_index = *((int*)addr);
     pointer::inc<int, byte>(addr);
 
-    first_operand_ref = *((bool*)addr);
+    casted_object_ref = *((bool*)addr);
     pointer::inc<bool, byte>(addr);
-    first_operand_index = *((int*)addr);
+    casted_object_index = *((int*)addr);
     pointer::inc<int, byte>(addr);
 
-    if (first_operand_ref) {
-        first_operand_index = static_cast<Integer*>(fetch(first_operand_index))->value();
+    if (casted_object_ref) {
+        casted_object_index = static_cast<Integer*>(fetch(casted_object_index))->value();
     }
     if (destination_register_ref) {
         destination_register_index = static_cast<Integer*>(fetch(destination_register_index))->value();
     }
 
-    place(destination_register_index, new Float(static_cast<Integer*>(fetch(first_operand_index))->value()));
+    place(destination_register_index, new Float(static_cast<Integer*>(fetch(casted_object_index))->value()));
 
     return addr;
 }
@@ -41,27 +41,27 @@ byte* CPU::itof(byte* addr) {
 byte* CPU::ftoi(byte* addr) {
     /*  Run ftoi instruction.
      */
-    bool first_operand_ref, destination_register_ref;
-    int first_operand_index, destination_register_index;
+    bool casted_object_ref, destination_register_ref;
+    int casted_object_index, destination_register_index;
 
     destination_register_ref = *((bool*)addr);
     pointer::inc<bool, byte>(addr);
     destination_register_index = *((int*)addr);
     pointer::inc<int, byte>(addr);
 
-    first_operand_ref = *((bool*)addr);
+    casted_object_ref = *((bool*)addr);
     pointer::inc<bool, byte>(addr);
-    first_operand_index = *((int*)addr);
+    casted_object_index = *((int*)addr);
     pointer::inc<int, byte>(addr);
 
-    if (first_operand_ref) {
-        first_operand_index = static_cast<Integer*>(fetch(first_operand_index))->value();
+    if (casted_object_ref) {
+        casted_object_index = static_cast<Integer*>(fetch(casted_object_index))->value();
     }
     if (destination_register_ref) {
         destination_register_index = static_cast<Integer*>(fetch(destination_register_index))->value();
     }
 
-    place(destination_register_index, new Integer(static_cast<Float*>(fetch(first_operand_index))->value()));
+    place(destination_register_index, new Integer(static_cast<Float*>(fetch(casted_object_index))->value()));
 
     return addr;
 }
@@ -69,27 +69,27 @@ byte* CPU::ftoi(byte* addr) {
 byte* CPU::stoi(byte* addr) {
     /*  Run stoi instruction.
      */
-    bool first_operand_ref, destination_register_ref;
-    int first_operand_index, destination_register_index;
+    bool casted_object_ref, destination_register_ref;
+    int casted_object_index, destination_register_index;
 
     destination_register_ref = *((bool*)addr);
     pointer::inc<bool, byte>(addr);
     destination_register_index = *((int*)addr);
     pointer::inc<int, byte>(addr);
 
-    first_operand_ref = *((bool*)addr);
+    casted_object_ref = *((bool*)addr);
     pointer::inc<bool, byte>(addr);
-    first_operand_index = *((int*)addr);
+    casted_object_index = *((int*)addr);
     pointer::inc<int, byte>(addr);
 
-    if (first_operand_ref) {
-        first_operand_index = static_cast<Integer*>(fetch(first_operand_index))->value();
+    if (casted_object_ref) {
+        casted_object_index = static_cast<Integer*>(fetch(casted_object_index))->value();
     }
     if (destination_register_ref) {
         destination_register_index = static_cast<Integer*>(fetch(destination_register_index))->value();
     }
 
-    place(destination_register_index, new Integer(std::stoi(static_cast<String*>(fetch(first_operand_index))->value())));
+    place(destination_register_index, new Integer(std::stoi(static_cast<String*>(fetch(casted_object_index))->value())));
 
     return addr;
 }
@@ -97,27 +97,27 @@ byte* CPU::stoi(byte* addr) {
 byte* CPU::stof(byte* addr) {
     /*  Run stof instruction.
      */
-    bool first_operand_ref, destination_register_ref;
-    int first_operand_index, destination_register_index;
+    bool casted_object_ref, destination_register_ref;
+    int casted_object_index, destination_register_index;
 
     destination_register_ref = *((bool*)addr);
     pointer::inc<bool, byte>(addr);
     destination_register_index = *((int*)addr);
     pointer::inc<int, byte>(addr);
 
-    first_operand_ref = *((bool*)addr);
+    casted_object_ref = *((bool*)addr);
     pointer::inc<bool, byte>(addr);
-    first_operand_index = *((int*)addr);
+    casted_object_index = *((int*)addr);
     pointer::inc<int, byte>(addr);
 
-    if (first_operand_ref) {
-        first_operand_index = static_cast<Integer*>(fetch(first_operand_index))->value();
+    if (casted_object_ref) {
+        casted_object_index = static_cast<Integer*>(fetch(casted_object_index))->value();
     }
     if (destination_register_ref) {
         destination_register_index = static_cast<Integer*>(fetch(destination_register_index))->value();
     }
 
-    place(destination_register_index, new Float(std::stod(static_cast<String*>(fetch(first_operand_index))->value())));
+    place(destination_register_index, new Float(std::stod(static_cast<String*>(fetch(casted_object_index))->value())));
 
     return addr;
 }
