45185c062d2447a85dfc4312b1b768999d8ae4e4
Marek Marecki <marekjm@ozro.pw>
2015-07-03 23:54:27 +0200

    Better variable names

--- src/cpu/instr/vector.cpp
+++ src/cpu/instr/vector.cpp
@@ -12,19 +12,19 @@ using namespace std;
 byte* CPU::vec(byte* addr) {
     /*  Run vec instruction.
      */
-    bool reg_ref;
-    int reg_num;
+    bool target_register_ref;
+    int target_register_index;
 
-    reg_ref = *((bool*)addr);
+    target_register_ref = *((bool*)addr);
     pointer::inc<bool, byte>(addr);
-    reg_num = *((int*)addr);
+    target_register_index = *((int*)addr);
     pointer::inc<int, byte>(addr);
 
-    if (reg_ref) {
-        reg_num = static_cast<Integer*>(fetch(reg_num))->value();
+    if (target_register_ref) {
+        target_register_index = static_cast<Integer*>(fetch(target_register_index))->value();
     }
 
-    place(reg_num, new Vector());
+    place(target_register_index, new Vector());
 
     return addr;
 }
@@ -35,35 +35,35 @@ byte* CPU::vinsert(byte* addr) {
      *  Vector always inserts a copy of the object in a register.
      *  FIXME: make it possible to insert references.
      */
-    bool regvec_ref, regval_ref, regpos_ref;
-    int regvec_num, regval_num, regpos_num;
+    bool vector_operand_ref, destination_register_ref, position_operand_ref;
+    int vector_operand_index, destination_register_index, position_operand_index;
 
-    regvec_ref = *((bool*)addr);
+    vector_operand_ref = *((bool*)addr);
     pointer::inc<bool, byte>(addr);
-    regvec_num = *((int*)addr);
+    vector_operand_index = *((int*)addr);
     pointer::inc<int, byte>(addr);
 
-    regval_ref = *((bool*)addr);
+    destination_register_ref = *((bool*)addr);
     pointer::inc<bool, byte>(addr);
-    regval_num = *((int*)addr);
+    destination_register_index = *((int*)addr);
     pointer::inc<int, byte>(addr);
 
-    regpos_ref = *((bool*)addr);
+    position_operand_ref = *((bool*)addr);
     pointer::inc<bool, byte>(addr);
-    regpos_num = *((int*)addr);
+    position_operand_index = *((int*)addr);
     pointer::inc<int, byte>(addr);
 
-    if (regvec_ref) {
-        regvec_num = static_cast<Integer*>(fetch(regvec_num))->value();
+    if (vector_operand_ref) {
+        vector_operand_index = static_cast<Integer*>(fetch(vector_operand_index))->value();
     }
-    if (regval_ref) {
-        regval_num = static_cast<Integer*>(fetch(regval_num))->value();
+    if (destination_register_ref) {
+        destination_register_index = static_cast<Integer*>(fetch(destination_register_index))->value();
     }
-    if (regpos_ref) {
-        regpos_num = static_cast<Integer*>(fetch(regpos_num))->value();
+    if (position_operand_ref) {
+        position_operand_index = static_cast<Integer*>(fetch(position_operand_index))->value();
     }
 
-    static_cast<Vector*>(fetch(regvec_num))->insert(regpos_num, fetch(regval_num)->copy());
+    static_cast<Vector*>(fetch(vector_operand_index))->insert(position_operand_index, fetch(destination_register_index)->copy());
 
     return addr;
 }
@@ -74,27 +74,27 @@ byte* CPU::vpush(byte* addr) {
      *  Vector always pushes a copy of the object in a register.
      *  FIXME: make it possible to push references.
      */
-    bool regvec_ref, regval_ref;
-    int regvec_num, regval_num;
+    bool vector_operand_ref, destination_register_ref;
+    int vector_operand_index, destination_register_index;
 
-    regvec_ref = *((bool*)addr);
+    vector_operand_ref = *((bool*)addr);
     pointer::inc<bool, byte>(addr);
-    regvec_num = *((int*)addr);
+    vector_operand_index = *((int*)addr);
     pointer::inc<int, byte>(addr);
 
-    regval_ref = *((bool*)addr);
+    destination_register_ref = *((bool*)addr);
     pointer::inc<bool, byte>(addr);
-    regval_num = *((int*)addr);
+    destination_register_index = *((int*)addr);
     pointer::inc<int, byte>(addr);
 
-    if (regvec_ref) {
-        regvec_num = static_cast<Integer*>(fetch(regvec_num))->value();
+    if (vector_operand_ref) {
+        vector_operand_index = static_cast<Integer*>(fetch(vector_operand_index))->value();
     }
-    if (regval_ref) {
-        regval_num = static_cast<Integer*>(fetch(regval_num))->value();
+    if (destination_register_ref) {
+        destination_register_index = static_cast<Integer*>(fetch(destination_register_index))->value();
     }
 
-    static_cast<Vector*>(fetch(regvec_num))->push(fetch(regval_num)->copy());
+    static_cast<Vector*>(fetch(vector_operand_index))->push(fetch(destination_register_index)->copy());
 
     return addr;
 }
@@ -105,40 +105,40 @@ byte* CPU::vpop(byte* addr) {
      *  Vector always pops a copy of the object in a register.
      *  FIXME: make it possible to pop references.
      */
-    bool regvec_ref, regdst_ref, regpos_ref;
-    int regvec_num, regdst_num, regpos_num;
+    bool vector_operand_ref, destination_register_ref, position_operand_ref;
+    int vector_operand_index, destination_register_index, position_operand_index;
 
-    regvec_ref = *((bool*)addr);
+    vector_operand_ref = *((bool*)addr);
     pointer::inc<bool, byte>(addr);
-    regvec_num = *((int*)addr);
+    vector_operand_index = *((int*)addr);
     pointer::inc<int, byte>(addr);
 
-    regdst_ref = *((bool*)addr);
+    destination_register_ref = *((bool*)addr);
     pointer::inc<bool, byte>(addr);
-    regdst_num = *((int*)addr);
+    destination_register_index = *((int*)addr);
     pointer::inc<int, byte>(addr);
 
-    regpos_ref = *((bool*)addr);
+    position_operand_ref = *((bool*)addr);
     pointer::inc<bool, byte>(addr);
-    regpos_num = *((int*)addr);
+    position_operand_index = *((int*)addr);
     pointer::inc<int, byte>(addr);
 
-    if (regvec_ref) {
-        regvec_num = static_cast<Integer*>(fetch(regvec_num))->value();
+    if (vector_operand_ref) {
+        vector_operand_index = static_cast<Integer*>(fetch(vector_operand_index))->value();
     }
-    if (regdst_ref) {
-        regdst_num = static_cast<Integer*>(fetch(regdst_num))->value();
+    if (destination_register_ref) {
+        destination_register_index = static_cast<Integer*>(fetch(destination_register_index))->value();
     }
-    if (regpos_ref) {
-        regpos_num = static_cast<Integer*>(fetch(regpos_num))->value();
+    if (position_operand_ref) {
+        position_operand_index = static_cast<Integer*>(fetch(position_operand_index))->value();
     }
 
     /*  1) fetch vector,
      *  2) pop value at given index,
      *  3) put it in a register,
      */
-    Type* ptr = static_cast<Vector*>(fetch(regvec_num))->pop(regpos_num);
-    if (regdst_num) { place(regdst_num, ptr); }
+    Type* ptr = static_cast<Vector*>(fetch(vector_operand_index))->pop(position_operand_index);
+    if (destination_register_index) { place(destination_register_index, ptr); }
 
     return addr;
 }
@@ -191,27 +191,27 @@ byte* CPU::vat(byte* addr) {
 byte* CPU::vlen(byte* addr) {
     /*  Run vlen instruction.
      */
-    bool regvec_ref, regval_ref;
-    int regvec_num, regval_num;
+    bool vector_operand_ref, destination_register_ref;
+    int vector_operand_index, destination_register_index;
 
-    regvec_ref = *((bool*)addr);
+    vector_operand_ref = *((bool*)addr);
     pointer::inc<bool, byte>(addr);
-    regvec_num = *((int*)addr);
+    vector_operand_index = *((int*)addr);
     pointer::inc<int, byte>(addr);
 
-    regval_ref = *((bool*)addr);
+    destination_register_ref = *((bool*)addr);
     pointer::inc<bool, byte>(addr);
-    regval_num = *((int*)addr);
+    destination_register_index = *((int*)addr);
     pointer::inc<int, byte>(addr);
 
-    if (regvec_ref) {
-        regvec_num = static_cast<Integer*>(fetch(regvec_num))->value();
+    if (vector_operand_ref) {
+        vector_operand_index = static_cast<Integer*>(fetch(vector_operand_index))->value();
     }
-    if (regval_ref) {
-        regval_num = static_cast<Integer*>(fetch(regval_num))->value();
+    if (destination_register_ref) {
+        destination_register_index = static_cast<Integer*>(fetch(destination_register_index))->value();
     }
 
-    place(regval_num, new Integer(static_cast<Vector*>(fetch(regvec_num))->len()));
+    place(destination_register_index, new Integer(static_cast<Vector*>(fetch(vector_operand_index))->len()));
 
     return addr;
 }
