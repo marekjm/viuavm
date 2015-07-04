df52d710c5a0c166916f93115e180edeb71c3fea
Marek Marecki <marekjm@ozro.pw>
2015-07-04 03:43:46 +0200

    Better instruction lister

--- src/bytecode/opcd.cpp
+++ src/bytecode/opcd.cpp
@@ -7,11 +7,35 @@
 #include "maps.h"
 using namespace std;
 
+
 int main() {
+    unsigned max_mnemonic_length = 0;
     for (pair<const OPCODE, string> i : OP_NAMES) {
-        cout << i.second << ":\t";
-        cout << i.first << " (0x" << hex << i.first << dec << "), size: ";
-        cout << OP_SIZES.at(i.second) << '\n';
+        max_mnemonic_length = ((max_mnemonic_length >= i.second.size()) ? max_mnemonic_length : i.second.size());
+    }
+
+    // separate mnemonic from the rest of data
+    max_mnemonic_length += 4;
+
+    const string initial_column = "MNEMONIC    ";
+    cout << initial_column << "| OPCODE | HEX OPCODE | SIZE | LENGTH\n" << endl;
+
+    max_mnemonic_length = (max_mnemonic_length < initial_column.size() ? initial_column.size() : max_mnemonic_length);
+
+    for (pair<const OPCODE, string> i : OP_NAMES) {
+        cout << i.second;
+        for (unsigned j = i.second.size(); j < (max_mnemonic_length); ++j) {
+            cout << ' ';
+        }
+        cout << "  ";
+        cout << i.first << (i.first < 10 ? " " : "");
+        cout << "       ";
+        cout << "0x" << hex << i.first << (i.first < 0x10 ? " " : "") << dec;
+        cout << "         ";
+        cout << OP_SIZES.at(i.second) << (OP_SIZES.at(i.second) < 10 ? " " : "");
+        cout << "     ";
+        cout << ((find(OP_VARIABLE_LENGTH.begin(), OP_VARIABLE_LENGTH.end(), i.first) != OP_VARIABLE_LENGTH.end()) ? "variable" : "constant");
+        cout << '\n';
     }
     cout << flush;
     return 0;
