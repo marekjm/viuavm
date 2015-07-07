aec186cad6ce6aaa46070e9e05b8d8e193f1d1ec
Marek Marecki <marekjm@ozro.pw>
2015-07-07 23:15:41 +0200

    Better version-updating script

--- scripts/update_commit_info.sh
+++ scripts/update_commit_info.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env sh
 
 FILE=src/version.h
-sed -i "s/COMMIT = \".*\"/COMMIT = \"`git rev-parse HEAD`\"/" $FILE
-sed -i "s/MICRO = \".*\"/MICRO = \"`git describe HEAD | cut -d'-' -f2`\"/" $FILE
+COMMITS_SINCE=`git describe HEAD | cut -d'-' -f2`
+SAVED_MICRO=`grep -P 'MICRO' $FILE | grep -Po '\d+'`
+
+if [[ $COMMITS_SINCE == $SAVED_MICRO ]]; then
+    exit 0
+fi
+
+HEAD_UPDATED_VERSION=`git show HEAD | grep -P '^---' | cut -d' ' -f2 | sed 's:a/::' | grep $FILE`
+
+if [[ $HEAD_UPDATED_VERSION == "" ]]; then
+    sed -i "s/COMMIT = \".*\"/COMMIT = \"`git rev-parse HEAD`\"/" $FILE
+    sed -i "s/MICRO = \".*\"/MICRO = \"$COMMITS_SINCE\"/" $FILE
+fi
