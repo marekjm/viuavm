ca8e2c2b2191fa61d54621171cc2bda07b5e94d0
Marek Marecki <marekjm@ozro.pw>
2015-07-08 23:00:24 +0200

    Better (even if just a little) version-updating script

--- scripts/update_commit_info.sh
+++ scripts/update_commit_info.sh
@@ -4,12 +4,16 @@ FILE=src/version.h
 COMMITS_SINCE=`git describe HEAD | cut -d'-' -f2`
 SAVED_MICRO=`grep -P 'MICRO' $FILE | grep -Po '\d+'`
 
+# add 1 to include the *just being commited* commit
+COMMITS_SINCE=`dc -e "1 $COMMITS_SINCE + p"`
+
 if [[ $COMMITS_SINCE == $SAVED_MICRO ]]; then
     exit 0
 fi
 
 HEAD_UPDATED_VERSION=`git show HEAD | grep -P '^---' | cut -d' ' -f2 | sed 's:a/::' | grep $FILE`
 
+echo $COMMITS_SINCE
 if [[ $HEAD_UPDATED_VERSION == "" ]]; then
     sed -i "s/COMMIT = \".*\"/COMMIT = \"`git rev-parse HEAD`\"/" $FILE
     sed -i "s/MICRO = \".*\"/MICRO = \"$COMMITS_SINCE\"/" $FILE
