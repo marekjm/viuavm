cb600b0bf0fe364cbd09f3a9ee0261da216637ae
Marek Marecki <marekjm@ozro.pw>
2015-07-09 23:52:51 +0200

    Add second standard library module: functional

---
+++ src/stdlib/viua/functional.asm
@@ -0,0 +1,194 @@
+; This script implements basic functions used in functional programming style
+;
+; It can be dynamically linked to programs running on Viua VM by putting "link functional" instruction in
+; source code of the script.
+; Alternatively, it can be statically linked if that would be more suitable.
+;
+; When linking dynamically, following signatures can be used:
+;
+;       .signature: functional::filter
+;       .signature: functional::map
+;       .signature: functional::apply
+;       .signature: functional::invoke
+;
+
+
+.function: filter
+    ; classic filter() function
+    ; it takes two arguments:
+    ;   * a filtering function,
+    ;   * a vector with values to be filtered,
+    arg 1 0
+    arg 2 1
+
+    ; vector for filtered values
+    vec 3
+
+    ; initial loop counter and
+    ; loop termination variable
+    izero 4
+    vlen 5 2
+
+    ; while (...) {
+    .mark: loop_begin
+    igte 6 4 5
+    branch 6 loop_end loop_body
+
+    .mark: loop_body
+
+    ; call filtering function to determine whether current element
+    ; is a valid value
+    frame 1
+    vat 7 2 @4
+    param 0 7
+    fcall 8 1
+
+    ; if the result from filtering function was "true" - the element should be pushed onto result vector
+    ; it it was "false" - skip to next iteration
+    branch 8 element_ok next_iter
+
+    .mark: element_ok
+    vpush 3 7
+
+    .mark: next_iter
+    ; empty the register because vat instruction creates references
+    empty 7
+
+    ; increase the counter and go back to the beginning of the loop
+    ;     ++i;
+    ; }
+    iinc 4
+    jump loop_begin
+
+    .mark: loop_end
+
+    ; move result vector into return register
+    move 0 3
+    end
+.end
+
+.function: map
+    ; this function takes two arguments:
+    ;   * a function,
+    ;   * a vector,
+    ; then, it maps (i.e. calls) the given function on every element of given vector
+    ; and returns a vector containing modified values.
+    ; returned vector is a newly created one - this function does not modify vectors in place.
+    arg 1 0
+    arg 2 1
+
+    ; new vector to store mapped values
+    vec 3
+
+    ; set initial counter value and
+    ; loop termination variable
+    izero 4
+    vlen 5 2
+
+    ; while (...) {
+    .mark: loop_begin
+    igte 6 4 5
+    branch 6 loop_end loop_body
+
+    .mark: loop_body
+
+    ; call supplied function on current element
+    frame 1
+    vat 7 2 @4
+    param 0 7
+    fcall 8 1
+
+    ; push result to new vector
+    vpush 3 8
+
+    ; empty the register, as vat instruction creates references
+    empty 7
+
+    ; increase loop counter and go back to the beginning
+    ;     ++i;
+    ; }
+    iinc 4
+    jump loop_begin
+
+    .mark: loop_end
+
+    ; move vector with mapped values to the return register
+    move 0 3
+    end
+.end
+
+.function: apply
+    ; this function applies another function on a single parameter
+    ;
+    ; this function is type agnostic
+    ; it just passes the parameter without additional processing
+    .name: 1 func
+    .name: 2 parameter
+
+    ; extract the parameters
+    arg func 0
+    arg parameter 1
+
+    ; apply the function to the parameter...
+    frame 1
+    param 0 parameter
+    fcall 3 func
+
+    ; ...and return the result
+    move 0 3
+    end
+.end
+
+.function: invoke
+    ; this function takes two parameters:
+    ;    1) a function object
+    ;    2) a vector of parameters for function given as first parameter
+    ;
+    ; it then creates a frame with required number of parameter slots (as
+    ; specified by length of the vector), and calls given function with this
+    ; frame
+    arg 1 0
+    arg 2 1
+
+    ; take length of the vector
+    .name: 4 vector_length
+    vlen vector_length 2
+    frame @vector_length
+
+    ; zero loop counter
+    .name: 3 loop_counter
+    izero loop_counter
+    .mark: while_begin
+
+    ; simple condition:
+    ; while (loop_counter < vector_length) {
+    .name: 5 loop_condition
+    igte loop_condition loop_counter vector_length
+    branch loop_condition while_end while_body
+
+    .mark: while_body
+
+    ; store item located inside parameter vector at index denoted by loop_counter in
+    ; a register
+    .name: 7 slot
+    vat slot 2 @loop_counter
+
+    ; add parameter
+    param @loop_counter slot
+
+    ; clear parameter slot
+    empty slot
+
+    ; loop_counter++
+    iinc loop_counter
+
+    jump while_begin
+
+    .mark: while_end
+
+    ; finally, after the frame is ready
+    ; call the function
+    fcall 8 1
+    move 0 8
+    end
+.end
