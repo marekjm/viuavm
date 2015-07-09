dce58aeecff56a9529b7cf59549522f0449c233d
Marek Marecki <marekjm@ozro.pw>
2015-07-09 23:29:41 +0200

    Small version bump

--- src/version.h
+++ src/version.h
@@ -4,11 +4,11 @@
 #pragma once
 
 const char* VERSION = "0.4.6";
-const char* MICRO = "177";
+const char* MICRO = "180";
 
 // commit pointed by the hash below is HEAD~1
 // while the MICRO variable shows actual number of commits added since last release,
 // the commit hash is off-by-one
-const char* COMMIT = "1271c20fc44b1115a67b651f009f6ba61c1bd456";
+const char* COMMIT = "8106328542d416c51a8f654910576c231d9b3e9e";
 
 #endif
