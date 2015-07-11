8b75092dec885cd3fa03ecd4b5bbc61d2e659fcd
Marek Marecki <marekjm@ozro.pw>
2015-07-12 00:09:30 +0200

    Fixed .gitignore

--- src/version.h
+++ src/version.h
@@ -4,11 +4,11 @@
 #pragma once
 
 const char* VERSION = "0.4.6";
-const char* MICRO = "200";
+const char* MICRO = "208";
 
 // commit pointed by the hash below is HEAD~1
 // while the MICRO variable shows actual number of commits added since last release,
 // the commit hash is off-by-one
-const char* COMMIT = "13e2628c13e594227332869312531735399a1e06";
+const char* COMMIT = "3bae86662cd2ee689cc5aa763df6362d07ad6ef8";
 
 #endif
