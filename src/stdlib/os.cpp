fd965a1f5edcde0000a16aa394253f3470f507ff
Marek Marecki <marekjm@ozro.pw>
2015-07-12 19:10:27 +0200

    Add standard "os" extension module

---
+++ src/stdlib/os.cpp
@@ -0,0 +1,29 @@
+#include <cstdlib>
+#include <string>
+#include "../types/integer.h"
+#include "../types/exception.h"
+#include "../cpu/frame.h"
+#include "../cpu/registerset.h"
+#include "../include/module.h"
+using namespace std;
+
+
+Type* os_system(Frame* frame, RegisterSet*, RegisterSet*) {
+    if (frame->args->at(0) == 0) {
+        throw new Exception("expected command to launch (string) as parameter 0");
+    }
+    string command = frame->args->get(0)->str();
+    int ret = system(command.c_str());
+    frame->regset->set(0, new Integer(ret));
+    return 0;
+}
+
+
+const ExternalFunctionSpec functions[] = {
+    { "system", &os_system },
+    { NULL, NULL },
+};
+
+extern "C" const ExternalFunctionSpec* exports() {
+    return functions;
+}
