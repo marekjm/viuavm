fcc631ab5ca1b6fa77a1195d65125872aebd81a2
Marek Marecki <marekjm@ozro.pw>
2015-07-20 19:08:45 +0200

    EXCALL opcode uses callForeign() to call foreign functions

--- src/cpu/cpu.cpp
+++ src/cpu/cpu.cpp
@@ -229,6 +229,70 @@ byte* CPU::callNative(byte* addr, const string& call_name, const bool& return_re
 
     return call_address;
 }
+byte* CPU::callForeign(byte* addr, const string& call_name, const bool& return_ref, const int& return_index) {
+    addr += (call_name.size()+1);
+
+    // save return address for frame
+    byte* return_address = addr;
+
+    if (frame_new == 0) {
+        throw new Exception("external function call without a frame: use `frame 0' in source code if the function takes no parameters");
+    }
+    // set function name and return address
+    frame_new->function_name = call_name;
+    frame_new->return_address = return_address;
+
+    frame_new->resolve_return_value_register = return_ref;
+    frame_new->place_return_value_in = return_index;
+
+    Frame* frame = frame_new;
+
+    pushFrame();
+
+    if (external_functions.count(call_name) == 0) {
+        throw new Exception("call to unregistered external function: " + call_name);
+    }
+
+    /* FIXME: second parameter should be a pointer to static registers or
+     *        0 if function does not have static registers registered
+     * FIXME: should external functions always have static registers allocated?
+     */
+    ExternalFunction* callback = external_functions.at(call_name);
+    (*callback)(frame, 0, regset);
+
+    // FIXME: woohoo! segfault!
+    Type* returned = 0;
+    bool returned_is_reference = false;
+    int return_value_register = frames.back()->place_return_value_in;
+    bool resolve_return_value_register = frames.back()->resolve_return_value_register;
+    if (return_value_register != 0) {
+        // we check in 0. register because it's reserved for return values
+        if (uregset->at(0) == 0) {
+            throw new Exception("return value requested by frame but external function did not set return register");
+        }
+        if (uregset->isflagged(0, REFERENCE)) {
+            returned = uregset->get(0);
+            returned_is_reference = true;
+        } else {
+            returned = uregset->get(0)->copy();
+        }
+    }
+
+    dropFrame();
+
+    // place return value
+    if (returned and frames.size() > 0) {
+        if (resolve_return_value_register) {
+            return_value_register = static_cast<Integer*>(fetch(return_value_register))->value();
+        }
+        place(return_value_register, returned);
+        if (returned_is_reference) {
+            uregset->flag(return_value_register, REFERENCE);
+        }
+    }
+
+    return return_address;
+}
 
 
 byte* CPU::begin() {
