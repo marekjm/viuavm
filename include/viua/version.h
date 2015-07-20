0f9353eee4abc9488a24e8096aa7d01ca59822a4
Marek Marecki <marekjm@ozro.pw>
2015-07-20 20:39:00 +0200

    Micro version bump after a rebase with devel branch

--- include/viua/version.h
+++ include/viua/version.h
@@ -4,11 +4,11 @@
 #pragma once
 
 const char* VERSION = "0.5.0";
-const char* MICRO = "51";
+const char* MICRO = "70";
 
 // commit pointed by the hash below is HEAD~1
 // while the MICRO variable shows actual number of commits added since last release,
 // the commit hash is off-by-one
-const char* COMMIT = "23361e51ceaa23e757e4ab78c055f96dc90be010";
+const char* COMMIT = "23f82db5f2a2810437ad7c33723ba7cf56d99c6f";
 
 #endif
