d52ca05854cde7bb5ec08e24d1ff849597eab2c4
Marek Marecki <marekjm@ozro.pw>
2015-07-20 19:19:40 +0200

    Test programs now use CALL to call foreign functions

--- sample/asm/external/hello_world.asm
+++ sample/asm/external/hello_world.asm
@@ -1,8 +1,10 @@
+.signature: World::print_hello
+
 .function: main
     eximport "build/test/World"
 
     frame 0
-    excall 0 World::print_hello
+    call 0 World::print_hello
 
     izero 0
     end
