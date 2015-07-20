ca36e989b74ef762273583bf946f3c822cb9a2d1
Marek Marecki <marekjm@ozro.pw>
2015-07-20 23:06:53 +0200

    Closes #82, breaks bytecode compatibility with older versions

--- include/viua/version.h
+++ include/viua/version.h
@@ -4,11 +4,11 @@
 #pragma once
 
 const char* VERSION = "0.5.0";
-const char* MICRO = "70";
+const char* MICRO = "74";
 
 // commit pointed by the hash below is HEAD~1
 // while the MICRO variable shows actual number of commits added since last release,
 // the commit hash is off-by-one
-const char* COMMIT = "23f82db5f2a2810437ad7c33723ba7cf56d99c6f";
+const char* COMMIT = "bd8e3a43e927655888774ec083f534c414f86128";
 
 #endif
