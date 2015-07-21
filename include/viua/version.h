637e78bc54de78134d6146673a7e99331d9526fe
Marek Marecki <marekjm@ozro.pw>
2015-07-21 23:02:46 +0200

    Micro version bump, implemented VIUAPRELINK and VIUAPREIMPORT

--- include/viua/version.h
+++ include/viua/version.h
@@ -4,11 +4,11 @@
 #pragma once
 
 const char* VERSION = "0.5.0";
-const char* MICRO = "81";
+const char* MICRO = "95";
 
 // commit pointed by the hash below is HEAD~1
 // while the MICRO variable shows actual number of commits added since last release,
 // the commit hash is off-by-one
-const char* COMMIT = "eb85132f533dffe723d77999eb435e30aa1649f6";
+const char* COMMIT = "d51c86f32dd8ad91e78c5d0b61a1b545e1315117";
 
 #endif
