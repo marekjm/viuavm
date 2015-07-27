cf3d5b29f005996909f0539473ee50ca52c405d9
Marek Marecki <marekjm@ozro.pw>
2015-07-27 23:45:45 +0200

    Using compiler to detect required frame size

--- sample/asm/functions/return_by_reference.asm
+++ sample/asm/functions/return_by_reference.asm
@@ -7,13 +7,15 @@
     ; store a number in register
     istore 1 69
 
-    frame 1
     ; pass it by reference to the function
-    paref 0 1
+    frame ^[(paref 0 1)]
+
     ; store return value in another register (it is a reference!)
     call 2 by_reference
+
     ; assign different value to it
     istore 2 42
+
     ; check if return-by-reference is working (should print 42)
     print 1
 
