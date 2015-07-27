11dfc2e5f459e50bbdf21b9e4d796512b7a49afb
Marek Marecki <marekjm@ozro.pw>
2015-07-27 22:42:08 +0200

    Small rewrite in VINSERT test program

--- sample/asm/vector/vinsert.asm
+++ sample/asm/vector/vinsert.asm
@@ -4,15 +4,13 @@
     .name: 4 ima
     .name: 5 sheep
 
-    strstore hurr "Hurr"
     strstore durr "durr"
     strstore ima "Im'a"
-    strstore sheep "sheep!"
 
     vec 1
 
-    vinsert 1 sheep
-    vinsert 1 hurr
+    vinsert 1 (strstore sheep "sheep!")
+    vinsert 1 (strstore hurr "Hurr")
     vinsert 1 durr 1
     vinsert 1 ima 2
 
