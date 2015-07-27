66c0043bc1c678fcdff48887088036d3d77da973
Marek Marecki <marekjm@ozro.pw>
2015-07-27 23:49:35 +0200

    Shortened code in static registers test program

--- sample/asm/functions/static_registers.asm
+++ sample/asm/functions/static_registers.asm
@@ -2,14 +2,9 @@
     ; switch to static register set
     ress static
 
-    ; check if 1 register is null
-    isnull 2 1
-    ; invert the check (check if register 1 is *not null*)
-    not 2
-
-    ; if register 1 is *not null* jump to :increase marker
+    ; if register 1 is *not null* jump to increase marker
     ; otherwise continue execution
-    branch 2 increase
+    branch (not (isnull 2 1)) increase
 
     ; these instructions are executed only when 1 register was null
     istore 1 0
@@ -23,17 +18,13 @@
     ress local
     tmpro 1
 
-    arg 3 0
-
-    ; integer at 1 is less than N
-    ilt 4 1 3
     ; integer at 1 is *at least* N
-    not 4
-    branch 4 finish
+    ; N is the parameter the function received
+    branch (not (ilt 4 1 (arg 3 0))) finish
+
     .mark: report
     print 1
-    frame 1
-    param 0 3
+    frame ^[(param 0 3)]
     call 0 counter
 
     .mark: finish
@@ -41,11 +32,7 @@
 .end
 
 .function: main
-    istore 1 10
-    frame 1
-    param 0 1
-    call 0 counter
-
-    izero 0
+    frame ^[(param 0 (istore 1 10))]
+    call ^(izero 0) counter
     end
 .end
