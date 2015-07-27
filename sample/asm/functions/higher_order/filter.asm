0e74214b6a905f24de6c55cb2a83a3ba549b863c
Marek Marecki <marekjm@ozro.pw>
2015-07-27 23:59:08 +0200

    Shortened filter/2 test program

--- sample/asm/functions/higher_order/filter.asm
+++ sample/asm/functions/higher_order/filter.asm
@@ -1,23 +1,17 @@
 .function: is_divisible_by_2
     arg 1 0
-
     istore 2 2
 
     .mark: loop_begin
-    ilt 3 1 2
-    branch 3 loop_end loop_body
+    branch (ilt 3 1 2) loop_end +1
 
-    .mark: loop_body
     isub 1 1 2
     jump loop_begin
 
-    .mark: loop_end
-
-    move 0 1
-
     ; make zero "true" and
     ; non-zero values "false"
-    not 0
+    .mark: loop_end
+    not (move 0 1)
 
     end
 .end
@@ -40,21 +34,15 @@
 
     ; while (...) {
     .mark: loop_begin
-    igte 6 4 5
-    branch 6 loop_end loop_body
-
-    .mark: loop_body
+    branch (igte 6 4 5) loop_end +1
 
     ; call filtering function to determine whether current element
-    ; is a valid value
-    frame 1
-    vat 7 2 @4
-    param 0 7
-    fcall 8 1
+    ; is a valid value...
+    frame ^[(param 0 (vat 7 2 @4))]
 
-    ; if the result from filtering function was "true" - the element should be pushed onto result vector
+    ; ...and if the result from filtering function was "true" - the element should be pushed onto result vector
     ; it it was "false" - skip to next iteration
-    branch 8 element_ok next_iter
+    branch (fcall 8 1) element_ok next_iter
 
     .mark: element_ok
     vpush 3 7
@@ -77,29 +65,16 @@
 .end
 
 .function: main
-    vec 1
-
-    istore 2 1
-    vpush 1 2
-    istore 2 2
-    vpush 1 2
-    istore 2 3
-    vpush 1 2
-    istore 2 4
-    vpush 1 2
-    istore 2 5
-    vpush 1 2
+    vpush (vec 1) (istore 2 1)
+    vpush 1 (istore 2 2)
+    vpush 1 (istore 2 3)
+    vpush 1 (istore 2 4)
+    vpush 1 (istore 2 5)
 
     print 1
 
-    function 3 is_divisible_by_2
-
-    frame 2
-    param 0 3
-    paref 1 1
-    call 4 filter
-
-    print 4
+    frame ^[(param 0 (function 3 is_divisible_by_2)) (paref 1 1)]
+    print (call 4 filter)
 
     izero 0
     end
