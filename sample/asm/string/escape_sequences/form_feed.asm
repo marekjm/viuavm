69579ded5f4247cbc288efbe169caeea5a6ba367
Marek Marecki <marekjm@ozro.pw>
2015-08-05 19:40:22 +0200

    Add tests for escape sequences printing

---
+++ sample/asm/string/escape_sequences/form_feed.asm
@@ -0,0 +1,6 @@
+.function: main
+    strstore 1 "Hello \fWorld!"
+    print 1
+    izero 0
+    end
+.end
---
+++ sample/asm/string/escape_sequences/tab.asm
@@ -0,0 +1,6 @@
+.function: main
+    strstore 1 "Hello\tWorld!"
+    print 1
+    izero 0
+    end
+.end
