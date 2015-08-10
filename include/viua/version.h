1e6ff5eef61500173d98f21382a1e757d171b271
Marek Marecki <marekjm@ozro.pw>
2015-08-10 22:33:22 +0200

    Micro version bump, foreign methods and prototypes can be registered in machine's typesystem

--- include/viua/version.h
+++ include/viua/version.h
@@ -4,11 +4,11 @@
 #pragma once
 
 const char* VERSION = "0.5.0";
-const char* MICRO = "274";
+const char* MICRO = "314";
 
 // commit pointed by the hash below is HEAD~1
 // while the MICRO variable shows actual number of commits added since last release,
 // the commit hash is off-by-one
-const char* COMMIT = "ef0e66a5991bff278c8b0c15bd3b7b5fd9a74dff";
+const char* COMMIT = "7b5fe96e876b54e573f967988ed00a09da3f4d97";
 
 #endif
