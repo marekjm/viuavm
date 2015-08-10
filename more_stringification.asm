7b5fe96e876b54e573f967988ed00a09da3f4d97
Marek Marecki <marekjm@ozro.pw>
2015-08-10 22:32:20 +0200

    Add sample program using String::stringify and String::represent

---
+++ more_stringification.asm
@@ -0,0 +1,31 @@
+.function: std::string::stringify
+    strstore 1 ""
+    frame ^[(paref 0 (arg 2 0))]
+    msg 1 1 stringify
+    move 0 1
+    end
+.end
+
+.function: std::string::represent
+    strstore 1 ""
+    frame ^[(paref 0 (arg 2 0))]
+    msg 1 1 represent
+    move 0 1
+    end
+.end
+
+.function: main
+    strstore 1 "Hello World!"
+
+    frame ^[(paref 0 1)]
+    call 1 std::string::stringify
+
+    frame ^[(paref 0 1)]
+    call 2 std::string::represent
+
+    print 1
+    print 2
+
+    izero 0
+    end
+.end
