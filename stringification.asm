a3f9615b3d1addae029746379d96efbfa1740da3
Marek Marecki <marekjm@ozro.pw>
2015-08-20 00:34:51 +0200

    Update sample stringification program to use new std::string functions

--- stringification.asm
+++ stringification.asm
@@ -1,7 +1,8 @@
-.signature: viua::core::string::stringify
+.signature: std::string::stringify
+.signature: std::string::represent
 
 .function: main
-    link string
+    link std::string
 
     ; store an object...
     new 1 Object
@@ -14,11 +15,15 @@
     ;
     ; let's do this!
     frame ^[(paref 0 1)]
-    call 2 viua::core::string::stringify
+    call 2 std::string::stringify
 
     ; the output will be *identical* to the first print instruction
     ; it would be different if we passed the Object by value
     print 2
+
+    frame ^[(paref 0 1)]
+    call 3 std::string::represent
+    print 3
 
     izero 0
     end
