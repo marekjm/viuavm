4a9389be9c7a6745f57c645a9d4b287a875f90b3
Marek Marecki <marekjm@ozro.pw>
2015-09-12 22:29:51 +0200

    Small version bump

--- include/viua/version.h
+++ include/viua/version.h
@@ -4,11 +4,11 @@
 #pragma once
 
 const char* VERSION = "0.5.0";
-const char* MICRO = "408";
+const char* MICRO = "417";
 
 // commit pointed by the hash below is HEAD~1
 // while the MICRO variable shows actual number of commits added since last release,
 // the commit hash is off-by-one
-const char* COMMIT = "0e8bb57b7a22a4c7bfe5e5b176efae51a0a5bc42";
+const char* COMMIT = "b1f2ad3101d5ba5a954209fb5c46db0581e68cd6";
 
 #endif
