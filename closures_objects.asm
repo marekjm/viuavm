d002adf66b374eb8cacf4d63c08f861de5be2ed3
Marek Marecki <marekjm@ozro.pw>
2015-09-12 22:45:31 +0200

    Fix old closure-based objects example to compile under new assembly

--- closures_objects.asm
+++ closures_objects.asm
@@ -2,7 +2,7 @@
     ; this is the setter method
 
     ; store 0th argument in 2nd register
-    arg 0 2
+    arg 2 0
 
     ; store an Integer in 1st register
     ; use value found in 2nd register as input
@@ -19,7 +19,7 @@
     ; register 0 is used as a return value register so
     ; this method will return a copy of the Integer being held
     ; by the simulated closure-based object
-    copy 1 0
+    copy 0 1
 
     ; end the function
     end
@@ -32,19 +32,19 @@
     ; because the "set_x" closure uses it to store the argument it gets
 
     ; store 0th argument in 1st register
-    arg 0 1
+    arg 1 0
 
     ; tell the CPU to bind 1st register in a closure
     clbind 1
     ; create a closure from function "set_x" in 3rd register
-    closure set_x 3
+    closure 3 set_x
 
     ; tell the CPU to bind 1st register in a closure
     ; this is required because CLOSURE opcode resets flags set
     ; by CLBIND opcode
     clbind 1
     ; create a closure from function "get_x" in 4th register
-    closure get_x 4
+    closure 4 get_x
 
     ; this will be the return value of the our constructor
     ; create a vector in 0th register...
@@ -68,7 +68,7 @@
     ; defined earlier
     frame 1
     param 0 1
-    call object 2
+    call 2 object
 
     ; print the object
     ; actually, second register is just a vector of closures so
@@ -85,42 +85,41 @@
     ; using vector in 2nd register,
     ; create a reference in register named 'get_x' that will point to
     ; index 1 in the vector being used
-    vat 2 get_x 1
+    vat get_x 2 1
     ; print what was returned
     print get_x
 
     ; create closure frame
-    clframe 0
+    frame 0
     ; call the getter-closure
     ; the call will store a copy of the Intger that
     ; was passed to the constructor function
-    clcall get_x 5
+    fcall 5 get_x
     print 5
 
 
     ; extract method "set_x" from the object
     .name: 4 set_x
-    vat 2 set_x 0
+    vat set_x 2 0
     print set_x
 
     ; set object's value to 69 (it was 42 before) using the setter-closure
     istore 6 69
-    clframe 1
+    frame 1
     ; set 0th parameter to be a copy of the object from 6th register
     param 0 6
-    clcall set_x 0
+    fcall 0 set_x
 
     ; then, call "get_x" once more to access the value stored by the
     ; simulated object
     ; remember - closure registers are bound so the call to "set_x" updated
     ; the same memory that "get_x" accesses
-    clframe 0
-    clcall get_x 5
+    frame 0
+    fcall 5 get_x
 
     ; print what was returned and
     ; see the Integer is now 69
     print 5
-
 
     izero 0
     end
