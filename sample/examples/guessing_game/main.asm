642efed31601f0018a4c4de0f38464a7590121cc
Marek Marecki <marekjm@ozro.pw>
2015-09-18 18:14:45 +0200

    Add "Guessing Game" example (TODO)

---
+++ sample/examples/guessing_game/main.asm
@@ -0,0 +1,7 @@
+.function: main
+    ; FIXME: implement this chapter of "The Rust Book": https://doc.rust-lang.org/book/guessing-game.html
+    print (strstore 1 "Hello World!")
+
+    izero 0
+    end
+.end
