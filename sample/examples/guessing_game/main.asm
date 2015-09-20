f096d7e51a9a85694509566e4e26fedc1bcedeff
Marek Marecki <marekjm@ozro.pw>
2015-09-20 14:59:00 +0200

    Initial code for "Guessing game" example

--- sample/examples/guessing_game/main.asm
+++ sample/examples/guessing_game/main.asm
@@ -1,6 +1,12 @@
+.signature: std::random::randint
+
 .function: main
     ; FIXME: implement this chapter of "The Rust Book": https://doc.rust-lang.org/book/guessing-game.html
-    print (strstore 1 "Hello World!")
+    import "random"
+
+    ; random integer between 0 and 100
+    frame ^[(param 0 (istore 2 0)) (param 1 (istore 2 100))]
+    print (call 1 std::random::randint)
 
     izero 0
     end
