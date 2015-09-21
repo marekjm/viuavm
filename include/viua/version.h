eb10dd5551bba94b0f2172632abf7bb0c08a7e15
Marek Marecki <marekjm@ozro.pw>
2015-09-21 22:58:28 +0200

    New try-catch syntax (fixes #92), version bump

--- include/viua/version.h
+++ include/viua/version.h
@@ -4,11 +4,11 @@
 #pragma once
 
 const char* VERSION = "0.5.0";
-const char* MICRO = "417";
+const char* MICRO = "461";
 
 // commit pointed by the hash below is HEAD~1
 // while the MICRO variable shows actual number of commits added since last release,
 // the commit hash is off-by-one
-const char* COMMIT = "b1f2ad3101d5ba5a954209fb5c46db0581e68cd6";
+const char* COMMIT = "cf44a2fa5609aeaac12752f9f7b71794e0933fb1";
 
 #endif
