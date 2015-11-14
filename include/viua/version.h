96c557ced07c6a5b072b0d9f3feffe5e08dc3226
Marek Marecki <marekjm@ozro.pw>
2015-11-14 01:33:20 +0100

    Basic threading support merged into devel
    Closes #87

--- include/viua/version.h
+++ include/viua/version.h
@@ -4,11 +4,11 @@
 #pragma once
 
 const char* VERSION = "0.6.0";
-const char* MICRO = "133";
+const char* MICRO = "147";
 
 // commit pointed by the hash below is HEAD~1
 // while the MICRO variable shows actual number of commits added since last release,
 // the commit hash is off-by-one
-const char* COMMIT = "2d07773e61918e1b96ff51c2e969d64d50dc3a64";
+const char* COMMIT = "6b9a227984b8a0b1b1b30b2048ee0a3cd2904091";
 
 #endif
