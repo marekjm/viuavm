c2f141debe0735732dee0d0379f3613bc8aad58d
Marek Marecki <marekjm@ozro.pw>
2015-11-14 17:42:01 +0100

    Merged thread-priority messages into devel
    Version bump.
    Closes #103

--- include/viua/version.h
+++ include/viua/version.h
@@ -3,12 +3,12 @@
 
 #pragma once
 
-const char* VERSION = "0.6.0";
-const char* MICRO = "147";
+const char* VERSION = "0.6.1";
+const char* MICRO = "15";
 
 // commit pointed by the hash below is HEAD~1
 // while the MICRO variable shows actual number of commits added since last release,
 // the commit hash is off-by-one
-const char* COMMIT = "6b9a227984b8a0b1b1b30b2048ee0a3cd2904091";
+const char* COMMIT = "5ef8d7e68d53630a1feba37aef0be6d894a5982b";
 
 #endif
