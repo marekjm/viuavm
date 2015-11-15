03b2166a86b87ed4df4d049fcb97bbb72e75e0b4
Marek Marecki <marekjm@ozro.pw>
2015-11-15 01:11:10 +0100

    Merged message passing into devel
    Closes #105

--- include/viua/version.h
+++ include/viua/version.h
@@ -4,11 +4,11 @@
 #pragma once
 
 const char* VERSION = "0.6.1";
-const char* MICRO = "15";
+const char* MICRO = "28";
 
 // commit pointed by the hash below is HEAD~1
 // while the MICRO variable shows actual number of commits added since last release,
 // the commit hash is off-by-one
-const char* COMMIT = "5ef8d7e68d53630a1feba37aef0be6d894a5982b";
+const char* COMMIT = "2a90c02d039869e447b7e0f2f752465a85b1012f";
 
 #endif
