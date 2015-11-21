f3eaa0f406afd791ae970922fe60a7bc73a8167d
Marek Marecki <marekjm@ozro.pw>
2015-11-21 13:53:19 +0100

    One-part-operand extraction template

--- include/viua/cpu/opex.h
+++ include/viua/cpu/opex.h
@@ -11,6 +11,13 @@ namespace viua {
         namespace util {
             void extractIntegerOperand(byte*& instruction_stream, bool& boolean, int& integer);
             void extractFloatingPointOperand(byte*& instruction_stream, float& fp);
+
+            template <class T> void extractOperand(byte*& instruction_stream, T& operand) {
+                T ex_operand;
+                ex_operand = *(reinterpret_cast<T*>(instruction_stream));
+                pointer::inc<T, byte>(instruction_stream);
+                operand = ex_operand;
+            }
         }
     }
 }
