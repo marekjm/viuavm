c6e5b2464974659ab263ff678727e9b2492e9582
Marek Marecki <marekjm@ozro.pw>
2015-11-22 23:16:03 +0100

    Rewrite String::size test

--- sample/types/String/size.asm
+++ sample/types/String/size.asm
@@ -1,10 +1,6 @@
 .function: main
-    strstore 1 "Hello World!"
-
-    frame ^[(param 0 1)]
-    msg 2 size
-
-    print 2
+    frame ^[(param 0 (strstore 1 "Hello World!"))]
+    print (msg 2 size)
 
     izero 0
     end
