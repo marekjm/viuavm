dad4681e2dcc185ee870383f7813a8a00921f7f0
Marek Marecki <marekjm@ozro.pw>
2015-11-23 00:32:09 +0100

    Add test for String::format method

---
+++ sample/types/String/format.asm
@@ -0,0 +1,9 @@
+.function: main
+    vpush (vpush (vec 1) (strstore 2 "formatted")) (strstore 2 "World")
+
+    frame ^[(param 0 (strstore 2 "Hello, #{0} #{1}!")) (param 1 1)]
+    print (msg 3 format)
+
+    izero 0
+    end
+.end
