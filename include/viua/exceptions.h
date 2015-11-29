4f3b6e3d03f0fb55212afb7b7b075d36c7de0839
Marek Marecki <marekjm@ozro.pw>
2015-11-29 02:00:54 +0100

    Add ArityException

--- include/viua/exceptions.h
+++ include/viua/exceptions.h
@@ -16,5 +16,38 @@ class OutOfRangeException: public Exception {
         OutOfRangeException(const std::string& s): cause(s) {}
 };
 
+class ArityException: public Exception {
+        unsigned long got_arity;
+        std::vector<decltype(got_arity)> valid_arities;
+    public:
+        std::string type() const override {
+            return "ArityException";
+        }
+
+        std::string str() const override {
+            std::ostringstream oss;
+            oss << "got arity " << got_arity << ", expected one of {";
+            for (decltype(valid_arities)::size_type i = 0; i < valid_arities.size(); ++i) {
+                oss << valid_arities[i];
+                if (i < (valid_arities.size()-1)) {
+                    oss << ", ";
+                }
+            }
+            oss << '}';
+            return oss.str();
+        }
+
+        Type* copy() const override {
+            return new ArityException(got_arity, valid_arities);
+        }
+
+        std::string what() const override {
+            return str();
+        }
+
+        ArityException(decltype(got_arity) a, decltype(valid_arities) v): got_arity(a), valid_arities(v) {}
+        ~ArityException() {}
+};
+
 
 #endif
