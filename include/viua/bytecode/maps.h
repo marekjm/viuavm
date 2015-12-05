83e82a155af79b0806b46e0d4280f0bc773306b3
Marek Marecki <marekjm@ozro.pw>
2015-12-05 13:18:54 +0100

    Change boolean flag before register index operands to OperandType

--- include/viua/bytecode/maps.h
+++ include/viua/bytecode/maps.h
@@ -8,103 +8,104 @@
 #include <vector>
 #include <string>
 #include <viua/bytecode/opcodes.h>
+#include <viua/bytecode/operand_types.h>
 
 
 const std::map<std::string, unsigned> OP_SIZES = {
     { "nop",    sizeof(byte) },
 
-    { "izero",  sizeof(byte) + sizeof(bool) + sizeof(int) },
-    { "istore", sizeof(byte) + 2*sizeof(bool) + 2*sizeof(int) },
-    { "iadd",   sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "isub",   sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "imul",   sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "idiv",   sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "iinc",   sizeof(byte) + sizeof(bool) + sizeof(int) },
-    { "idec",   sizeof(byte) + sizeof(bool) + sizeof(int) },
-    { "ilt",    sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "ilte",   sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "igt",    sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "igte",   sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "ieq",    sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
+    { "izero",  sizeof(byte) + sizeof(OperandType) + sizeof(int) },
+    { "istore", sizeof(byte) + 2*sizeof(OperandType) + 2*sizeof(int) },
+    { "iadd",   sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "isub",   sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "imul",   sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "idiv",   sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "iinc",   sizeof(byte) + sizeof(OperandType) + sizeof(int) },
+    { "idec",   sizeof(byte) + sizeof(OperandType) + sizeof(int) },
+    { "ilt",    sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "ilte",   sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "igt",    sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "igte",   sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "ieq",    sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
 
-    { "fstore", sizeof(byte) + sizeof(bool) + sizeof(int) + sizeof(float) },
-    { "fadd",   sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "fsub",   sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "fmul",   sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "fdiv",   sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "flt",    sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "flte",   sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "fgt",    sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "fgte",   sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "feq",    sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
+    { "fstore", sizeof(byte) + sizeof(OperandType) + sizeof(int) + sizeof(float) },
+    { "fadd",   sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "fsub",   sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "fmul",   sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "fdiv",   sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "flt",    sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "flte",   sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "fgt",    sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "fgte",   sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "feq",    sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
 
-    { "bstore", sizeof(byte) + 2*sizeof(bool) + sizeof(int) + sizeof(byte) },
-    { "badd",   sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "bsub",   sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "binc",   sizeof(byte) + sizeof(bool) + sizeof(int) },
-    { "bdec",   sizeof(byte) + sizeof(bool) + sizeof(int) },
-    { "blt",    sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "blte",   sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "bgt",    sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "bgte",   sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "beq",    sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
+    { "bstore", sizeof(byte) + 2*sizeof(OperandType) + sizeof(int) + sizeof(byte) },
+    { "badd",   sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "bsub",   sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "binc",   sizeof(byte) + sizeof(OperandType) + sizeof(int) },
+    { "bdec",   sizeof(byte) + sizeof(OperandType) + sizeof(int) },
+    { "blt",    sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "blte",   sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "bgt",    sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "bgte",   sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "beq",    sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
 
-    { "itof",   sizeof(byte) + 2*sizeof(bool) + 2*sizeof(int) },
-    { "ftoi",   sizeof(byte) + 2*sizeof(bool) + 2*sizeof(int) },
-    { "stoi",   sizeof(byte) + 2*sizeof(bool) + 2*sizeof(int) },
-    { "stof",   sizeof(byte) + 2*sizeof(bool) + 2*sizeof(int) },
+    { "itof",   sizeof(byte) + 2*sizeof(OperandType) + 2*sizeof(int) },
+    { "ftoi",   sizeof(byte) + 2*sizeof(OperandType) + 2*sizeof(int) },
+    { "stoi",   sizeof(byte) + 2*sizeof(OperandType) + 2*sizeof(int) },
+    { "stof",   sizeof(byte) + 2*sizeof(OperandType) + 2*sizeof(int) },
 
-    { "strstore",sizeof(byte) + sizeof(bool) + sizeof(int) },
-    { "streq",  sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
+    { "strstore",sizeof(byte) + sizeof(OperandType) + sizeof(int) },
+    { "streq",  sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
 
-    { "vec",    sizeof(byte) + sizeof(bool) + sizeof(int) },
-    { "vinsert",sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "vpush",  sizeof(byte) + 2*sizeof(bool) + 2*sizeof(int) },
-    { "vpop",   sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "vat",    sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "vlen",   sizeof(byte) + 2*sizeof(bool) + 2*sizeof(int) },
+    { "vec",    sizeof(byte) + sizeof(OperandType) + sizeof(int) },
+    { "vinsert",sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "vpush",  sizeof(byte) + 2*sizeof(OperandType) + 2*sizeof(int) },
+    { "vpop",   sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "vat",    sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "vlen",   sizeof(byte) + 2*sizeof(OperandType) + 2*sizeof(int) },
 
-    { "bool",   sizeof(byte) + sizeof(bool) + sizeof(int) },
-    { "not",    sizeof(byte) + sizeof(bool) + sizeof(int) },
-    { "and",    sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
-    { "or",     sizeof(byte) + 3*sizeof(bool) + 3*sizeof(int) },
+    { "bool",   sizeof(byte) + sizeof(OperandType) + sizeof(int) },
+    { "not",    sizeof(byte) + sizeof(OperandType) + sizeof(int) },
+    { "and",    sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
+    { "or",     sizeof(byte) + 3*sizeof(OperandType) + 3*sizeof(int) },
 
-    { "move",   sizeof(byte) + 2*sizeof(bool) + 2*sizeof(int) },
-    { "copy",   sizeof(byte) + 2*sizeof(bool) + 2*sizeof(int) },
-    { "ref",    sizeof(byte) + 2*sizeof(bool) + 2*sizeof(int) },
-    { "swap",   sizeof(byte) + 2*sizeof(bool) + 2*sizeof(int) },
-    { "free",   sizeof(byte) + sizeof(bool) + sizeof(int) },
-    { "empty",  sizeof(byte) + sizeof(bool) + sizeof(int) },
-    { "isnull", sizeof(byte) + 2*sizeof(bool) + 2*sizeof(int) },
+    { "move",   sizeof(byte) + 2*sizeof(OperandType) + 2*sizeof(int) },
+    { "copy",   sizeof(byte) + 2*sizeof(OperandType) + 2*sizeof(int) },
+    { "ref",    sizeof(byte) + 2*sizeof(OperandType) + 2*sizeof(int) },
+    { "swap",   sizeof(byte) + 2*sizeof(OperandType) + 2*sizeof(int) },
+    { "free",   sizeof(byte) + sizeof(OperandType) + sizeof(int) },
+    { "empty",  sizeof(byte) + sizeof(OperandType) + sizeof(int) },
+    { "isnull", sizeof(byte) + 2*sizeof(OperandType) + 2*sizeof(int) },
     { "ress",   sizeof(byte) + sizeof(int) },
-    { "tmpri",  sizeof(byte) + sizeof(bool) + sizeof(int) },
-    { "tmpro",  sizeof(byte) + sizeof(bool) + sizeof(int) },
+    { "tmpri",  sizeof(byte) + sizeof(OperandType) + sizeof(int) },
+    { "tmpro",  sizeof(byte) + sizeof(OperandType) + sizeof(int) },
 
-    { "print",  sizeof(byte) + sizeof(bool) + sizeof(int) },
-    { "echo",   sizeof(byte) + sizeof(bool) + sizeof(int) },
+    { "print",  sizeof(byte) + sizeof(OperandType) + sizeof(int) },
+    { "echo",   sizeof(byte) + sizeof(OperandType) + sizeof(int) },
 
-    { "clbind", sizeof(byte) + sizeof(bool) + sizeof(int) },
-    { "closure",sizeof(byte) + sizeof(bool) + sizeof(int) },
+    { "clbind", sizeof(byte) + sizeof(OperandType) + sizeof(int) },
+    { "closure",sizeof(byte) + sizeof(OperandType) + sizeof(int) },
 
-    { "function",sizeof(byte) + sizeof(bool) + sizeof(int) },
-    { "fcall",  sizeof(byte) + 2*sizeof(bool) + 2*sizeof(int) },
+    { "function",sizeof(byte) + sizeof(OperandType) + sizeof(int) },
+    { "fcall",  sizeof(byte) + 2*sizeof(OperandType) + 2*sizeof(int) },
 
-    { "frame",  sizeof(byte) + 2*sizeof(bool) + 2*sizeof(int) },
-    { "param",  sizeof(byte) + 2*sizeof(bool) + 2*sizeof(int) },
-    { "paref",  sizeof(byte) + 2*sizeof(bool) + 2*sizeof(int) },
-    { "call",   sizeof(byte) + sizeof(bool) + sizeof(int) },
-    { "arg",    sizeof(byte) + 2*sizeof(bool) + 2*sizeof(int) },
-    { "argc",   sizeof(byte) + sizeof(bool) + sizeof(int) },
-    { "thread", sizeof(byte) + sizeof(bool) + sizeof(int) },
-    { "thjoin", sizeof(byte) + sizeof(bool) + sizeof(int) },
-    { "threceive", sizeof(byte) + sizeof(bool) + sizeof(int) },
+    { "frame",  sizeof(byte) + 2*sizeof(OperandType) + 2*sizeof(int) },
+    { "param",  sizeof(byte) + 2*sizeof(OperandType) + 2*sizeof(int) },
+    { "paref",  sizeof(byte) + 2*sizeof(OperandType) + 2*sizeof(int) },
+    { "call",   sizeof(byte) + sizeof(OperandType) + sizeof(int) },
+    { "arg",    sizeof(byte) + 2*sizeof(OperandType) + 2*sizeof(int) },
+    { "argc",   sizeof(byte) + sizeof(OperandType) + sizeof(int) },
+    { "thread", sizeof(byte) + sizeof(OperandType) + sizeof(int) },
+    { "thjoin", sizeof(byte) + sizeof(OperandType) + sizeof(int) },
+    { "threceive", sizeof(byte) + sizeof(OperandType) + sizeof(int) },
 
     { "jump",   sizeof(byte) + sizeof(uint64_t) },
-    { "branch", sizeof(byte) + sizeof(bool)+sizeof(int) + 2*sizeof(uint64_t) },
+    { "branch", sizeof(byte) + sizeof(OperandType)+sizeof(int) + 2*sizeof(uint64_t) },
 
-    { "throw",  sizeof(byte) + sizeof(int) + sizeof(bool) },
+    { "throw",  sizeof(byte) + sizeof(OperandType) + sizeof(int) },
     { "catch",  sizeof(byte) }, // catch "<type>" <block>
-    { "pull",   sizeof(byte) + sizeof(bool) + sizeof(int) }, // pull <register>
+    { "pull",   sizeof(byte) + sizeof(OperandType) + sizeof(int) }, // pull <register>
     { "try",    sizeof(byte) },
     { "enter",  sizeof(byte) },
     { "leave",  sizeof(byte) },
@@ -112,14 +113,14 @@ const std::map<std::string, unsigned> OP_SIZES = {
     { "import", sizeof(byte) },
     { "link",   sizeof(byte) },
 
-    { "class",  sizeof(byte) + sizeof(bool) + sizeof(int) },    // class 1 Foo
-    { "prototype",sizeof(byte) + sizeof(bool) + sizeof(int) },  // prototype 1 Foo
-    { "derive", sizeof(byte) + sizeof(bool) + sizeof(int) },    // derive 1 Bar
-    { "attach", sizeof(byte) + sizeof(bool) + sizeof(int) },    // attach 1 function method
-    { "register",sizeof(byte) + sizeof(bool) + sizeof(int) },   // register 1
+    { "class",  sizeof(byte) + sizeof(OperandType) + sizeof(int) },    // class 1 Foo
+    { "prototype",sizeof(byte) + sizeof(OperandType) + sizeof(int) },  // prototype 1 Foo
+    { "derive", sizeof(byte) + sizeof(OperandType) + sizeof(int) },    // derive 1 Bar
+    { "attach", sizeof(byte) + sizeof(OperandType) + sizeof(int) },    // attach 1 function method
+    { "register",sizeof(byte) + sizeof(OperandType) + sizeof(int) },   // register 1
 
-    { "new",    sizeof(byte) + sizeof(bool) + sizeof(int) },    // new <target> Foo
-    { "msg",    sizeof(byte) + sizeof(bool) + sizeof(int) },    // msg <return> method
+    { "new",    sizeof(byte) + sizeof(OperandType) + sizeof(int) },    // new <target> Foo
+    { "msg",    sizeof(byte) + sizeof(OperandType) + sizeof(int) },    // msg <return> method
 
     { "end",    sizeof(byte) },
     { "halt",   sizeof(byte) },
