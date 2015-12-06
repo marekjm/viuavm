9c711d32ce36c2dd735978c95cebb92316278d30
Marek Marecki <marekjm@ozro.pw>
2015-12-06 20:32:57 +0100

    Fix build

--- include/viua/assert.h
+++ include/viua/assert.h
@@ -21,15 +21,7 @@ namespace viua {
             }
         }
 
-        void assert_typeof(Type* object, const std::string& expected) {
-            /** Use this assertion when strict type checking is required.
-             *
-             *  Example: checking if an object is an Integer.
-             */
-            if (object->type() != expected) {
-                throw new TypeException(expected, object->type());
-            }
-        }
+        void assert_typeof(Type* object, const std::string& expected);
 
 
         template<typename T, typename U> inline bool any_equal(const T& to_compare, const U& first) {
