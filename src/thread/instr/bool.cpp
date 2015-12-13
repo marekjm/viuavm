3d78e794eb61b0b469120af6baf5901ab69f2521
Marek Marecki <marekjm@ozro.pw>
2015-12-13 16:28:54 +0100

    Refactor Boolean instructions

--- src/thread/instr/bool.cpp
+++ src/thread/instr/bool.cpp
@@ -13,56 +13,28 @@ using namespace std;
 
 byte* Thread::lognot(byte* addr) {
     auto target = viua::operand::extract(addr);
+
     place(viua::operand::getRegisterIndexOrException(target.get(), this), new Boolean(not target->resolve(this)->boolean()));
+
     return addr;
 }
 
 byte* Thread::logand(byte* addr) {
-    /*  Run ieq instruction.
-     */
-    bool first_operand_ref, second_operand_ref, destination_register_ref;
-    int first_operand_index, second_operand_index, destination_register_index;
+    auto target = viua::operand::extract(addr);
+    auto first = viua::operand::extract(addr);
+    auto second = viua::operand::extract(addr);
 
-    viua::cpu::util::extractIntegerOperand(addr, destination_register_ref, destination_register_index);
-    viua::cpu::util::extractIntegerOperand(addr, first_operand_ref, first_operand_index);
-    viua::cpu::util::extractIntegerOperand(addr, second_operand_ref, second_operand_index);
-
-    if (destination_register_ref) {
-        first_operand_index = static_cast<Integer*>(fetch(first_operand_index))->value();
-    }
-    if (first_operand_ref) {
-        first_operand_index = static_cast<Integer*>(fetch(first_operand_index))->value();
-    }
-    if (second_operand_ref) {
-        second_operand_index = static_cast<Integer*>(fetch(second_operand_index))->value();
-    }
-
-    place(destination_register_index, new Boolean(fetch(first_operand_index)->boolean() and fetch(second_operand_index)->boolean()));
+    place(viua::operand::getRegisterIndexOrException(target.get(), this), new Boolean(first->resolve(this)->boolean() and second->resolve(this)->boolean()));
 
     return addr;
 }
 
 byte* Thread::logor(byte* addr) {
-    /*  Run ieq instruction.
-     */
-    bool first_operand_ref, second_operand_ref, destination_register_ref;
-    int first_operand_index, second_operand_index, destination_register_index;
+    auto target = viua::operand::extract(addr);
+    auto first = viua::operand::extract(addr);
+    auto second = viua::operand::extract(addr);
 
-    viua::cpu::util::extractIntegerOperand(addr, destination_register_ref, destination_register_index);
-    viua::cpu::util::extractIntegerOperand(addr, first_operand_ref, first_operand_index);
-    viua::cpu::util::extractIntegerOperand(addr, second_operand_ref, second_operand_index);
-
-    if (destination_register_ref) {
-        first_operand_index = static_cast<Integer*>(fetch(first_operand_index))->value();
-    }
-    if (first_operand_ref) {
-        first_operand_index = static_cast<Integer*>(fetch(first_operand_index))->value();
-    }
-    if (second_operand_ref) {
-        second_operand_index = static_cast<Integer*>(fetch(second_operand_index))->value();
-    }
-
-    place(destination_register_index, new Boolean(fetch(first_operand_index)->boolean() or fetch(second_operand_index)->boolean()));
+    place(viua::operand::getRegisterIndexOrException(target.get(), this), new Boolean(first->resolve(this)->boolean() or second->resolve(this)->boolean()));
 
     return addr;
 }
