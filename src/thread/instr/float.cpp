2077e466a77174f9fb9503b18946b28f704f4632
Marek Marecki <marekjm@ozro.pw>
2015-12-13 02:35:38 +0100

    Begin refactoring Float instructions

--- src/thread/instr/float.cpp
+++ src/thread/instr/float.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <functional>
 #include <viua/bytecode/bytetypedef.h>
 #include <viua/types/type.h>
 #include <viua/types/boolean.h>
 #include <viua/types/integer.h>
 #include <viua/types/float.h>
 #include <viua/cpu/opex.h>
+#include <viua/operand.h>
 #include <viua/cpu/cpu.h>
 using namespace std;
 
@@ -28,33 +30,35 @@ byte* Thread::fstore(byte* addr) {
     return addr;
 }
 
+// ObjectPlacer shall be used only as a type of Thread::place() function when passing it
+// to perform<>() implementation template.
+using ObjectPlacer = void(Thread::*)(unsigned,Type*);
+
+template<class Operator, class ResultType> byte* perform(byte* addr, Thread* t, ObjectPlacer placer) {
+    /** Heavily abstracted binary opcode implementation for Integer-related instructions.
+     *
+     *  First parameter - byte* addr - is the instruction pointer from which operand extraction should begin.
+     *
+     *  Second parameter - Thread* t - is a pointer to current VM thread of execution (passed as `this`).
+     *
+     *  Third parameter - ObjectPlacer - is a member-function pointer to Thread::place.
+     *  Since it is private, we have to cheat the compiler by extracting its pointer while in
+     *  Thread class's scope and passing it here.
+     *  Voila - we can place objects in thread's current register set.
+     */
+    unsigned target_register_index = viua::operand::getRegisterIndexOrException(viua::operand::extract(addr).get(), t);
+
+    auto first = viua::operand::extract(addr);
+    auto second = viua::operand::extract(addr);
+    (t->*placer)(target_register_index, new ResultType(Operator()(static_cast<Float*>(first->resolve(t))->value(), static_cast<Float*>(second->resolve(t))->value())));
+
+    return addr;
+}
+
 byte* Thread::fadd(byte* addr) {
     /*  Run fadd instruction.
      */
-    bool first_operand_ref, second_operand_ref, destination_register_ref;
-    int first_operand_index, second_operand_index, destination_register_index;
-
-    viua::cpu::util::extractIntegerOperand(addr, destination_register_ref, destination_register_index);
-    viua::cpu::util::extractIntegerOperand(addr, first_operand_ref, first_operand_index);
-    viua::cpu::util::extractIntegerOperand(addr, second_operand_ref, second_operand_index);
-
-    if (first_operand_ref) {
-        first_operand_index = static_cast<Integer*>(fetch(first_operand_index))->value();
-    }
-    if (second_operand_ref) {
-        second_operand_index = static_cast<Integer*>(fetch(second_operand_index))->value();
-    }
-    if (destination_register_ref) {
-        first_operand_index = static_cast<Integer*>(fetch(first_operand_index))->value();
-    }
-
-    float a, b;
-    a = static_cast<Float*>(fetch(first_operand_index))->value();
-    b = static_cast<Float*>(fetch(second_operand_index))->value();
-
-    place(destination_register_index, new Float(a + b));
-
-    return addr;
+    return perform<std::plus<float>, Float>(addr, this, &Thread::place);
 }
 
 byte* Thread::fsub(byte* addr) {
