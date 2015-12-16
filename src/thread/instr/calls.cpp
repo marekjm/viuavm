8c85ebc5cdb1bc6c4deabe2c932a1c4eeb74130e
Marek Marecki <marekjm@ozro.pw>
2015-12-16 22:49:49 +0100

    Small refactoring in PAREF instruction

--- src/thread/instr/calls.cpp
+++ src/thread/instr/calls.cpp
@@ -55,15 +55,19 @@ byte* Thread::param(byte* addr) {
 byte* Thread::paref(byte* addr) {
     /** Run paref instruction.
      */
-    int parameter_no_operand_index, object_operand_index;
-    bool parameter_no_operand_ref = false, object_operand_ref = false;
+    int parameter_no_operand_index;
 
-    viua::cpu::util::extractIntegerOperand(addr, parameter_no_operand_ref, parameter_no_operand_index);
-    viua::cpu::util::extractIntegerOperand(addr, object_operand_ref, object_operand_index);
-
-    if (parameter_no_operand_ref) {
-        parameter_no_operand_index = static_cast<Integer*>(fetch(parameter_no_operand_index))->value();
+    auto parameter_no_operand_index_source = viua::operand::extract(addr);
+    if (viua::operand::RegisterIndex* ri = dynamic_cast<viua::operand::RegisterIndex*>(parameter_no_operand_index_source.get())) {
+        parameter_no_operand_index = ri->get(this);
+    } else {
+        throw new Exception("invalid operand type");
     }
+
+    int object_operand_index;
+    bool object_operand_ref = false;
+
+    viua::cpu::util::extractIntegerOperand(addr, object_operand_ref, object_operand_index);
     if (object_operand_ref) {
         object_operand_index = static_cast<Integer*>(fetch(object_operand_index))->value();
     }
