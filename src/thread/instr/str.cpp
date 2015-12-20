2b163ca219fd96c71dbeda13d77094967b2320ab
Marek Marecki <marekjm@ozro.pw>
2015-12-20 15:19:53 +0100

    Refactor STRSTORE instruction

--- src/thread/instr/str.cpp
+++ src/thread/instr/str.cpp
@@ -7,26 +7,18 @@
 #include <viua/types/string.h>
 #include <viua/cpu/opex.h>
 #include <viua/support/string.h>
+#include <viua/operand.h>
 #include <viua/cpu/cpu.h>
 using namespace std;
 
 
 byte* Thread::strstore(byte* addr) {
-    /*  Run strstore instruction.
-     */
-    int reg;
-    bool reg_ref = false;
-
-    viua::cpu::util::extractIntegerOperand(addr, reg_ref, reg);
+    int target = viua::operand::getRegisterIndex(viua::operand::extract(addr).get(), this);
 
     string svalue = string(addr);
     addr += svalue.size()+1;
 
-    if (reg_ref) {
-        reg = static_cast<Integer*>(fetch(reg))->value();
-    }
-
-    place(reg, new String(str::strdecode(svalue)));
+    place(target, new String(str::strdecode(svalue)));
 
     return addr;
 }
