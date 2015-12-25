e682371c454f9f6ca860a9b4e0240a3be60767f8
Marek Marecki <marekjm@ozro.pw>
2015-12-25 22:48:43 +0100

    Fix a typo in test program name

--- tests/tests.py
+++ tests/tests.py
@@ -769,7 +769,7 @@ class MultithreadingTests(unittest.TestCase):
         # this will of course generate leaks, but we are not interested in them since
         # after process termination operating system will automatically reclaim memory
         MEMORY_LEAK_CHECKS_SKIP_LIST.append(self)
-        runTestSplitlines(self, 'main_orhpaning_threads.asm', ['Hello multithreaded World! (2)', 'fatal: aborting execution: main/1 orphaned threads, stack corrupted'], 1)
+        runTestSplitlines(self, 'main_orphaning_threads.asm', ['Hello multithreaded World! (2)', 'fatal: aborting execution: main/1 orphaned threads, stack corrupted'], 1)
 
     def testGettingPriorityOfAThread(self):
         runTest(self, 'get_priority.asm', '1')
