eb65d6824911863b67e0d3a8b18aa29649eb1066
Marek Marecki <marekjm@ozro.pw>
2016-01-03 23:27:16 +0100

    Refactor pointer expiration test

--- sample/types/Pointer/check_if_is_expired.asm
+++ sample/types/Pointer/check_if_is_expired.asm
@@ -1,20 +1,24 @@
 .signature: Pointer::expired
 
+.function: isExpired
+    frame ^[(param 0 (arg 1 0))]
+    call 2 Pointer::expired
+    echo (strstore 3 "expired: ")
+    move 0 (print 2)
+    end
+.end
+
 .function: main
     istore 1 42
     ptr 2 1
 
     frame ^[(param 0 2)]
-    call 3 Pointer::expired
-    echo (strstore 4 "expired: ")
-    print 3
+    call 0 isExpired
 
     delete 1
 
     frame ^[(param 0 2)]
-    call 3 Pointer::expired
-    echo 4
-    print 3
+    call 0 isExpired
 
     izero 0
     end
