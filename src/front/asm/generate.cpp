7984367cd94e66a59db8e12fba8e97ea7c0b4ee9
Marek Marecki <marekjm@ozro.pw>
2016-01-09 23:53:15 +0100

    Assembler recognizes PAMV instructions

--- src/front/asm/generate.cpp
+++ src/front/asm/generate.cpp
@@ -433,6 +433,10 @@ Program& compile(Program& program, const vector<string>& lines, map<string, int>
             string a_chnk, b_chnk;
             tie(a_chnk, b_chnk) = assembler::operands::get2(operands);
             program.param(assembler::operands::getint(resolveregister(a_chnk, names)), assembler::operands::getint(resolveregister(b_chnk, names)));
+        } else if (str::startswith(line, "pamv")) {
+            string a_chnk, b_chnk;
+            tie(a_chnk, b_chnk) = assembler::operands::get2(operands);
+            program.oppamv(assembler::operands::getint(resolveregister(a_chnk, names)), assembler::operands::getint(resolveregister(b_chnk, names)));
         } else if (str::startswith(line, "paref")) {
             string a_chnk, b_chnk;
             tie(a_chnk, b_chnk) = assembler::operands::get2(operands);
