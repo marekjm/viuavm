9955e775081cd719fedc69a52dfa28242cd50391
Marek Marecki <marekjm@ozro.pw>
2016-01-24 13:37:54 +0100

    Fix comments

--- examples/readwrite.asm
+++ examples/readwrite.asm
@@ -2,14 +2,14 @@
 ;
 ; Compilation:
 ;
-;   g++ -std=c++11 -Wall -pedantic -fPIC -c -o registerset.o ../src/cpu/registerset.cpp
-;   g++ -std=c++11 -Wall -pedantic -fPIC -shared -o io.so io.cpp registerset.o
-;   ../bin/vm/asm readwrite.asm
+;   g++ -std=c++11 -Wall -pedantic -fPIC -c -I../include -o registerset.o ../src/cpu/registerset.cpp
+;   g++ -std=c++11 -Wall -pedantic -fPIC -shared -I../include -o io.so io.cpp registerset.o
+;   ../build/bin/vm/asm readwrite.asm
 ;
 ; After libraries and the program are compiled result can be tested with following command
 ; which will ask for a path to read, read it, ask for a path write and write the string to it.
 ;
-;   ../bin/vm/cpu a.out
+;   ../build/bin/vm/cpu a.out
 ;
 
 .function: main
