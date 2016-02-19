fa1248a26e3b3ccccc6eaab61581c54b1d3c8b4e
Marek Marecki <marekjm@ozro.pw>
2016-02-20 00:02:21 +0100

    Remove commented code

--- src/types/reference.cpp
+++ src/types/reference.cpp
@@ -40,23 +40,13 @@ Reference* Reference::copy() const {
 }
 
 Reference::Reference(Type *ptr): pointer(new Type*(ptr)), counter(new unsigned(1)) {
-    //cout << "Reference::Reference(";
-    //cout << (*pointer)->type();
-    //cout << " *ptr = 0x" << hex << long(*pointer) << ", " << dec << ')' << endl;
 }
 Reference::~Reference() {
     /** Copies of the reference may be freely spawned and destroyed, but
      *  the internal object *MUST* be preserved until its refcount reaches zero.
      */
-    //cout << "(in 0x" << hex << long(this) << ") refcount decrement of: 0x" << long(*pointer) << dec << ": " << *counter << ", ";
-    /* cout << ((*counter)-1) << endl; */
     if ((--(*counter)) == 0) {
-        //cout << '0' << endl;
         cout << hex;
-        /* cout << "destruction of: 0x" << long(*pointer) << endl; */
-        /* cout << "\\___located at: 0x" << long(pointer) << endl; */
-        /* cout << "\\_performed by: 0x" << long(this) << endl; */
-        /* cout << dec; */
         delete *pointer;
         delete pointer;
         delete counter;
