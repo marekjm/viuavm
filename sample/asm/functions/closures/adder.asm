ccd8fa8250d918336420c5a044aeaefb5e897290
Marek Marecki <marekjm@ozro.pw>
2016-02-20 20:38:27 +0100

    Comments

--- sample/asm/functions/closures/adder.asm
+++ sample/asm/functions/closures/adder.asm
@@ -1,10 +1,16 @@
 .function: adder
+    ; expects register 1 to be an enclosed integer
     arg 2 0
     iadd 0 2 1
     return
 .end
 
 .function: make_adder
+    ; takes an integer and returns a function of one argument adding
+    ; given integer to its only parameter
+    ;
+    ; example:
+    ;   make_adder(3)(5) == 8
     .name: 1 number
     arg number 0
     closure 2 adder
@@ -14,16 +20,20 @@
 .end
 
 .function: main
+    ; create the adder function
     .name: 2 add_three
     frame ^[(param 0 (istore 1 3))]
     call add_three make_adder
 
+    ; add_three(2)
     frame ^[(param 0 (istore 3 2))]
     print (fcall 4 add_three)
 
+    ; add_three(5)
     frame ^[(param 0 (istore 3 5))]
     print (fcall 4 add_three)
 
+    ; add_three(13)
     frame ^[(param 0 (istore 3 13))]
     print (fcall 4 add_three)
 
