c8f1dac74c575c5d279935aaeb57acf989e75e70
Marek Marecki <marekjm@ozro.pw>
2016-02-23 23:15:47 +0100

    Fix wording of the comment

--- include/viua/bytecode/bytetypedef.h
+++ include/viua/bytecode/bytetypedef.h
@@ -6,9 +6,9 @@
 /** This header contains only one typedef - for byte.
  *
  *  This is because this typedef is required by various files across Wudoo
- *  source code, and often they do not the opcodes.h header.
- *  Such header would prevent redefinition of `byte` in every file and
- *  will help to keep the definition consitent.
+ *  source code, and often they do not need the opcodes.h header.
+ *  This header prevents redefinition of `byte` in every file and
+ *  helps keeping the definition consistent.
  *
  */
 
