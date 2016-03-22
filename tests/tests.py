bb65bdebff6c722c38576af83df0e0d358958510
Marek Marecki <marekjm@ozro.pw>
2016-03-22 16:28:17 +0100

    Test for std::vector::reverse/1

--- tests/tests.py
+++ tests/tests.py
@@ -990,6 +990,9 @@ class StandardRuntimeLibraryModuleVector(unittest.TestCase):
     def testVectorOfInts(self):
         runTestNoDisassemblyRerun(self, 'of_ints.asm', '[0, 1, 2, 3, 4, 5, 6, 7]')
 
+    def testVectorReverse(self):
+        runTestNoDisassemblyRerun(self, 'reverse.asm', ['[0, 1, 2, 3, 4, 5, 6, 7]', '[7, 6, 5, 4, 3, 2, 1, 0]'], output_processing_function=lambda o: o.strip().splitlines())
+
 
 class TypeStringTests(unittest.TestCase):
     PATH = './sample/types/String'
