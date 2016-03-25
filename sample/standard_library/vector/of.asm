27d50b00dab1fe9573e2deeb96d75bc92952870f
Marek Marecki <marekjm@ozro.pw>
2016-03-25 16:33:26 +0100

    Test for std::vector::of/2

---
+++ sample/standard_library/vector/of.asm
@@ -0,0 +1,20 @@
+.signature: std::vector::of/2
+
+.function: return_integer
+    arg 0 0
+    return
+.end
+
+.function: main
+    link std::vector
+
+    register (class 1 Foo)
+
+    frame ^[(pamv 0 (istore 1 8)) (pamv 1 (function 1 return_integer))]
+    call 1 std::vector::of/2
+
+    print 1
+
+    izero 0
+    return
+.end
