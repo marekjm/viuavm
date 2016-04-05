4616017aba217304afbc013c0bb3181d41f0a1c0
Marek Marecki <marekjm@ozro.pw>
2016-04-05 22:20:51 +0200

    Catch exceptions from foreign function calls

--- src/cpu/cpu.cpp
+++ src/cpu/cpu.cpp
@@ -35,26 +35,30 @@ void ForeignFunctionCallRequest::call(ExternalFunction* callback) {
      * FIXME: should external functions always have static registers allocated?
      * FIXME: third parameter should be a pointer to global registers
      */
-    (*callback)(frame, nullptr, nullptr);
+    try {
+        (*callback)(frame, nullptr, nullptr);
 
-    /* // FIXME: woohoo! segfault! */
-    Type* returned = nullptr;
-    int return_value_register = frame->place_return_value_in;
-    bool resolve_return_value_register = frame->resolve_return_value_register;
-    if (return_value_register != 0) {
-        // we check in 0. register because it's reserved for return values
-        if (frame->regset->at(0) == nullptr) {
-            caller_process->raiseException(new Exception("return value requested by frame but external function did not set return register"));
+        /* // FIXME: woohoo! segfault! */
+        Type* returned = nullptr;
+        int return_value_register = frame->place_return_value_in;
+        bool resolve_return_value_register = frame->resolve_return_value_register;
+        if (return_value_register != 0) {
+            // we check in 0. register because it's reserved for return values
+            if (frame->regset->at(0) == nullptr) {
+                caller_process->raiseException(new Exception("return value requested by frame but external function did not set return register"));
+            }
+            returned = frame->regset->pop(0);
         }
-        returned = frame->regset->pop(0);
-    }
 
-    // place return value
-    if (returned and caller_process->trace().size() > 0) {
-        if (resolve_return_value_register) {
-            return_value_register = static_cast<Integer*>(caller_process->obtain(return_value_register))->value();
+        // place return value
+        if (returned and caller_process->trace().size() > 0) {
+            if (resolve_return_value_register) {
+                return_value_register = static_cast<Integer*>(caller_process->obtain(return_value_register))->value();
+            }
+            caller_process->put(return_value_register, returned);
         }
-        caller_process->put(return_value_register, returned);
+    } catch (Type *exception) {
+        process->raiseException(exception);
     }
 }
 void ForeignFunctionCallRequest::registerException(Type* object) {
