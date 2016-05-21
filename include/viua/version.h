740bc1357b723e43f9222c1072ddf4cfb67bc2e2
Marek Marecki <marekjm@ozro.pw>
2016-05-21 21:03:27 +0200

    Revamped error reporting, version bump

--- include/viua/version.h
+++ include/viua/version.h
@@ -4,11 +4,11 @@
 #pragma once
 
 const char* VERSION = "0.8.0";
-const char* MICRO = "30";
+const char* MICRO = "73";
 
 // commit pointed by the hash below is HEAD~1
 // while the MICRO variable shows actual number of commits added since last release,
 // the commit hash is off-by-one
-const char* COMMIT = "68a2ff138658774912720a5c10877cb61835dec2";
+const char* COMMIT = "a10727fc3ecc3ed5ca0a0cbf02fa778de31da455";
 
 #endif
