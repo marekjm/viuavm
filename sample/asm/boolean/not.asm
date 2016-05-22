68457587c0764ce291597675f4e6109fe754f8e3
Marek Marecki <marekjm@ozro.pw>
2016-05-22 20:48:17 +0200

    Boolean NOT sample compiles with -E

--- sample/asm/boolean/not.asm
+++ sample/asm/boolean/not.asm
@@ -1,21 +1,15 @@
-.function: boolean
-    arg 1 0
-    not 1
-    not 1
-    move 0 1
+.function: boolean/1
+    move 0 (not (not (arg 1 0)))
     return
 .end
 
 .function: main 1
     izero 1
 
-    frame 1
-    param 0 1
-    call 1 boolean
+    frame ^[(param 0 1)]
+    call 1 boolean/1
 
-    print 1
-    not 1
-    print 1
+    print (not (print 1))
 
     izero 0
     return
