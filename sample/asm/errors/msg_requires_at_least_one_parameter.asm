8ccde7009e9d7d28c6434234e965e43e8fa067be
Marek Marecki <marekjm@ozro.pw>
2016-05-22 21:53:12 +0200

    Test for msg-requires-at-least-one-parameter error

---
+++ sample/asm/errors/msg_requires_at_least_one_parameter.asm
@@ -0,0 +1,7 @@
+.function: main
+    frame 0
+    msg 0 foo
+
+    izero 0
+    return
+.end
--- tests/tests.py
+++ tests/tests.py
@@ -880,6 +880,14 @@ class AssemblerErrorTests(unittest.TestCase):
         self.assertEqual(1, exit_code)
         self.assertEqual("./sample/asm/errors/arity_error.asm:8: error: invalid number of parameters in call to function foo/1: expected 1 got 0", output.strip())
 
+    def testMsgRequiresAtLeastOneParameter(self):
+        name = 'msg_requires_at_least_one_parameter.asm'
+        assembly_path = os.path.join(self.PATH, name)
+        compiled_path = os.path.join(COMPILED_SAMPLES_PATH, '{0}_{1}.bin'.format(self.PATH[2:].replace('/', '_'), name))
+        output, error, exit_code = assemble(assembly_path, compiled_path, opts=('--Ehalt-is-last',), okcodes=(1,0))
+        self.assertEqual(1, exit_code)
+        self.assertEqual("./sample/asm/errors/msg_requires_at_least_one_parameter.asm:3: error: invalid number of parameters in dynamic dispatch of foo: expected at least 1, got 0", output.strip())
+
     def testNoReturnAtTheEndOfAFunctionError(self):
         name = 'no_return_at_the_end_of_a_function.asm'
         assembly_path = os.path.join(self.PATH, name)
