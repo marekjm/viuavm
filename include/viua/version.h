aa77bb3010a19995493c7bd4af090dd6ee506ab3
Marek Marecki <marekjm@ozro.pw>
2016-05-23 22:12:19 +0200

    All samples compile with -E flag (part of #141 fixed), version bump

--- include/viua/version.h
+++ include/viua/version.h
@@ -4,11 +4,11 @@
 #pragma once
 
 const char* VERSION = "0.8.0";
-const char* MICRO = "73";
+const char* MICRO = "109";
 
 // commit pointed by the hash below is HEAD~1
 // while the MICRO variable shows actual number of commits added since last release,
 // the commit hash is off-by-one
-const char* COMMIT = "a10727fc3ecc3ed5ca0a0cbf02fa778de31da455";
+const char* COMMIT = "73e90f4e5dc703a8a2b83cc628c0b55ac2ca45c8";
 
 #endif
