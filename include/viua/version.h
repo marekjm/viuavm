d721ca866bfb470b589f87a793ad9cee0b510f6f
Marek Marecki <marekjm@ozro.pw>
2016-05-26 00:05:32 +0200

    Assembler raises more errors by default, closes #141

--- include/viua/version.h
+++ include/viua/version.h
@@ -4,11 +4,11 @@
 #pragma once
 
 const char* VERSION = "0.8.0";
-const char* MICRO = "109";
+const char* MICRO = "120";
 
 // commit pointed by the hash below is HEAD~1
 // while the MICRO variable shows actual number of commits added since last release,
 // the commit hash is off-by-one
-const char* COMMIT = "73e90f4e5dc703a8a2b83cc628c0b55ac2ca45c8";
+const char* COMMIT = "ac619efbbe9d171563fa08edb2cec3c7e531a38e";
 
 #endif
