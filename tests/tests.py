e10cffcd69b0a8eb69288bd58312895270967250
Marek Marecki <marekjm@ozro.pw>
2016-05-26 12:32:23 +0200

    Test for multiple main function candidate detection

--- tests/tests.py
+++ tests/tests.py
@@ -1017,6 +1017,19 @@ class AssemblerErrorTests(unittest.TestCase):
         self.assertEqual(1, exit_code)
         self.assertEqual("./sample/asm/errors/unknown_instruction.asm:2: error: unknown instruction: 'prnt'", output.strip())
 
+    def testMoreThanOneMainFunction(self):
+        name = 'more_than_one_main_function.asm'
+        assembly_path = os.path.join(self.PATH, name)
+        compiled_path = os.path.join(COMPILED_SAMPLES_PATH, '{0}_{1}.bin'.format(self.PATH[2:].replace('/', '_'), name))
+        output, error, exit_code = assemble(assembly_path, compiled_path, okcodes=(1,0))
+        self.assertEqual(1, exit_code)
+        error_lines = [
+            "./sample/asm/errors/more_than_one_main_function.asm: error: more than one candidate for main function",
+            "./sample/asm/errors/more_than_one_main_function.asm: note: main/1 function found in module ./sample/asm/errors/more_than_one_main_function.asm",
+            "./sample/asm/errors/more_than_one_main_function.asm: note: main/2 function found in module ./sample/asm/errors/more_than_one_main_function.asm",
+        ]
+        self.assertEqual(error_lines, output.strip().splitlines())
+
 
 class AssemblerErrorRejectingDuplicateSymbolsTests(unittest.TestCase):
     PATH = './sample/asm/errors/single_definition_rule'
