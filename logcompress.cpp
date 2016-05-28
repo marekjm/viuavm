da0d46f433dfb5c8526ea50e710296bde95e1fc7
Marek Marecki <marekjm@ozro.pw>
2016-05-28 22:05:54 +0200

    Initial code for log compressing tool

---
+++ logcompress.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+int main(int argc, char *argv[]) {
+    string input_filename, output_filename;
+
+    if (argc < 2) {
+        cerr << "error: no input file" << endl;
+        return 1;
+    }
+
+    // extract input and output filenames from
+    // commandline parameters
+    input_filename = string(argv[1]);
+    if (argc >= 3) {
+        output_filename = string(argv[2]);
+    } else {
+        output_filename = (input_filename + ".compressed.log");
+    }
+
+    ifstream in(input_filename);
+
+    string line, previous, preprevious;
+    string _;
+    unsigned long repeated = 0;
+
+    int initialisation = 3;
+
+    while (getline(in, _)) {
+        preprevious = previous;
+        previous = line;
+        line = _;
+
+        if (initialisation) {
+            // first three iterations are just to initialise the state
+            --initialisation;
+            continue;
+        }
+
+        if (line == previous and previous == preprevious) {
+            // another repeated line
+            ++repeated;
+        } else {
+            cout << line << endl;
+            cout << "\n# repeated " << repeated << " time(s)...\n" << endl;
+            cout << line << endl;
+        }
+
+        /* if (initialisation && line == previous) { */
+        /*     ++repeated; */
+        /* } */
+
+        /* if (line == previous) { */
+        /*     ++repeated; */
+        /* } else { */
+        /*     cout << line << endl; */
+        /*     if (repeated) { */
+        /*         cout << "\n# repeated " << repeated << " time(s)...\n" << endl; */
+        /*         cout << line << endl; */
+        /*     } */
+        /*     repeated = 0; */
+        /* } */
+    }
+
+    if (line == previous and previous == preprevious) {
+        cout << line << endl;
+        cout << "\n# repeated " << repeated+1 << " time(s)...\n" << endl;
+        cout << line << endl;
+    }
+
+
+    return 0;
+}
