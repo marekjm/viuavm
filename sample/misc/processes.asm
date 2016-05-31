92ecfa38de3bed125c35e89fe852dbb0239ff0c6
Marek Marecki <marekjm@ozro.pw>
2016-05-31 22:55:16 +0200

    Be fancy and use cross-function tail recursion in process spawning sample program

--- sample/misc/processes.asm
+++ sample/misc/processes.asm
@@ -5,28 +5,47 @@
     return
 .end
 
-.function: process_spawner/1
-    echo (strstore 2 "process_spawner/1: ")
+.function: spawn_processes/2
+    .name: 1 counter
+    arg counter 0
 
-    .name: 1 limit
-    echo (arg 1 0)
-    print (strstore 2 " processs to launch")
+    .name: 2 limit
+    arg limit 1
 
-    .name: 3 counter
-    izero counter
+    ; if limit is N, processes IDs go from 0 to N-1
+    ; this is why the counter is incremented before the
+    ; branch instruction which checks for equality
+    iinc counter
 
-    .mark: loop
-    branch (igte 4 counter limit) endloop +1
+    ; if the counter is equal to limit return immediately
+    branch (ieq 3 counter limit) +1 +2
+    return
 
+    ; spawn a new process
     frame ^[(param 0 counter)]
     process 5 run_in_a_process/1
 
     frame ^[(param 0 5)]
     msg 0 detach/1
 
-    iinc counter
-    jump loop
-    .mark: endloop
+    ; take advantage of tail recursion in Viua and
+    ; elegantly follow with process spawner execution
+    frame ^[(pamv 0 counter) (pamv 1 limit)]
+    tailcall spawn_processes/2
+
+    ; FIXME: assembler should not complain if a function ends with tailcall
+    ; instead of return
+    return
+.end
+
+.function: process_spawner/1
+    echo (strstore 2 "process_spawner/1: ")
+    .name: 1 limit
+    echo (arg 1 0)
+    print (strstore 2 " processs to launch")
+
+    frame ^[(pamv 0 (izero 3)) (pamv 1 limit)]
+    tailcall spawn_processes/2
 
     return
 .end
