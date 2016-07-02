5c9fdf46264a6e3a2c525b347c30bd93e8b296eb
Marek Marecki <marekjm@ozro.pw>
2016-07-02 16:48:05 +0200

    Quick fix

--- src/process.cpp
+++ src/process.cpp
@@ -163,7 +163,7 @@ void Process::dropFrame() {
 
 byte* Process::adjustJumpBaseFor(const string& call_name) {
     byte *entry_point = nullptr;
-    auto ep = scheduler->cpu()->getEntryPointOf(call_name);
+    auto ep = scheduler->getEntryPointOf(call_name);
     entry_point = ep.first;
     jump_base = ep.second;
     return entry_point;
