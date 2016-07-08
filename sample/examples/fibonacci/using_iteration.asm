eb30e86453fdaf0f00accf826652833d0055203d
Marek Marecki <marekjm@ozro.pw>
2016-07-08 21:26:31 +0200

    Add an iterative Fibonacci example

--- sample/examples/fibonacci/using_iteration.asm
+++ sample/examples/fibonacci/using_iteration.asm
@@ -1,12 +1,37 @@
+.function: fibonacci/2
+    .name: 1 current_value
+    .name: 2 accumulator
+    arg current_value 0
+    arg accumulator 1
+
+    .mark: fibonacci/2__loop
+    branch current_value +1 fibonacci/0__finished
+
+    iadd accumulator ^(idec current_value)
+
+    jump fibonacci/2__loop
+
+    .mark: fibonacci/0__finished
+    move 0 accumulator
+    return
+.end
+
 .function: fibonacci/1
-    izero 0
+    .name: 2 accumulator
+    frame ^[(pamv 0 (arg 1 0)) (pamv 1 (izero 2))]
+    call accumulator fibonacci/2
+
+    .mark: fibonacci/0__finished
+    move 0 accumulator
     return
 .end
 
 .function: main/0
     .name: 1 result
 
-    frame ^[(pamv 0 (istore 1 20))]
+    istore result 5
+
+    frame ^[(pamv 0 result)]
     call result fibonacci/1
 
     print result
