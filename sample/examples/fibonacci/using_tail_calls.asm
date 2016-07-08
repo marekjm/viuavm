57466b7d544dff67eef350f86e5a7570c4d7e3cf
Marek Marecki <marekjm@ozro.pw>
2016-07-08 21:21:41 +0200

    Add a tail-recursive Fibonacci example

--- sample/examples/fibonacci/using_tail_calls.asm
+++ sample/examples/fibonacci/using_tail_calls.asm
@@ -1,13 +1,29 @@
-.function: fibonacci/1
-    izero 0
+.function: fibonacci/2
+    .name: 1 current_value
+    .name: 2 accumulator
+    arg current_value 0
+    arg accumulator 1
+
+    branch current_value +1 fibonacci/0__finished
+
+    iadd accumulator current_value
+
+    frame ^[(pamv 0 (idec current_value)) (pamv 1 accumulator)]
+    tailcall fibonacci/2
+
+    .mark: fibonacci/0__finished
+    move 0 accumulator
     return
 .end
 
 .function: main/0
     .name: 1 result
 
-    frame ^[(pamv 0 (istore 1 20))]
-    call result fibonacci/1
+    istore result 5
+
+    ; pass accumulator in second parameter slot
+    frame ^[(pamv 0 result) (pamv 1 (izero 2)]
+    call result fibonacci/2
 
     print result
 
