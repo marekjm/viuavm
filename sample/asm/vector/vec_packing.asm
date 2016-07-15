f35bff2544cbb5aa8207176733683f3b0e61bc65
Marek Marecki <marekjm@ozro.pw>
2016-07-16 00:47:48 +0200

    Tests for packing VEC

---
+++ sample/asm/vector/vec_packing.asm
@@ -0,0 +1,11 @@
+.function: main/0
+    strstore 1 "answer to life"
+    istore 2 42
+
+    vec 3 1 2
+
+    print 3
+
+    izero 0
+    return
+.end
---
+++ sample/asm/vector/vec_packing_null.asm
@@ -0,0 +1,13 @@
+.function: main/0
+    strstore 2 "answer to life"
+    istore 3 42
+
+    -- this would pack
+    -- the vector inside itself
+    vec 1 2 8
+
+    print 1
+
+    izero 0
+    return
+.end
