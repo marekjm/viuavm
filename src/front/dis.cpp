62cb41f4f9e232adc412281b336356c218720a4c
Marek Marecki <marekjm@ozro.pw>
2016-07-16 20:34:16 +0200

    Encode escape sequences when disassembling

--- src/front/dis.cpp
+++ src/front/dis.cpp
@@ -220,7 +220,7 @@ int main(int argc, char* argv[]) {
         disassembled_lines.push_back("; meta information\n");
     }
     for (const auto each : meta_information) {
-        disassembled_lines.push_back(".info: " + each.first + ' ' + str::enquote(each.second) + "\n");
+        disassembled_lines.push_back(".info: " + each.first + ' ' + str::enquote(str::strencode(each.second)) + "\n");
     }
     if (meta_information.size()) {
         disassembled_lines.push_back("\n");
