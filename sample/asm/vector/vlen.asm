ff36f7d95bd5e45c0e195db647716587e9497c4d
Marek Marecki <marekjm@ozro.pw>
2016-09-08 22:58:54 +0200

    Fix vpush from empty registers in vlen test (caught by SA)

--- sample/asm/vector/vlen.asm
+++ sample/asm/vector/vlen.asm
@@ -20,7 +20,7 @@
 .function: main/1
     istore 1 0
 
-    vpush (vpush (vpush (vpush (vpush (vpush (vpush (vpush (vec 2) 1) 1) 1) 1) 1) 1) 1) 1
+    vpush (vpush (vpush (vpush (vpush (vpush (vpush (vpush (vec 2) (copy 3 1)) (copy 3 1)) (copy 3 1)) (copy 3 1)) (copy 3 1)) (copy 3 1)) (copy 3 1)) 1
     print (vlen 3 2)
 
     izero 0
