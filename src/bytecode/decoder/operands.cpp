ac683559628c926a61cf011766465ef42f2e2889
Marek Marecki <marekjm@ozro.pw>
2016-09-21 22:11:16 +0200

    Deduplicate code

--- src/bytecode/decoder/operands.cpp
+++ src/bytecode/decoder/operands.cpp
@@ -39,6 +39,26 @@ static auto get_operand_type(byte *ip) -> OperandType {
     return extract<OperandType>(ip);
 }
 
+template<class Value, class Class> static auto fetch_primitive_value(byte *ip, Process *p, Value initial) -> tuple<byte*, Value> {
+    OperandType ot = get_operand_type(ip);
+    ++ip;
+
+    Value value = initial;
+    if (ot == OT_REGISTER_INDEX or ot == OT_REGISTER_REFERENCE) {
+        value = extract<Value>(ip);
+        ip += sizeof(Value);
+    } else {
+        throw new Exception("decoded invalid operand type");
+    }
+    if (ot == OT_REGISTER_REFERENCE) {
+        // FIXME once dynamic operand types are implemented the need for this cast will go away
+        // because the operand *will* be encoded as a real uint
+        Class *i = static_cast<Class*>(p->obtain(static_cast<unsigned>(value)));
+        value = i->value();
+    }
+    return tuple<byte*, Value>(ip, value);
+}
+
 auto viua::bytecode::decoder::operands::fetch_register_index(byte *ip, Process *process) -> tuple<byte*, unsigned> {
     OperandType ot = get_operand_type(ip);
     ++ip;
@@ -63,23 +83,7 @@ auto viua::bytecode::decoder::operands::fetch_register_index(byte *ip, Process *
 }
 
 auto viua::bytecode::decoder::operands::fetch_primitive_char(byte *ip, Process *process) -> tuple<byte*, char> {
-    OperandType ot = get_operand_type(ip);
-    ++ip;
-
-    char value = 0;
-    if (ot == OT_REGISTER_INDEX or ot == OT_REGISTER_REFERENCE) {
-        value = extract<char>(ip);
-        ip += sizeof(char);
-    } else {
-        throw new Exception("decoded invalid operand type");
-    }
-    if (ot == OT_REGISTER_REFERENCE) {
-        // FIXME once dynamic operand types are implemented the need for this cast will go away
-        // because the operand *will* be encoded as a real uint
-        Byte *i = static_cast<Byte*>(process->obtain(static_cast<unsigned>(value)));
-        value = i->value();
-    }
-    return tuple<byte*, char>(ip, value);
+    return fetch_primitive_value<char, Byte>(ip, process, 0);
 }
 
 auto viua::bytecode::decoder::operands::fetch_primitive_uint(byte *ip, Process *process) -> tuple<byte*, unsigned> {
@@ -94,23 +98,7 @@ auto viua::bytecode::decoder::operands::fetch_primitive_uint64(byte *ip, Process
 }
 
 auto viua::bytecode::decoder::operands::fetch_primitive_int(byte *ip, Process* p) -> tuple<byte*, int> {
-    OperandType ot = get_operand_type(ip);
-    ++ip;
-
-    int value = 0;
-    if (ot == OT_REGISTER_INDEX or ot == OT_REGISTER_REFERENCE) {
-        value = extract<int>(ip);
-        ip += sizeof(int);
-    } else {
-        throw new Exception("decoded invalid operand type");
-    }
-    if (ot == OT_REGISTER_REFERENCE) {
-        // FIXME once dynamic operand types are implemented the need for this cast will go away
-        // because the operand *will* be encoded as a real uint
-        Integer *i = static_cast<Integer*>(p->obtain(static_cast<unsigned>(value)));
-        value = i->value();
-    }
-    return tuple<byte*, int>(ip, value);
+    return fetch_primitive_value<int, Integer>(ip, p, 0);
 }
 
 auto viua::bytecode::decoder::operands::extract_primitive_uint64(byte *ip, Process*) -> uint64_t {
