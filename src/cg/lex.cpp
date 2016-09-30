fd337c951bf58fdcafe7fbeaa23511bbb354c7a1
Marek Marecki <marekjm@ozro.pw>
2016-09-30 17:31:29 +0200

    Add FIXME

--- src/cg/lex.cpp
+++ src/cg/lex.cpp
@@ -756,6 +756,7 @@ namespace viua {
             }
 
             vector<Token> unwrap_lines(vector<Token> input_tokens, bool full) {
+                // FIXME: this function need refactoring
                 decltype(input_tokens) unwrapped_tokens;
                 decltype(input_tokens) tokens;
                 decltype(input_tokens) final_tokens;
