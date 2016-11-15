a494209e22aacb03886ed8c0d9700235bd5e902c
Marek Marecki <marekjm@ozro.pw>
2016-11-15 19:49:27 +0100

    Fix correctly calculate size of msg

--- src/cg/tools.cpp
+++ src/cg/tools.cpp
@@ -602,7 +602,17 @@ namespace viua {
                 return tuple<uint64_t, decltype(i)>(calculated_size, i);
             }
             static auto size_of_msg(const vector<viua::cg::lex::Token>& tokens, decltype(tokens.size()) i) -> tuple<uint64_t, decltype(i)> {
-                uint64_t calculated_size = sizeof(byte);
+                uint64_t calculated_size = sizeof(byte);    // start with the size of a single opcode
+
+                decltype(calculated_size) size_increment = 0;
+
+                // for target register
+                tie(size_increment, i) = size_of_register_index_operand(tokens, i);
+                calculated_size += size_increment;
+
+                calculated_size += tokens.at(i).str().size() + 1;
+                ++i;
+
                 return tuple<uint64_t, decltype(i)>(calculated_size, i);
             }
             static auto size_of_insert(const vector<viua::cg::lex::Token>& tokens, decltype(tokens.size()) i) -> tuple<uint64_t, decltype(i)> {
