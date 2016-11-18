083fcb6b6692718b58b54299fc93bb500f62ee73
Marek Marecki <marekjm@ozro.pw>
2016-11-18 21:24:50 +0100

    Remove stale FIXMEs

--- src/cg/tools.cpp
+++ src/cg/tools.cpp
@@ -529,14 +529,12 @@ namespace viua {
                 return size_of_instruction_with_one_ri_operand(tokens, i);
             }
             static auto size_of_join(const vector<viua::cg::lex::Token>& tokens, decltype(tokens.size()) i) -> tuple<uint64_t, decltype(i)> {
-                // FIXME: timeout
                 return size_of_instruction_with_three_ri_operands(tokens, i);
             }
             static auto size_of_send(const vector<viua::cg::lex::Token>& tokens, decltype(tokens.size()) i) -> tuple<uint64_t, decltype(i)> {
                 return size_of_instruction_with_two_ri_operands(tokens, i);
             }
             static auto size_of_receive(const vector<viua::cg::lex::Token>& tokens, decltype(tokens.size()) i) -> tuple<uint64_t, decltype(i)> {
-                // FIXME: timeout
                 return size_of_instruction_with_two_ri_operands(tokens, i);
             }
             static auto size_of_watchdog(const vector<viua::cg::lex::Token>& tokens, decltype(tokens.size()) i) -> tuple<uint64_t, decltype(i)> {
