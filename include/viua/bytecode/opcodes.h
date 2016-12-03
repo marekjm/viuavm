7dcf5d91512602a9d5e27cae521da7d8a5bfe1d1
Marek Marecki <marekjm@ozro.pw>
2016-12-03 22:45:09 +0100

    Fix a typo

--- include/viua/bytecode/opcodes.h
+++ include/viua/bytecode/opcodes.h
@@ -147,7 +147,7 @@ enum OPCODE : byte {
     REGISTER,   // register a prototype in VM's typesystem
 
     NEW,        // construct new instance of a class in a register
-    MSG,        // send a message to an object (used for dynamic dispatch, for static use plan "CALL")
+    MSG,        // send a message to an object (used for dynamic dispatch, for static use plain "CALL")
     INSERT,     // insert an object as a value of an attribute of another object
     REMOVE,     // remove an attribute from an object
 
