22fe55972dd5b607e368fccae4a9d5e0713b8423
Marek Marecki <marekjm@ozro.pw>
2016-12-24 03:19:52 +0100

    Small type fix

--- src/cg/bytecode/instructions.cpp
+++ src/cg/bytecode/instructions.cpp
@@ -91,8 +91,8 @@ static viua::internals::types::byte* insert_two_ri_and_primitive_int_instruction
                 *(reinterpret_cast<OperandType*>(addr_ptr)) = OT_REGISTER_REFERENCE;
                 pointer::inc<OperandType, viua::internals::types::byte>(addr_ptr);
 
-                *(reinterpret_cast<viua::internals::types::plain_int*>(addr_ptr))  = c.value;
-                pointer::inc<viua::internals::types::plain_int, viua::internals::types::byte>(addr_ptr);
+                *(reinterpret_cast<viua::internals::types::register_index*>(addr_ptr))  = c.value;
+                pointer::inc<viua::internals::types::register_index, viua::internals::types::byte>(addr_ptr);
             } else {
                 *(reinterpret_cast<OperandType*>(addr_ptr)) = OT_INT;
                 pointer::inc<OperandType, viua::internals::types::byte>(addr_ptr);
