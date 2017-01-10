29dfb3a9ca4feb88c45ca0cd6b8c3a9c331836fa
Marek Marecki <marekjm@ozro.pw>
2017-01-11 00:48:24 +0100

    Additional fixes

--- sample/asm/int/boolean_as_int.asm
+++ sample/asm/int/boolean_as_int.asm
@@ -21,7 +21,7 @@
     istore (.name: %iota forty_two) 42
     istore (.name: %iota sixty_nine) 69
 
-    lt int64 (.name: %iota compared) forty_two sixty_nine
+    lt int64 (.name: %iota compared) %forty_two %sixty_nine
 
     print (add int32 %iota %sixty_nine %compared)
 
