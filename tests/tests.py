3d2b06879ceab9d8ba68db09a1cd7fd30471e1e3
Marek Marecki <marekjm@ozro.pw>
2017-04-23 03:16:37 +0200

    Make Boolean a type distinct from numerics

--- tests/tests.py
+++ tests/tests.py
@@ -463,6 +463,7 @@ class IntegerInstructionsTests(unittest.TestCase):
     def testIntegersInCondition(self):
         runTest(self, 'in_condition.asm', 'true', 0)
 
+    @unittest.skip('')
     def testBooleanAsInteger(self):
         runTest(self, 'boolean_as_int.asm', '70', 0)
 
