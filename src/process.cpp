94afb7bcb4b989f3263e8b306e6e8e4489d25c67
Marek Marecki <marekjm@ozro.pw>
2017-05-21 14:06:10 +0200

    Include names of deferred functions in trace lines

--- src/process.cpp
+++ src/process.cpp
@@ -60,7 +60,7 @@ auto viua::process::Process::get_trace_line(viua::internals::types::byte* for_ad
         }
         trace_line << ' ' << string(reinterpret_cast<char*>(working_address));
     }
-    if (static_cast<OPCODE>(*for_address) == TAILCALL) {
+    if (static_cast<OPCODE>(*for_address) == TAILCALL or static_cast<OPCODE>(*for_address) == DEFER) {
         trace_line << ' ';
         trace_line << string(reinterpret_cast<char*>(for_address+1));
     }
