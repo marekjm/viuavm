871e07cbab354124d1d28ed35524cbc50e94ee88
Marek Marecki <marekjm@ozro.pw>
2017-07-16 15:23:42 +0200

    Use literal binary instead of enabling bits one-by-one

--- sample/asm/bits/manipulation/bitand.asm
+++ sample/asm/bits/manipulation/bitand.asm
@@ -18,35 +18,14 @@
 ;
 
 .function: main/0
-    not (izero (.name: %iota enabled) local)
-
-    istore (.name: %iota bitwidth) local 8
-
-    bits (.name: %iota first) local %bitwidth local
-    bits (.name: %iota second) local %bitwidth local
-
-    .name: %iota bit_index
-    bitset %first local (istore %bit_index local 0) %enabled local
-    bitset %first local (istore %bit_index local 2) %enabled local
-    bitset %first local (istore %bit_index local 4) %enabled local
-    bitset %first local (istore %bit_index local 5) %enabled local
-    bitset %first local (istore %bit_index local 6) %enabled local
-    bitset %first local (istore %bit_index local 7) %enabled local
-
-    bitset %second local (istore %bit_index local 0) %enabled local
-    bitset %second local (istore %bit_index local 3) %enabled local
-    bitset %second local (istore %bit_index local 4) %enabled local
-    bitset %second local (istore %bit_index local 5) %enabled local
-    bitset %second local (istore %bit_index local 7) %enabled local
+    bits (.name: %iota first) local  0b11110101
+    bits (.name: %iota second) local 0b10111001
 
     print %first local
     print %second local
 
     bitand (.name: %iota third) local %first local %second local
     print %third local
-
-    ;print (bits %2 local (istore %1 local 8) local) local
-    ;print (bitnot %3 local %2 local)
 
     izero %0 local
     return
