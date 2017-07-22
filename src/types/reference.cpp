26caae23dcdba0d9e9bab370a84cc53ea6f6d241
Marek Marecki <marekjm@ozro.pw>
2017-07-22 17:13:17 +0200

    Use curly brace initialisation

--- src/types/reference.cpp
+++ src/types/reference.cpp
@@ -55,7 +55,7 @@ unique_ptr<viua::types::Value> viua::types::Reference::copy() const {
 }
 
 viua::types::Reference::Reference(viua::types::Value* ptr)
-    : pointer(new viua::types::Value*(ptr)), counter(new uint64_t(1)) {}
+    : pointer(new viua::types::Value*(ptr)), counter(new uint64_t{1}) {}
 viua::types::Reference::Reference(Value** ptr, uint64_t* ctr) : pointer(ptr), counter(ctr) {}
 viua::types::Reference::~Reference() {
     /** Copies of the reference may be freely spawned and destroyed, but
