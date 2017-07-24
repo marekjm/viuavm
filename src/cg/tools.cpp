fe977e7e7c358d95d3a9cf0ce11db14710804e28
Marek Marecki <marekjm@ozro.pw>
2017-07-24 23:40:56 +0200

    Calculate size of "fixedincrement" instruction

--- src/cg/tools.cpp
+++ src/cg/tools.cpp
@@ -638,6 +638,10 @@ namespace viua {
                 -> tuple<bytecode_size_type, decltype(i)> {
                 return size_of_instruction_with_two_ri_operands_with_rs_types(tokens, i);
             }
+            static auto size_of_fixedincrement(const TokenVector& tokens, TokenVector::size_type i)
+                -> tuple<bytecode_size_type, decltype(i)> {
+                return size_of_instruction_with_one_ri_operand(tokens, i);
+            }
             static auto size_of_move(const TokenVector& tokens, TokenVector::size_type i)
                 -> tuple<bytecode_size_type, decltype(i)> {
                 return size_of_instruction_with_two_ri_operands_with_rs_types(tokens, i);
@@ -1356,6 +1360,9 @@ namespace viua {
                     } else if (tokens.at(i) == "ror") {
                         ++i;
                         tie(increase, i) = size_of_ror(tokens, i);
+                    } else if (tokens.at(i) == "fixedincrement") {
+                        ++i;
+                        tie(increase, i) = size_of_fixedincrement(tokens, i);
                     } else if (tokens.at(i) == "move") {
                         ++i;
                         tie(increase, i) = size_of_move(tokens, i);
