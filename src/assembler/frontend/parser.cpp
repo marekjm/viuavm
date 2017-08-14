7b3cb9c2ae8a46c15d1954f98de989237cd426c4
Marek Marecki <marekjm@ozro.pw>
2017-08-14 23:57:10 +0200

    Recognise ".unused:" directive

--- src/assembler/frontend/parser.cpp
+++ src/assembler/frontend/parser.cpp
@@ -260,6 +260,8 @@ auto viua::assembler::frontend::parser::parse_directive(const vector_view<Token>
             throw viua::cg::lex::InvalidSyntax(tokens.at(i), "invalid marker");
         }
         directive->operands.push_back(tokens.at(i++));
+    } else if (tokens.at(0) == ".unused:") {
+        directive->operands.push_back(tokens.at(i++));
     }
     if (tokens.at(i) != "\n") {
         throw viua::cg::lex::InvalidSyntax(tokens.at(i), "extra parameters to a directive").add(tokens.at(0));
