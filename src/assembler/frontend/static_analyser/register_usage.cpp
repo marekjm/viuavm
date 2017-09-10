d3d6df5564ec33ad2ed562519a95462ad2ecf890
Marek Marecki <marekjm@ozro.pw>
2017-09-10 20:42:15 +0200

    Include "import" instruction in SA else-if chain

--- src/assembler/frontend/static_analyser/register_usage.cpp
+++ src/assembler/frontend/static_analyser/register_usage.cpp
@@ -1918,6 +1918,8 @@ static auto check_register_usage_for_instruction_block_impl(RegisterUsageProfile
             // FIXME TODO SA for entered blocks
         } else if (opcode == LEAVE) {
             // do nothing
+        } else if (opcode == IMPORT) {
+            // do nothing
         } else if (opcode == ATOM) {
             auto operand = dynamic_cast<RegisterIndex*>(instruction->operands.at(0).get());
             if (not operand) {
