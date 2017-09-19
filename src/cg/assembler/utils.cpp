954d9fc24ea874d91afc7db4e2f2941a5f904283
Marek Marecki <marekjm@ozro.pw>
2017-09-19 22:12:57 +0200

    Functions MUST end with arity specifier (/[0-9]*)

--- src/cg/assembler/utils.cpp
+++ src/cg/assembler/utils.cpp
@@ -27,7 +27,7 @@ using namespace std;
 regex assembler::utils::getFunctionNameRegex() {
     // FIXME function names *MUST* end with '/' to avoid ambiguity when determining whether a token
     // is a function name, or a label
-    return regex{"(?:::)?[a-zA-Z_][a-zA-Z0-9_]*(?:::[a-zA-Z_][a-zA-Z0-9_]*)*(?:(/[0-9]*)?)?"};
+    return regex{"(?:::)?[a-zA-Z_][a-zA-Z0-9_]*(?:::[a-zA-Z_][a-zA-Z0-9_]*)*/([0-9]+)?"};
 }
 
 bool assembler::utils::isValidFunctionName(const string& function_name) {
@@ -43,12 +43,8 @@ smatch assembler::utils::matchFunctionName(const string& function_name) {
 int assembler::utils::getFunctionArity(const string& function_name) {
     int arity = -1;
     auto parts = matchFunctionName(function_name);
-    auto sz = parts[1].str().size();
-    if (sz > 1) {
-        ssub_match a = parts[1];
-        arity = stoi(a.str().substr(1));  // cut off the '/' before converting to integer
-    } else if (sz == 1) {
-        arity = -2;
+    if (auto a = parts[1].str(); a.size()) {
+        arity = stoi(a);
     }
     return arity;
 }
