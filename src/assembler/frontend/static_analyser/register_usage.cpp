34c2e8a5fb702808c7af2bae98ee0b88453e49ab
Marek Marecki <marekjm@ozro.pw>
2017-10-30 22:49:06 +0100

    Fix false positive for "unused value" after if

--- src/assembler/frontend/static_analyser/register_usage.cpp
+++ src/assembler/frontend/static_analyser/register_usage.cpp
@@ -2238,9 +2238,19 @@ static auto check_register_usage_for_instruction_block_impl(RegisterUsageProfile
                          */
                     }
                 } catch (InvalidSyntax& e) {
-                    throw TracedSyntaxError{}.append(e).append(
-                        InvalidSyntax{instruction->tokens.at(0), "after taking false branch here:"}.add(
-                            instruction->operands.at(2)->tokens.at(0)));
+                    if (register_with_unused_value != e.what() and
+                        string{e.what()}.substr(0, 6) == "unused") {
+                        /*
+                         * If an error was thrown for a different register it means that the register that
+                         * was unused in true branch was used in the false one (so no errror), and
+                         * the register for which the false branch threw was used in the true one (so no
+                         * error either).
+                         */
+                    } else {
+                        throw TracedSyntaxError{}.append(e).append(
+                            InvalidSyntax{instruction->tokens.at(0), "after taking false branch here:"}.add(
+                                instruction->operands.at(2)->tokens.at(0)));
+                    }
                 } catch (TracedSyntaxError& e) {
                     if (register_with_unused_value != e.errors.front().what() and
                         string{e.errors.front().what()}.substr(0, 6) == "unused") {
