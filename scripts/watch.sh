36ac643e85fcb7d6d2d50963eff4ec469a5e87aa
Marek Marecki <marekjm@ozro.pw>
2017-11-25 21:09:05 +0100

    Enable colours in watch script

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+CXX_EXTRA_FLAGS=-fdiagnostics-color=always
+export CXX_EXTRA_FLAGS
+
 while true; do
     (find ./include -name '*.h' ; find ./src/ -name '*.cpp') | entr -p ./scripts/compile_and_notify.sh
 done
