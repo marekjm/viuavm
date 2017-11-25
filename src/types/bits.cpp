ad1f642e97e83fae24f7ee90244164813f4bb787
Marek Marecki <marekjm@ozro.pw>
2017-11-25 21:14:47 +0100

    Implement signed saturating addition (with bugs)

--- src/types/bits.cpp
+++ src/types/bits.cpp
@@ -776,7 +776,7 @@ namespace viua {
                 v.push_back(false);
                 return v;
             }
-            static auto signed_make_min[[maybe_unused]](size_t const n) -> vector<bool> {
+            static auto signed_make_min(size_t const n) -> vector<bool> {
                 vector<bool> v;
                 v.reserve(n);
                 v.push_back(true);
@@ -841,8 +841,123 @@ namespace viua {
                 return decremented;
             }
 
-            static auto signed_add(vector<bool> lhs, vector<bool>) -> vector<bool> {
-                return lhs;
+            static auto take_twos_complement(vector<bool> const& v) -> vector<bool> {
+                return signed_increment(binary_inversion(v));
+            }
+            static auto signed_lt(vector<bool> lhs, vector<bool> rhs) {
+                lhs = binary_expand(lhs, max(lhs.size(), rhs.size()));
+                rhs = binary_expand(rhs, max(lhs.size(), rhs.size()));
+
+                if (binary_is_negative(lhs) and not binary_is_negative(rhs)) {
+                    return true;
+                }
+                if (not binary_is_negative(lhs) and binary_is_negative(rhs)) {
+                    return false;
+                }
+
+                if (binary_is_negative(lhs)) {
+                    lhs = take_twos_complement(lhs);
+                }
+                if (binary_is_negative(rhs)) {
+                    rhs = take_twos_complement(rhs);
+                }
+
+                for (auto i = (lhs.size() - 1); i; --i) {
+                    if (lhs.at(i - 1) < rhs.at(i - 1)) {
+                        // definitely lhs < rhs
+                        return true;
+                    } else if (lhs.at(i - 1) > rhs.at(i - 1)) {
+                        // totally lhs > rhs
+                        return false;
+                    }
+                }
+                // equal to each other
+                return true;
+            }
+
+            static auto signed_add(vector<bool> lhs, vector<bool> rhs) -> vector<bool> {
+                vector<bool> result;
+                auto size_of_result = std::max(lhs.size(), rhs.size());
+                result.reserve(size_of_result + 1);
+                std::fill_n(std::back_inserter(result), size_of_result, false);
+
+                bool carry = false;
+
+                auto lhs_negative = binary_is_negative(lhs);
+                auto rhs_negative = binary_is_negative(rhs);
+
+                auto result_should_be_negative = (lhs_negative and rhs_negative);
+                if (lhs_negative and (not rhs_negative) and signed_lt(rhs, lhs)) {
+                    result_should_be_negative = true;
+                }
+                if ((not lhs_negative) and rhs_negative and signed_lt(lhs, lhs)) {
+                    result_should_be_negative = true;
+                }
+
+                for (auto i = decltype(size_of_result){0}; i < size_of_result; ++i) {
+                    const auto from_lhs = (i < lhs.size() ? lhs.at(i) : false);
+                    const auto from_rhs = (i < rhs.size() ? rhs.at(i) : false);
+
+                    /*
+                     * lhs + rhs -> 0 + 0 -> 0
+                     *
+                     * This is the easy case.
+                     * Everything is zero, so we just carry the carry into the result at
+                     * the current position, and reset the carry flag to zero (it was consumed).
+                     */
+                    if ((not from_rhs) and (not from_lhs)) {
+                        result.at(i) = carry;
+                        carry = false;
+                        continue;
+                    }
+
+                    /*
+                     * lhs + rhs -> 1 + 1 -> 10
+                     *
+                     * This gives us a zero on current position, and
+                     * carry flag in the enabled state.
+                     *
+                     * If carry was enabled before we have 0 + 1 = 1, so we should
+                     * enable bit on current position in the result.
+                     * If carry was not enabled we have 0 + 0, so we should
+                     * obviously leave the bit disabled in the result.
+                     * This means that we can just copy state of the carry flag into
+                     * the result bit string on current position.
+                     */
+                    if (from_rhs and from_lhs) {
+                        result.at(i) = carry;
+                        carry = true;
+                        continue;
+                    }
+
+                    /*
+                     * At this point either the lhs or rhs is enabled, but not both.
+                     * So if the carry bit is enabled this gives us 1 + 1 = 10, so
+                     * zero should be put in result on the current position, and
+                     * carry flag should be enabled.
+                     */
+                    if (carry) {
+                        continue;
+                    }
+
+                    /*
+                     * All other cases.
+                     * Either lhs or rhs is enabled, and carry is not.
+                     * So this is the 0 + 1 = 1 case.
+                     * Easy.
+                     * Just enable the bit in the result.
+                     */
+                    result.at(i) = true;
+                }
+
+                if (result_should_be_negative and not binary_is_negative(result)) {
+                    result = signed_make_min(lhs.size());
+                }
+                if (not result_should_be_negative and binary_is_negative(result)) {
+                    result = signed_make_max(lhs.size());
+                }
+
+                return result;
             }
             static auto signed_mul(vector<bool> lhs, vector<bool>) -> vector<bool> {
                 return lhs;
