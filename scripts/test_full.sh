3aef79f4931cf18296bf4f838b07858f61ac2a6d
Marek Marecki <marekjm@ozro.pw>
2017-11-30 12:47:42 +0100

    Disable optimised builds on Travis CI

--- scripts/test_full.sh
+++ scripts/test_full.sh
@@ -4,17 +4,27 @@ set -e
 set -x
 
 # First, test with sanitisers.
+echo "Running sanitised build"
 make clean
 make "$@"
 VIUA_TEST_SUITE_VALGRIND_CHECKS=0 make test
 
 # Then, test without sanitisers but with Valgrind.
+echo "Running Valgrind build"
 make clean
 make GENERIC_SANITISER_FLAGS= CLANG_SANITISER_FLAGS= GCC_SANITISER_FLAGS= "$@"
 make test
 
-# Then, test with optimisations enabled, and
-# without either Valgrind or sanitisers slowing the machine down.
-make clean
-make GENERIC_SANITISER_FLAGS= CLANG_SANITISER_FLAGS= GCC_SANITISER_FLAGS= CXXOPTIMIZATIONFLAGS=-O3 "$@"
-VIUA_TEST_SUITE_VALGRIND_CHECKS=0 make test
+if [[ $OPTIMISED == '' ]]; then
+    OPTIMISED=yes
+fi
+if [[ $OPTIMISED == 'yes' ]]; then
+    # Then, test with optimisations enabled, and
+    # without either Valgrind or sanitisers slowing the machine down.
+    # Note that compiling Viua with optimisations enabled requires a machine with more
+    # about 8GB of RAM.
+    echo "Running optimised build"
+    make clean
+    make GENERIC_SANITISER_FLAGS= CLANG_SANITISER_FLAGS= GCC_SANITISER_FLAGS= CXXOPTIMIZATIONFLAGS=-O3 "$@"
+    VIUA_TEST_SUITE_VALGRIND_CHECKS=0 make test
+fi
