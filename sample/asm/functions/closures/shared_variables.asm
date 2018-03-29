e940f3d84ff224b6b5cd9da63776e6bca838b00b
Marek Marecki <marekjm@ozro.pw>
2018-03-29 12:07:14 +0200

    Fix usage of implicit "current" register set specifier

--- sample/asm/functions/closures/shared_variables.asm
+++ sample/asm/functions/closures/shared_variables.asm
@@ -33,7 +33,7 @@
 
 .function: returns_closures/0
     ; create a vector to store closures
-    vector %1
+    vector %1 local %0 local %0
 
     ; create a value to be bound in both closures
     integer %2 42
--- sample/asm/vector/vec_packing_out_of_range.asm
+++ sample/asm/vector/vec_packing_out_of_range.asm
@@ -1,12 +1,12 @@
 .function: main/0
-    string %2 "answer to life"
-    integer %3 42
+    string %2 local "answer to life"
+    integer %3 local 42
 
     -- this would pack outside of available
     -- register %set index range
-    vector %1 %2 %20
+    vector %1 local %2 local %20
 
-    print %1
+    print %1 local
 
     izero %0 local
     return
