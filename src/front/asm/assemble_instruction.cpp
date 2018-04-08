0132f7a3144cd54ea6d06d6e7b6dbabfef87b5be
Marek Marecki <marekjm@ozro.pw>
2018-04-08 16:31:21 +0200

    Separate function for assembling "remove"

--- src/front/asm/assemble_instruction.cpp
+++ src/front/asm/assemble_instruction.cpp
@@ -559,6 +559,35 @@ static auto assemble_op_msg(Program& program, std::vector<Token> const& tokens,
             program.opmsg(ret, tokens.at(fn));
         }
 }
+static auto assemble_op_remove(Program& program, std::vector<Token> const& tokens,
+        Token_index const i) -> void {
+        Token_index target = i + 1;
+        Token_index source = target + 2;
+        Token_index key    = source + 2;
+
+        if (tokens.at(target) == "void") {
+            --source;
+            --key;
+            program.opremove(
+                assembler::operands::getint(resolveregister(tokens.at(target))),
+                assembler::operands::getint_with_rs_type(
+                    resolveregister(tokens.at(source)),
+                    resolve_rs_type(tokens.at(source + 1))),
+                assembler::operands::getint_with_rs_type(
+                    resolveregister(tokens.at(key)),
+                    resolve_rs_type(tokens.at(key + 1))));
+        } else {
+            program.opremove(assembler::operands::getint_with_rs_type(
+                                 resolveregister(tokens.at(target)),
+                                 resolve_rs_type(tokens.at(target + 1))),
+                             assembler::operands::getint_with_rs_type(
+                                 resolveregister(tokens.at(source)),
+                                 resolve_rs_type(tokens.at(source + 1))),
+                             assembler::operands::getint_with_rs_type(
+                                 resolveregister(tokens.at(key)),
+                                 resolve_rs_type(tokens.at(key + 1))));
+        }
+}
 
 viua::internals::types::bytecode_size assemble_instruction(
     Program& program,
@@ -1575,32 +1604,7 @@ viua::internals::types::bytecode_size assemble_instruction(
                              resolveregister(tokens.at(source)),
                              resolve_rs_type(tokens.at(source + 1))));
     } else if (tokens.at(i) == "remove") {
-        Token_index target = i + 1;
-        Token_index source = target + 2;
-        Token_index key    = source + 2;
-
-        if (tokens.at(target) == "void") {
-            --source;
-            --key;
-            program.opremove(
-                assembler::operands::getint(resolveregister(tokens.at(target))),
-                assembler::operands::getint_with_rs_type(
-                    resolveregister(tokens.at(source)),
-                    resolve_rs_type(tokens.at(source + 1))),
-                assembler::operands::getint_with_rs_type(
-                    resolveregister(tokens.at(key)),
-                    resolve_rs_type(tokens.at(key + 1))));
-        } else {
-            program.opremove(assembler::operands::getint_with_rs_type(
-                                 resolveregister(tokens.at(target)),
-                                 resolve_rs_type(tokens.at(target + 1))),
-                             assembler::operands::getint_with_rs_type(
-                                 resolveregister(tokens.at(source)),
-                                 resolve_rs_type(tokens.at(source + 1))),
-                             assembler::operands::getint_with_rs_type(
-                                 resolveregister(tokens.at(key)),
-                                 resolve_rs_type(tokens.at(key + 1))));
-        }
+        assemble_op_remove(program, tokens, i);
     } else if (tokens.at(i) == "return") {
         program.opreturn();
     } else if (tokens.at(i) == "halt") {
