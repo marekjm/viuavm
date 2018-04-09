fff25839584bb80d5bb18ff07e96a7acc6bdfa5d
Marek Marecki <marekjm@ozro.pw>
2018-04-09 11:53:04 +0200

    Separate function for assembling "or"

--- src/front/asm/assemble_instruction.cpp
+++ src/front/asm/assemble_instruction.cpp
@@ -781,19 +781,7 @@ viua::internals::types::bytecode_size assemble_instruction(
     } else if (tokens.at(i) == "and") {
         assemble_three_register_op<&Program::opand>(program, tokens, i);
     } else if (tokens.at(i) == "or") {
-        Token_index target = i + 1;
-        Token_index lhs    = target + 2;
-        Token_index rhs    = lhs + 2;
-
-        program.opor(assembler::operands::getint_with_rs_type(
-                         resolveregister(tokens.at(target)),
-                         resolve_rs_type(tokens.at(target + 1))),
-                     assembler::operands::getint_with_rs_type(
-                         resolveregister(tokens.at(lhs)),
-                         resolve_rs_type(tokens.at(lhs + 1))),
-                     assembler::operands::getint_with_rs_type(
-                         resolveregister(tokens.at(rhs)),
-                         resolve_rs_type(tokens.at(rhs + 1))));
+        assemble_three_register_op<&Program::opor>(program, tokens, i);
     } else if (tokens.at(i) == "bits") {
         assemble_op_bits(program, tokens, i);
     } else if (tokens.at(i) == "bitand") {
