96bedd7c43d9af935856809d995727a9e8ed71b2
Marek Marecki <marekjm@ozro.pw>
2018-04-09 11:51:12 +0200

    Separate function for assembling "gte"

--- src/front/asm/assemble_instruction.cpp
+++ src/front/asm/assemble_instruction.cpp
@@ -696,19 +696,7 @@ viua::internals::types::bytecode_size assemble_instruction(
     } else if (tokens.at(i) == "gt") {
         assemble_three_register_op<&Program::opgt>(program, tokens, i);
     } else if (tokens.at(i) == "gte") {
-        Token_index target = i + 1;
-        Token_index lhs    = target + 2;
-        Token_index rhs    = lhs + 2;
-
-        program.opgte(assembler::operands::getint_with_rs_type(
-                          resolveregister(tokens.at(target)),
-                          resolve_rs_type(tokens.at(target + 1))),
-                      assembler::operands::getint_with_rs_type(
-                          resolveregister(tokens.at(lhs)),
-                          resolve_rs_type(tokens.at(lhs + 1))),
-                      assembler::operands::getint_with_rs_type(
-                          resolveregister(tokens.at(rhs)),
-                          resolve_rs_type(tokens.at(rhs + 1))));
+        assemble_three_register_op<&Program::opgte>(program, tokens, i);
     } else if (tokens.at(i) == "eq") {
         Token_index target = i + 1;
         Token_index lhs    = target + 2;
