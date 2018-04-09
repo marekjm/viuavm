9734eef1d537078bc974144539770bf2fcf5a6ae
Marek Marecki <marekjm@ozro.pw>
2018-04-09 12:48:51 +0200

    Separate function for assembling "swap"

--- src/front/asm/assemble_instruction.cpp
+++ src/front/asm/assemble_instruction.cpp
@@ -905,15 +905,7 @@ viua::internals::types::bytecode_size assemble_instruction(
     } else if (tokens.at(i) == "ptr") {
         assemble_double_register_op<&Program::opptr>(program, tokens, i);
     } else if (tokens.at(i) == "swap") {
-        Token_index target = i + 1;
-        Token_index source = target + 2;
-
-        program.opswap(assembler::operands::getint_with_rs_type(
-                           resolveregister(tokens.at(target)),
-                           resolve_rs_type(tokens.at(target + 1))),
-                       assembler::operands::getint_with_rs_type(
-                           resolveregister(tokens.at(source)),
-                           resolve_rs_type(tokens.at(source + 1))));
+        assemble_double_register_op<&Program::opswap>(program, tokens, i);
     } else if (tokens.at(i) == "delete") {
         Token_index target = i + 1;
 
