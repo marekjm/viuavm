0212685c5b9c60534b2e39437613ea17d65c2a69
Marek Marecki <marekjm@ozro.pw>
2018-04-09 11:52:13 +0200

    Separate function for assembling "textcommonsuffix"

--- src/front/asm/assemble_instruction.cpp
+++ src/front/asm/assemble_instruction.cpp
@@ -750,19 +750,7 @@ viua::internals::types::bytecode_size assemble_instruction(
     } else if (tokens.at(i) == "textcommonprefix") {
         assemble_three_register_op<&Program::optextcommonprefix>(program, tokens, i);
     } else if (tokens.at(i) == "textcommonsuffix") {
-        Token_index target = i + 1;
-        Token_index lhs    = target + 2;
-        Token_index rhs    = lhs + 2;
-
-        program.optextcommonsuffix(assembler::operands::getint_with_rs_type(
-                                       resolveregister(tokens.at(target)),
-                                       resolve_rs_type(tokens.at(target + 1))),
-                                   assembler::operands::getint_with_rs_type(
-                                       resolveregister(tokens.at(lhs)),
-                                       resolve_rs_type(tokens.at(lhs + 1))),
-                                   assembler::operands::getint_with_rs_type(
-                                       resolveregister(tokens.at(rhs)),
-                                       resolve_rs_type(tokens.at(rhs + 1))));
+        assemble_three_register_op<&Program::optextcommonsuffix>(program, tokens, i);
     } else if (tokens.at(i) == "textconcat") {
         Token_index target = i + 1;
         Token_index lhs    = target + 2;
