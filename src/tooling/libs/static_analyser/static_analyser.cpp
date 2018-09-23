a5b7945146d2c7b1837cde3d7d06a24b5c4fb387
Marek Marecki <marekjm@ozro.pw>
2018-09-23 15:15:53 +0200

    Add `string` to static analyser

--- src/tooling/libs/static_analyser/static_analyser.cpp
+++ src/tooling/libs/static_analyser/static_analyser.cpp
@@ -1208,7 +1208,24 @@ static auto analyse_single_function(
                 } case GT: {
                 } case GTE: {
                 } case EQ: {
+                    // FIXME
+                    break;
                 } case STRING: {
+                    auto const& dest = *static_cast<Register_address const*>(instruction.operands.at(0).get());
+
+                    auto defining_tokens = std::vector<viua::tooling::libs::lexer::Token>{};
+                    defining_tokens.push_back(line->token(0));
+
+                    std::copy(dest.tokens().begin(), dest.tokens().end(), std::back_inserter(defining_tokens));
+
+                    function_state.define_register(
+                        function_state.resolve_index(dest)
+                        , dest.register_set
+                        , function_state.make_wrapper(std::make_unique<values::String>())
+                        , std::move(defining_tokens)
+                    );
+
+                    break;
                 } case STREQ: {
                 } case TEXTEQ: {
                 } case TEXTAT: {
