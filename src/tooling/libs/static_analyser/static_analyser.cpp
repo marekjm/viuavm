9d8acb512d9c8f0d0337460b17cdbc547284598e
Marek Marecki <marekjm@ozro.pw>
2018-10-10 21:02:31 +0200

    Ignore that, it was not a special case

--- src/tooling/libs/static_analyser/static_analyser.cpp
+++ src/tooling/libs/static_analyser/static_analyser.cpp
@@ -1740,17 +1740,24 @@ static auto analyse_single_function(
                         , function_state.type_of(source_index, source.register_set).to_simple()));
                     throw_if_invalid_type(function_state, dest, dest_index, dest_type_signature);
 
-                    /*
-                     * Special-case when the leading type is pointer.
-                     *
-                     * Why? Because then the value inserted is not erased and having its type
-                     * inferred is useful.
-                     */
                     if (dest_type_signature.front() == values::Value_type::Pointer) {
                         auto& wrapper = static_cast<values::Vector&>(static_cast<values::Pointer&>(
                             function_state.type_of(dest_index, dest.register_set).value()
                         ).of().value());
-                        wrapper.of(function_state.type_of(source_index, source.register_set));
+                        if (source.access == viua::internals::Access_specifier::POINTER_DEREFERENCE) {
+                            wrapper.of(static_cast<values::Pointer&>(function_state.type_of(source_index, source.register_set).value()).of());
+                        } else {
+                            wrapper.of(function_state.type_of(source_index, source.register_set));
+                        }
+                    } else {
+                        auto& wrapper = static_cast<values::Vector&>(
+                            function_state.type_of(dest_index, dest.register_set).value()
+                        );
+                        if (source.access == viua::internals::Access_specifier::POINTER_DEREFERENCE) {
+                            wrapper.of(static_cast<values::Pointer&>(function_state.type_of(source_index, source.register_set).value()).of());
+                        } else {
+                            wrapper.of(function_state.type_of(source_index, source.register_set));
+                        }
                     }
 
                     if (source.access != viua::internals::Access_specifier::POINTER_DEREFERENCE) {
