ecb3ac14f61971542f3011572760bc6b0ee737c6
Marek Marecki <marekjm@ozro.pw>
2018-11-11 13:38:28 +0100

    Typecheck `structkeys`

--- src/tooling/libs/static_analyser/static_analyser.cpp
+++ src/tooling/libs/static_analyser/static_analyser.cpp
@@ -2423,6 +2423,31 @@ static auto analyse_single_function(
 
                     break;
                 } case STRUCTKEYS: {
+                    auto const& source =
+                        *static_cast<Register_address const*>(instruction.operands.at(1).get());
+                    auto const source_index = throw_if_empty(function_state, source);
+                    auto const source_type_signature = maybe_with_pointer(source.access, {
+                        values::Value_type::Struct
+                    });
+                    throw_if_invalid_type(function_state, source, source_index, source_type_signature);
+
+                    auto const& dest =
+                        *static_cast<Register_address const*>(instruction.operands.at(0).get());
+
+                    auto defining_tokens = std::vector<viua::tooling::libs::lexer::Token>{};
+                    defining_tokens.push_back(line->token(0));
+                    copy_whole(dest.tokens(), std::back_inserter(defining_tokens));
+
+                    auto const dest_index = function_state.resolve_index(dest);
+                    function_state.define_register(
+                        dest_index
+                        , dest.register_set
+                        , function_state.make_wrapper(std::make_unique<values::Vector>(
+                            function_state.make_wrapper(std::make_unique<values::Atom>())
+                        ))
+                        , std::move(defining_tokens)
+                    );
+
                     break;
                 } case RETURN: {
                 } case HALT: {
