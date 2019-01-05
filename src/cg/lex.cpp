d449381c5d98f428267986d6e0aa319915e5dc85
Marek Marecki <marekjm@ozro.pw>
2019-01-05 12:31:10 +0100

    Update copyright notices

--- src/cg/lex.cpp
+++ src/cg/lex.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2016, 2017, 2018 Marek Marecki
+ *  Copyright (C) 2016-2019 Marek Marecki
  *
  *  This file is part of Viua VM.
  *
--- src/tooling/libs/lexer/normaliser.cpp
+++ src/tooling/libs/lexer/normaliser.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2018 Marek Marecki
+ *  Copyright (C) 2018, 2019 Marek Marecki
  *
  *  This file is part of Viua VM.
  *
