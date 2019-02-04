350973c17c432dcc948584994b39b103751b196f
Marek Marecki <marekjm@ozro.pw>
2019-02-04 19:26:09 +0100

    Typecheck RECEIVE instruction

--- src/tooling/libs/static_analyser/static_analyser.cpp
+++ src/tooling/libs/static_analyser/static_analyser.cpp
@@ -2880,6 +2880,23 @@ static auto analyse_single_arm(
                 break;
             }
             case RECEIVE: {
+                auto const& dest = *static_cast<Register_address const*>(
+                    instruction.operands.at(0).get());
+
+                auto defining_tokens =
+                    std::vector<viua::tooling::libs::lexer::Token>{};
+                defining_tokens.push_back(line->token(0));
+                copy_whole(dest.tokens(), std::back_inserter(defining_tokens));
+
+                auto const dest_index = function_state.resolve_index(dest);
+                function_state.define_register(
+                    dest_index,
+                    dest.register_set,
+                    function_state.make_wrapper(
+                        std::make_unique<values::Value>(values::Value_type::Value)),
+                    std::move(defining_tokens));
+
+                break;
             }
             case WATCHDOG: {
                 // FIXME TODO
