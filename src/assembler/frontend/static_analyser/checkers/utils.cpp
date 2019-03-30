e56e8d8700014c0482c291e6320b3e81f516c088
Marek Marecki <marekjm@ozro.pw>
2019-03-30 11:42:50 +0100

    Better error message for out of bounds register access

--- src/assembler/frontend/static_analyser/checkers/utils.cpp
+++ src/assembler/frontend/static_analyser/checkers/utils.cpp
@@ -205,7 +205,7 @@ auto check_use_of_register(Register_usage_profile& rup,
                       "register of `copy` and `move` instructions"));
     }
 
-    if ((not rup.in_bounds(r)) and r.rss != Register_sets::PARAMETERS) {
+    if ((not rup.in_bounds(r)) and r.rss == Register_sets::LOCAL) {
         throw Traced_syntax_error{}
             .append(Invalid_syntax{
                 r.tokens.at(0),
@@ -216,24 +216,17 @@ auto check_use_of_register(Register_usage_profile& rup,
             .append(Invalid_syntax{rup.allocated_where().value(), ""}.note(
                 "increase this value to " + std::to_string(r.index + 1)
                 + " to fix this issue"));
-    }
-
-    if ((not rup.defined(Register(r))) and r.rss == Register_sets::PARAMETERS) {
-        auto msg = std::ostringstream{};
-        if (rup.erased(Register(r))) {
-            msg << error_core_msg << " erased " << to_string(r.rss)
-                << " register " << str::enquote(std::to_string(r.index));
-        } else {
-            msg << error_core_msg << ' ' << to_string(r.rss) << " register "
-                << str::enquote(std::to_string(r.index)) << " out of range";
-        }
-        auto error = Traced_syntax_error{}.append(
-            Invalid_syntax(r.tokens.at(0), msg.str()));
-        if (rup.erased(Register(r))) {
-            error.append(Invalid_syntax(rup.erased_where(Register(r)), "")
-                             .note("erased here:"));
-        }
-        throw error;
+    } else if (not rup.in_bounds(r)) {
+        auto const msg = "access to "
+            + to_string(r.rss)
+            + " register "
+            + str::enquote(std::to_string(r.index))
+            + " out of bounds";
+        throw Traced_syntax_error{}
+            .append(Invalid_syntax{
+                r.tokens.at(0),
+                msg
+            });
     }
 
     if (not rup.defined(Register(r))) {
