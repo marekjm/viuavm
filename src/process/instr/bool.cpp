815077ad5aed85713f22d1a54f579ed9451bdaa2
Marek Marecki <marekjm@ozro.pw>
2019-10-28 20:52:23 +0100

    Deduplicate code implementing binary logical operations

--- src/process/instr/bool.cpp
+++ src/process/instr/bool.cpp
@@ -17,6 +17,7 @@
  *  along with Viua VM.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <functional>
 #include <viua/bytecode/bytetypedef.h>
 #include <viua/bytecode/decoder/operands.h>
 #include <viua/kernel/kernel.h>
@@ -40,36 +41,29 @@ auto viua::process::Process::opnot(Op_address_type addr) -> Op_address_type {
     return addr;
 }
 
-auto viua::process::Process::opand(Op_address_type addr) -> Op_address_type {
+using Addr_type = viua::internals::types::Op_address_type;
+template<typename Oper>
+static auto binary_logical_op(Addr_type addr, viua::process::Process* proc) -> Addr_type {
     viua::kernel::Register* target = nullptr;
-    tie(addr, target) =
-        viua::bytecode::decoder::operands::fetch_register(addr, this);
+    std::tie(addr, target) =
+        viua::bytecode::decoder::operands::fetch_register(addr, proc);
 
     viua::types::Value *first = nullptr, *second = nullptr;
-    tie(addr, first) =
-        viua::bytecode::decoder::operands::fetch_object(addr, this);
-    tie(addr, second) =
-        viua::bytecode::decoder::operands::fetch_object(addr, this);
+    std::tie(addr, first) =
+        viua::bytecode::decoder::operands::fetch_object(addr, proc);
+    std::tie(addr, second) =
+        viua::bytecode::decoder::operands::fetch_object(addr, proc);
 
-    *target = make_unique<viua::types::Boolean>(first->boolean()
-                                                and second->boolean());
+    *target = std::make_unique<viua::types::Boolean>(Oper{}(first->boolean(),
+                                                second->boolean()));
 
     return addr;
 }
 
+auto viua::process::Process::opand(Op_address_type addr) -> Op_address_type {
+    return binary_logical_op<std::logical_and<bool>>(addr, this);
+}
+
 auto viua::process::Process::opor(Op_address_type addr) -> Op_address_type {
-    viua::kernel::Register* target = nullptr;
-    tie(addr, target) =
-        viua::bytecode::decoder::operands::fetch_register(addr, this);
-
-    viua::types::Value *first = nullptr, *second = nullptr;
-    tie(addr, first) =
-        viua::bytecode::decoder::operands::fetch_object(addr, this);
-    tie(addr, second) =
-        viua::bytecode::decoder::operands::fetch_object(addr, this);
-
-    *target = make_unique<viua::types::Boolean>(first->boolean()
-                                                or second->boolean());
-
-    return addr;
+    return binary_logical_op<std::logical_or<bool>>(addr, this);
 }
