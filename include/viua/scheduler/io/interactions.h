6bd72193e083396ffd5ad352b74bb0fd0b344bc7
Marek Marecki <marekjm@ozro.pw>
2019-11-04 22:18:38 +0100

    Fix: add initialiser

--- include/viua/scheduler/io/interactions.h
+++ include/viua/scheduler/io/interactions.h
@@ -91,7 +91,7 @@ struct IO_interaction {
         using result_type = std::unique_ptr<viua::types::Value>;
 
         State const state = State::In_flight;
-        std::optional<Status> const status;
+        std::optional<Status> const status = std::nullopt;
         std::unique_ptr<viua::types::Value> result;
 
         Interaction_result() = default;
