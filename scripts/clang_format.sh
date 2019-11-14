ce745896af25150f30b0c7cca5fbe4ce67be7eb7
Marek Marecki <marekjm@ozro.pw>
2019-11-14 21:32:11 +0100

    Fix formatting script

--- scripts/clang_format.sh
+++ scripts/clang_format.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-(find ./include -name '*.h' ; find ./src -name '*.cpp') | xargs -n 1 --verbose clang-format -i
+MODE=$1
+
+if [[ $MODE == 'git' ]]; then
+    git ls-files -m | grep -P '\.(h|cpp)$' | xargs -n 1 --verbose clang-format -i
+else
+    (find ./include -name '*.h' ; find ./src -name '*.cpp') | xargs -n 1 --verbose clang-format -i
+fi
