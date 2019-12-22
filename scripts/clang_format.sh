a46ebaf6d2bdac8f7af7cce4779b380603fd778c
Marek Marecki <marekjm@ozro.pw>
2019-12-22 17:49:38 +0100

    Format faster by using more CPUs (if available)

--- scripts/clang_format.sh
+++ scripts/clang_format.sh
@@ -2,8 +2,13 @@
 
 MODE=$1
 
+CPUS_AVAILABLE=$(sed 's/^processor[\t ]*: *\([0-9]*\)/\1/;t;d' /proc/cpuinfo | tail -n 1)
+
 if [[ $MODE == 'git' ]]; then
-    git ls-files -m | grep -P '\.(h|cpp)$' | xargs -n 1 --verbose clang-format -i
+    git ls-files -m |
+        grep -P '\.(h|cpp)$' |
+        xargs -n 1 -P $CPUS_AVAILABLE --verbose clang-format -i
 else
-    (find ./include -name '*.h' ; find ./src -name '*.cpp') | xargs -n 1 --verbose clang-format -i
+    (find ./include -name '*.h' ; find ./src -name '*.cpp') |
+        xargs -n 1 -P $CPUS_AVAILABLE --verbose clang-format -i
 fi
