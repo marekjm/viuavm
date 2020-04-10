dedab7bb01b9449015229444bc050c7111de97e0
Marek Marecki <marekjm@ozro.pw>
2020-04-10 21:05:48 +0200

    Products and entries

--- misc/platformy_technologiczne/pt.sql
+++ misc/platformy_technologiczne/pt.sql
@@ -12,6 +12,23 @@ create table orders (
     , order_date timestamp
 );
 
+drop table if exists products;
+create table products (
+      id serial primary key
+    , name varchar
+    , price decimal(8,4)
+);
+
+drop table if exists entries;
+create table entries (
+      id serial primary key
+    , order_id integer
+    , product_id integer
+    , unit_price decimal(8,4)
+    , discount decimal(8,4)
+    , quantity decimal(8,4)
+);
+
 insert into employees (name) values
       ('John Doe')
     , ('Jane Doe')
@@ -45,4 +62,17 @@ insert into orders (customer, employee, order_date) values
     , ('FOLKO', 6, '1996-07-24 12:00:00')
     , ('BLONP', 2, '1996-07-25 12:00:00')
     , ('WARTH', 3, '1996-07-26 12:00:00')
+;
+
+insert into products (name, price) values
+      ('Foo', 2.50)
+    , ('Bar', 16.00)
+    , ('Spam', 0.99)
+    , ('Baz', 0.19)
+    , ('The answer to the Universe, Life, and Everything', 42.00)
+;
+
+insert into entries (order_id, product_id, unit_price, discount, quantity) values
+      (1, 1, 2.50, 0.0, 2.0)
+    , (1, 2, 16.00, 0.0, 0.16)
 ;
