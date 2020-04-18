27c4de5a4179eb5fccc9357761bd2d633af4a955
Marek Marecki <marekjm@ozro.pw>
2020-04-18 18:59:30 +0200

    Add an aside note about correct atom literals
    Atom literals use single quotes (like this: 'good') instead of double
    quotes (like this: "bad") which are used by strings.

--- src/assembler/frontend/static_analyser/checkers/check_op_atom.cpp
+++ src/assembler/frontend/static_analyser/checkers/check_op_atom.cpp
@@ -28,6 +28,7 @@ auto check_op_atom(Register_usage_profile& register_usage_profile,
                    Instruction const& instruction) -> void
 {
     using viua::assembler::frontend::parser::Atom_literal;
+    using viua::assembler::frontend::parser::Text_literal;
 
     auto operand = get_operand<Register_index>(instruction, 0);
     if (not operand) {
@@ -40,9 +41,16 @@ auto check_op_atom(Register_usage_profile& register_usage_profile,
 
     auto source = get_operand<Atom_literal>(instruction, 1);
     if (not source) {
-        throw invalid_syntax(instruction.operands.at(1)->tokens,
+        auto error = invalid_syntax(instruction.operands.at(1)->tokens,
                              "invalid operand")
             .note("expected atom literal");
+        if (auto s = get_operand<Text_literal>(instruction, 1); s) {
+            error.aside(
+                instruction.operands.at(1)->tokens.at(0)
+                , ("atom literals use single quotes: '"
+                   + s->content.substr(1, s->content.size() - 2) + "'"));
+        }
+        throw error;
     }
 
     auto val         = Register{};
