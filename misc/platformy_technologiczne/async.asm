818ca87ac347e3b09f99c9871a582088638d4ffe
Marek Marecki <marekjm@ozro.pw>
2020-04-18 19:00:28 +0200

    WIP

--- misc/platformy_technologiczne/async.asm
+++ misc/platformy_technologiczne/async.asm
@@ -53,15 +53,96 @@
     call %0 local make_tagged_message_impl/1
     return
 .end
+.function: make_get_field_message/3
+    allocate_registers %5 local
+
+    .name: 0 message
+    .name: iota tag
+    .name: iota reply_to
+    .name: iota q
+    .name: iota field
+
+    frame %1
+    atom %tag local 'get_field'
+    move %0 arguments %tag local
+    call %message local make_tagged_message_impl/1
+
+    atom %tag local 'reply_to'
+    move %reply_to local %0 parameters
+    structinsert %message local %tag local %reply_to local
+
+    atom %tag local 'q'
+    move %q local %1 parameters
+    structinsert %message local %tag local %q local
+
+    atom %tag local 'field'
+    move %field local %2 parameters
+    structinsert %message local %tag local %field local
+
+    return
+.end
+.function: make_postgres_result_message/1
+    allocate_registers %3 local
+
+    .name: 0 message
+    .name: iota tag
+    .name: iota value
+
+    frame %1
+    atom %tag local 'postgres_result'
+    move %0 arguments %tag local
+    call %message local make_tagged_message_impl/1
+
+    atom %tag local 'value'
+    move %value local %0 parameters
+    structinsert %message local %tag local %value local
+
+    return
+.end
 
 
+.function: postgres_io_get_field_impl/2
+    allocate_registers %6 local
+
+    .name: 0 r0
+    .name: iota pq_conn
+    .name: iota message
+    .name: iota reply_to
+    .name: iota q
+    .name: iota field
+
+    move %pq_conn local %0 parameters
+    move %message local %1 parameters
+
+    atom %q local 'q'
+    structremove %q local %message local %q local
+
+    frame %2
+    move %0 arguments %pq_conn local
+    move %1 arguments %q local
+    call %q local viuapq::get_one/2
+
+    atom %field local 'field'
+    structremove %field local %message local %field local
+    structremove %field local %q local %field local
+
+    frame %1
+    move %0 arguments %field local
+    call %field local make_postgres_result_message/1
+
+    atom %reply_to local 'reply_to'
+    structremove %reply_to local %message local %reply_to local
+
+    send %reply_to local %field local
+
+    return
+.end
 .function: postgres_io_actor/1
-    allocate_registers %9 local
+    allocate_registers %8 local
 
     .name: 0 r0
     .name: iota connection
     .name: iota message
-    .name: iota key
     .name: iota got_tag
     .name: iota tag_shutdown
     .name: iota tag_get_one_row
@@ -96,8 +177,17 @@
     .mark: stage_get_one_row
     .mark: stage_get_all_rows
     .mark: stage_get_field
+    frame %2
+    ptr %r0 local %connection local
+    move %0 arguments %r0 local
+    move %1 arguments %message local
+    call void postgres_io_get_field_impl/2
 
     .mark: stage_shutdown
+    frame %1
+    move %0 arguments %connection local
+    call void viuapq::finish/1
+
     string %r0 local "Postgres I/O actor shutting down"
     print %r0 local
     return
@@ -127,9 +217,21 @@
     process %postgres_connection local postgres_io_actor/1
     ; end POSTGRESQL CONNECTION
 
+    frame %3
+    self %r0 local
+    move %0 arguments %r0 local
+    string %r0 local "select count(*) from a"
+    move %1 arguments %r0 local
+    atom %r0 local 'count'
+    move %2 arguments %r0 local
+    call %r0 local make_get_field_message/3
+    send %postgres_connection local %r0 local
+
+    receive %r0 local infinity
+    print %r0 local
+
     frame %0
     call %r0 local make_shutdown_message/0
-
     send %postgres_connection local %r0 local
 
     join void %postgres_connection local infinity
