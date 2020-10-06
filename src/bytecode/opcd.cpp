3a9c50864256ae5556b41449ee1d34a7a0b93214
Marek Marecki <marekjm@ozro.pw>
2020-10-06 17:28:20 +0200

    Update bytecode listing program

--- src/bytecode/opcd.cpp
+++ src/bytecode/opcd.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2015, 2016 Marek Marecki
+ *  Copyright (C) 2015, 2016, 2020 Marek Marecki
  *
  *  This file is part of Viua VM.
  *
@@ -19,6 +19,7 @@
 
 #include <algorithm>
 #include <iostream>
+#include <iomanip>
 #include <map>
 #include <string>
 
@@ -29,45 +30,25 @@
 
 int main()
 {
-    auto const max_mnemonic_length = []() -> std::string::size_type {
-        auto l = std::string::size_type{0};
-        for (std::pair<const OPCODE, std::string> i : OP_NAMES) {
-            l = std::max(l, i.second.size());
-        }
-        return (l + 1);
-    }();
-
-    auto const initial_column = std::string{"MNEMONIC            "};
-    std::cout << initial_column << "| OPCODE  | HEX OPCODE\n" << std::endl;
-
-    auto const column_length =
-        std::max(max_mnemonic_length, initial_column.size());
-
     for (auto i = uint8_t{0}; i < static_cast<uint8_t>(0xff); ++i) {
         auto const opcode = static_cast<OPCODE>(i);
-        auto mnemonic     = std::string{"??"};
-        if (OP_NAMES.count(opcode)) {
-            mnemonic = OP_NAMES.at(opcode);
-        } else {
+        if (!OP_NAMES.count(opcode)) {
             continue;
         }
 
-        std::cout << mnemonic;
-        std::cout << ' ';
-        for (auto j = mnemonic.size() + 1; j < column_length; ++j) {
-            std::cout << '.';
-        }
-        std::cout << "| ";
+        std::cout
+            << std::dec
+            << std::setw(3)
+            << std::setfill(' ')
+            << static_cast<uint64_t>(opcode);
+        std::cout << "  0x";
+        std::cout
+            << std::hex
+            << std::setw(2)
+            << std::setfill('0')
+            << static_cast<uint64_t>(opcode);
 
-        std::cout << "  ";
-        std::cout << (opcode < 10 ? " " : "");
-        std::cout << (opcode < 100 ? " " : "");
-        std::cout << opcode;
-        std::cout << "       ";
-        std::cout << "0x";
-        std::cout << (opcode < 0x10 ? "0" : "");
-        std::cout << std::hex << opcode << std::dec;
-        std::cout << '\n';
+        std::cout << "  " << OP_NAMES.at(opcode) << '\n';
     }
     return 0;
 }
