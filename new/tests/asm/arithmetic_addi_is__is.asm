452b921d340c1fb878049518fce111f5b7e69a12
Marek Marecki <marekjm@ozro.pw>
2021-12-09 23:08:16 +0100

    Add some more tests

---
+++ new/tests/asm/arithmetic_addi_is__is.asm
@@ -0,0 +1,7 @@
+.function: [[entry_point]] main
+    li $1, 39
+    addi $2, $1, 3
+
+    ebreak
+    return
+.end
---
+++ new/tests/asm/arithmetic_addi_is__refis.asm
@@ -0,0 +1,8 @@
+.function: [[entry_point]] main
+    li $1, 39
+    ref $2, $1
+    addi $3, *2, 3
+
+    ebreak
+    return
+.end
