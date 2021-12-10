38340bd1d790b357d4c88b8f815f448298bcc44a
Marek Marecki <marekjm@ozro.pw>
2021-12-10 17:46:31 +0100

    Test float and double constructor ops

---
+++ new/tests/asm/double.asm
@@ -0,0 +1,6 @@
+.function: [[entry_point]] main
+    double $1, 6.02214076
+
+    ebreak
+    return
+.end
---
+++ new/tests/asm/float.asm
@@ -0,0 +1,6 @@
+.function: [[entry_point]] main
+    float $1, 3.14
+
+    ebreak
+    return
+.end
