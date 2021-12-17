e5efd8feed1e9cdbd5d2818b05190c58c6c35fe5
Marek Marecki <marekjm@ozro.pw>
2021-12-17 22:00:58 +0100

    Update the loop test
    Make it more useful by testing more stuff.

--- new/tests/asm/loop_for_1_to_10.asm
+++ new/tests/asm/loop_for_1_to_10.asm
@@ -4,13 +4,20 @@
 
     ; 2nd instruction
     gt $3, $1, $2
-    if $3, 7
+    if $3, 9
+
+    ; These two instructions are here just to test whether the calculations that
+    ; the assembler makes to conver logical indexes into physical indexes are
+    ; correct. Why these two? Because li with big numbers expands to more than
+    ; one instruction.
+    g.li $4, 318736561391831
+    delete $4
 
     ebreak
     addi $1, $1, 1u
     jump 2
 
-    ; 7th instruction
+    ; 9th instruction
     ebreak
     return
 .end
