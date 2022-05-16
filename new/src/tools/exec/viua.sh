71397962ed5a68b918f423eb0ec127d1f333c82f
Marek Marecki <marekjm@ozro.pw>
2022-05-17 00:07:56 +0200

    Add help mode to the entry point script
    The following command displays help screen for viua-xyz:
    
        $ viua help xyz
    
    Passing just the --help option displays help screen for the entry point
    script itself:
    
        $ viua --help

--- new/src/tools/exec/viua.sh
+++ new/src/tools/exec/viua.sh
@@ -4,17 +4,27 @@ set -e
 
 VIUA_DIR=~/.local/lib/viua
 
+function show_help {
+    local WHAT_FOR=${1:-overview}
+    exec man 1 viua-${WHAT_FOR}
+}
+
 function main {
     TOOL=${1}
     case ${TOOL} in
         asm|dis|vm|readelf|repl)
             exec ${VIUA_DIR}/viua-core/${TOOL} ${@:2}
             ;;
+        help)
+            show_help "${@:2}"
+            exit 0
+            ;;
+        --help|'')
+            show_help
+            exit 0
+            ;;
         --version)
             exec ${VIUA_DIR}/viua-core/vm ${@:1}
-            ;;
-        '')
-            exec ${VIUA_DIR}/viua-core/vm --version
             ;;
         -*)
             2>&1 echo "viua: error: unknown option \`${TOOL}'"
