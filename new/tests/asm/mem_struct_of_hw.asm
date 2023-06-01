4c92b9abe635ebb502a9c1318f69ed44c6dda475
Marek Marecki <marekjm@ozro.pw>
2023-06-01 18:48:36 +0200

    Add tests showing how to construct simple structs
    With word and half-word sized members. Turns out, alignment is
    important. Who would have thought?

---
+++ new/tests/asm/mem_struct_of_hw.asm
@@ -0,0 +1,36 @@
+.function: [[entry_point]] main
+    ; The struct looks like thus:
+    ;
+    ;   struct foo {
+    ;       uint16_t a;
+    ;       uint32_t b;
+    ;   };
+    ;
+    ; What is sizeof(foo)? Two words ie, 8 bytes.
+    ;
+    ; Why, if a half-word and a word is only 6 bytes? Because the second field
+    ; is a word, which needs to be properly aligned, thus creating a two byte
+    ; gap beteen the first and second member.
+    ;
+    ; Why is this gap needed? Because the multiplier (the third parmeter of SM
+    ; and LM instructions) works in increments of UNIT ie, two bytes for
+    ; half-words, four bytes for words, etc. If we wanted the keep the order of
+    ; the fields and remove the gap, accessing the second field would require
+    ; using two SM instructions and some bit-shifting.
+
+    ; Allocate space for a struct: two words.
+    li $1.l, 2u
+    amwa $1.l, $1.l, 0
+
+    ; Create the value for the first memer of the struct, a half-word, and store
+    ; it in the memory area allocated for the struct.
+    li $2.l, 0xadde
+    sh $2.l, $1.l, 0
+
+    ; Do the same for the second member, a word.
+    li $3.l, 0xefbeedfe
+    sw $3.l, $1.l, 1
+
+    ebreak
+    return
+.end
---
+++ new/tests/asm/mem_struct_of_wh.asm
@@ -0,0 +1,26 @@
+.function: [[entry_point]] main
+    ; The struct looks like thus:
+    ;
+    ;   struct foo {
+    ;       uint32_t a;
+    ;       uint16_t b;
+    ;   };
+    ;
+    ; What is sizeof(foo)? Three half-words ie, 6 bytes.
+
+    ; Allocate space for a struct: three half-words.
+    li $1.l, 2u
+    amwa $1.l, $1.l, 0
+
+    ; Create the value for the first memer of the struct, a word, and store it
+    ; in the memory area allocated for the struct.
+    li $3.l, 0xefbeedfe
+    sw $3.l, $1.l, 0
+
+    ; Do the same for the second member, a half-word.
+    li $2.l, 0xadde
+    sh $2.l, $1.l, 2
+
+    ebreak
+    return
+.end
