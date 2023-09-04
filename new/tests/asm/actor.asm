b553f14ac7d52259b288b5bbec4e68ac5357cbe3
Marek Marecki <marekjm@ozro.pw>
2023-09-04 17:43:21 +0200

    Ensure actor names are also wrapped in quotes when necessary

--- new/tests/asm/actor.asm
+++ new/tests/asm/actor.asm
@@ -1,12 +1,12 @@
 .function: [[entry_point]] main
     frame $0.a
-    actor $1, dummy
+    actor $1, "dummy::function"
 
     ebreak
     return
 .end
 
-.function: dummy
+.function: "dummy::function"
     li $1, 41
     ebreak
 
