0480ffb727057d86c6043ce94dcf4582ecd78355
Marek Marecki <marekjm@ozro.pw>
2025-05-20 23:41:10 +0200

    Make installation more civilised
    Now it works like this:
    
     - viua ie, the frontend script is installed into BINDIR to make it
       available on PATH
     - the toolchain ie, all the executable doing the actual work are
       installed into EXEDIR and are only available through the frontend
       script
     - man pages are installed into appropriate directories under the main
       MANDIR
    
    When installing Viua you can set PREFIX= to be whatever you like. For
    example:
    
        ]$ make PREFIX=~/.local install
    
    will install the frontend script, the toolchain, and the man pages in
    your home directory.
    
    Uninstalling is similarly easy. Just execute
    
        ]$ make PREFIX=~/.local uninstall
    
    to erase the toolchain from your system. Remember to use the same
    PREFIX= as for installation, or you will not actually erase the
    toolchain.
    
    You can always use
    
        ]$ viua --prefix
    
    to see where does your installation live.

--- new/src/tools/exec/viua.sh
+++ new/src/tools/exec/viua.sh
@@ -2,33 +2,22 @@
 
 set -e
 
-VIUA_DIR=${VIUA_DIR:-~/.local/lib/viua}
-VIUA_OPT_DIR=${VIUA_OPT_DIR:-~/.local/opt/viua}
-
 function show_help {
     local WHAT_FOR=${1:-viua}
-    export MANPATH=${VIUA_DIR}/../../share/man:
     exec man viua-${WHAT_FOR}
 }
 
-function viua_opt {
-    echo "optional installations in: ${VIUA_OPT_DIR}"
-    if [[ ! -d ${VIUA_OPT_DIR} ]]; then
-        exit 1
-    fi
-    for each in $(ls -1 ${VIUA_OPT_DIR}); do
-        echo -e " * ${each}\t@ $(cat ${VIUA_OPT_DIR}/${each}/etc/viua/version | cut -d. -f1,2,3)"
-    done
-}
+PREFIX=/usr
+LIBDIR=${PREFIX}/lib
+EXEDIR=${PREFIX}/libexec/viua
+BINDIR=${PREFIX}/bin
 
 function main {
     TOOL=${1}
     case ${TOOL} in
         asm|dis|vm|readelf|repl)
-            exec ${VIUA_DIR}/viua-core/${TOOL} ${@:2}
-            ;;
-        opt)
-            viua_opt "${@:2}"
+            set -x
+            exec ${EXEDIR}/${TOOL} "${@:2}"
             ;;
         help)
             show_help "${@:2}"
@@ -39,22 +28,17 @@ function main {
             exit 0
             ;;
         --version)
-            exec ${VIUA_DIR}/viua-core/vm ${@:1}
+            exec ${EXEDIR}/vm ${@:1} | sed 's/ vm//'
             ;;
-        --opt)
-            OPT_NAME=${2}
-            if [[ ${OPT_NAME} == '-' ]]; then
-                OPT_NAME=default
-            fi
-            export VIUA_DIR=${VIUA_OPT_DIR}/${OPT_NAME}/lib/viua
-            main "${@:3}"
+        --prefix)
+            echo "PREFIX=${PREFIX}"
             ;;
         -*)
             2>&1 echo "viua: error: unknown option \`${TOOL}'"
             exit 1
             ;;
         *)
-            2>&1 echo "viua: error: \`${TOOL}' is not a part of the toolchain"
+            2>&1 echo "viua: error: not a part of the viua toolchain: \`${TOOL}'"
             exit 1
             ;;
     esac
