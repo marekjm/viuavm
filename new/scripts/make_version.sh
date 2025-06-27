53f9792f041d63bc9c9509946e92968fc1ada428
Marek Marecki <marekjm@ozro.pw>
2025-06-27 16:01:55 +0200

    Use BLAKE2b limited to 160 bits for the fingerprint
    BLAKE2b is faster and more secure than SHA1. By default BLAKE2b outputs
    a long digest, but the function is flexible and can be adjusted to
    produce a shorter digest - which we take advantage of to create a
    classic, SHA1-style 160 bit long digest.

--- new/scripts/make_version.sh
+++ new/scripts/make_version.sh
@@ -12,7 +12,7 @@ if [ $(git ls-files -m | wc -l) -ne 0 ]; then
     GIT_DIRTY='-dirty'
 fi
 
-FINGERPRINT=$(cat $(find ./include ./src -type f | sort) | openssl dgst -sha3-256 | cut -d' ' -f2)
+FINGERPRINT=$(cat $(find ./include ./src -type f | sort) | b2sum --length 160 | cut -d' ' -f1)
 
 MODE=${1:-default}
 
