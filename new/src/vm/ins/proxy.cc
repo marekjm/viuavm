f81b8300e866334dcc696d2defbaa5b732df78c8
Marek Marecki <marekjm@ozro.pw>
2025-07-16 21:14:58 +0200

    Catch out-of-bounds access of argument and parameter registers

--- new/src/vm/ins/proxy.cc
+++ new/src/vm/ins/proxy.cc
@@ -17,6 +17,9 @@
  *  along with Viua VM.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <format>
+#include <string>
+#include <string_view>
 
 #include <viua/arch/arch.h>
 #include <viua/support/fdstream.h>
@@ -36,6 +39,43 @@ auto mutable_proxy(
         throw abort_execution{ stack, "dereferences are not implemented" };
     }
 
+    auto at_or_exception = [&stack, i = a.index](std::string_view const rs,
+                                                 auto& v) -> Mutable_proxy
+    {
+        /*
+         * Local register set always has 256 registers, but parameter and
+         * argument sets are different. The capacity of the latter two sets is
+         * determined by the "frame" instruction that allocated them. For
+         * example:
+         *
+         *      frame $2.a
+         *
+         * allocates a frame with 2 (two) registers. From the caller's point of
+         * view these are argument registers ie, they are supposed to be used to
+         * pass arguments (or, actual parameters) to the callee; from the point
+         * of view of the eventual callee, the same set will appear as parameter
+         * registers ie, registers containing formal parameters of the function.
+         *
+         * The above fancy-sounding mumbo-jumbo is all fine and dandy, but what
+         * it means is that both parameter and argument sets is dynamic, not
+         * static, and thus requires a dynamic check. Hence, the nice "if"
+         * below this comment.
+         */
+        if (i >= v.size()) {
+            /*
+             * Whoopsie! This should not have happened.
+             *
+             * FIXME Improve static analysis run by the assembler to catch
+             * out-of-bounds access to argument and parameter registers.
+             */
+            throw abort_execution{
+                stack,
+                std::format("access to nonexistent register ${}.{}", i, rs)
+            };
+        }
+        return { &v.at(i) };
+    };
+
     switch (a.set) {
         using enum viua::arch::REGISTER_SET;
         case VOID:
@@ -43,9 +83,9 @@ auto mutable_proxy(
         case LOCAL:
             return { &stack.frames.back().registers.at(a.index) };
         case PARAMETER:
-            return { &stack.frames.back().parameters.at(a.index) };
+            return at_or_exception("p", stack.frames.back().parameters);
         case ARGUMENT:
-            return { &stack.args.at(a.index) };
+            return at_or_exception("a", stack.args);
         default:
             throw abort_execution{
                 stack, "illegal write access to register " + a.to_string()
