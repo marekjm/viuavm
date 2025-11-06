d3a59d964007eeb96fffb4cfdef7ec7dc85bfab8
Marek Marecki <marekjm@ozro.pw>
2025-11-06 19:30:02 +0100

    Add floating-point arithmetic tests
    I do not have interesting numbers for them, but it is not important,
    since floating-point relies completely on the underlying hardware
    platform - testing edge cases has been handled lower on the stack.
    
    What is interesting on the VM level is proper encoding and decoding of
    instructions, loading of operands (since for doubles there is some
    memory interaction), and the general "handling" of floating points.

---
+++ new/tests/asm/arfp_add_dbl.asm
@@ -0,0 +1,12 @@
+.section ".text"
+
+.symbol [[entry_point]] main
+.label main
+    double $1.l, 2.01
+    double $2.l, 1.13
+
+    add $3.l, $1.l, $2.l
+
+    ebreak
+
+    return void
---
+++ new/tests/asm/arfp_div_flt.asm
@@ -0,0 +1,12 @@
+.section ".text"
+
+.symbol [[entry_point]] main
+.label main
+    float $1.l, 2.01
+    float $2.l, 1.13
+
+    div $3.l, $1.l, $2.l
+
+    ebreak
+
+    return void
