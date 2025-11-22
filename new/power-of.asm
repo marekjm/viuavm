bd5360fbf4090e635c61fa4561916bbb6e8d2c9d
Marek Marecki <marekjm@ozro.pw>
2025-11-22 23:54:11 +0100

    power-of: make the e constant
    I could just hardcode Euler's number, but I can also write a function
    to emit it with a parametrised number of iterations. There is not much
    point to do it, but both factorial() and make_e() are good candidates
    for test cases.

--- new/power-of.asm
+++ new/power-of.asm
@@ -26,11 +26,46 @@
     return $1.l
 
 
+; See https://en.wikipedia.org/wiki/E_(mathematical_constant)
+.symbol make_e_fl
+.label make_e_fl
+    ; This is the accumulator for the sum, and is the return value of the make_e
+    ; function.
+    float $1.l, 1.0
+
+    ; This is the loop counter.
+    li $2.l, 1u
+
+    ; Just for float conversions.
+    float $4.l, 1.0
+
+.label make_e_fl_loop
+    ; Obtain an item in the series: 1.0f / factorial(n)
+    frame $1.a
+    copy $0.a, $2.l
+    call $3.l, factorial
+
+    div $3.l, $4.l, $3.l
+
+    ; Update the accumulator.
+    add $1.l, $1.l, $3.l
+
+    ; Increment the loop counter...
+    addi $2.l, $2.l, 1u
+
+    ; ...and check if the loop should be broken.
+    gt $3.l, $2.l, $0.p
+    not $3.l, $3.l
+    if $3.l, make_e_fl_loop
+
+    return $1.l
+
+
 .symbol [[entry_point]] main
 .label main
     frame $1.a
-    li $0.a, 0
-    call $1.l, factorial
+    li $0.a, 9u
+    call $2.l, make_e_fl
     ebreak
 
     return
