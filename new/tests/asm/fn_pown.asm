3deaddf310d1f93b26bcc3a169e930671c74d18b
Marek Marecki <marekjm@ozro.pw>
2025-12-02 23:03:50 +0100

    Make "pown" be a standard function
    It is a useful one, so it makes sense to make it available to any and
    all tests that may need to call it.

--- new/tests/asm/fn_pown.asm
+++ new/tests/asm/fn_pown.asm
@@ -1,33 +1,7 @@
 .section ".text"
 
 
-.symbol pown
-.label pown
-    if $1.p, pown_exp_nonzero
-    if $0.p, pown_base_nonzero
-
-    addi $0.l, $0.p, 1
-    return $0.l
-
-.label pown_base_nonzero
-    div $0.l, $0.p, $0.p
-    return $0.l
-
-.label pown_exp_nonzero
-    copy $0.l, $0.p
-    li $1.l, 1u
-
-.label pown_loop
-    eq $2.l, $1.l, $1.p
-    if $2.l, pown_epilogue
-
-    mul $0.l, $0.l, $0.p
-    addi $1.l, $1.l, 1u
-
-    if void, pown_loop
-
-.label pown_epilogue
-    return $0.l
+.symbol [[extern]] pown
 
 
 .symbol [[entry_point]] main
