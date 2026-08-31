cabb9c1d9c8fdc7d4a58288b022425b1b3d293fb
Marek Marecki <marekjm@ozro.pw>
2026-08-31 22:46:05 +0200

    Dispatch to both arms of the equation works
    The public interface is the ln() function, which then calls the "proper"
    backend implementation for either of the three cases:
    
     - ln(|-1 + x| < 1)
     - ln(|-1 + x| = 1) ie, ln(2) which is hardcoded
     - ln(|-1 + x| > 1)
    
    What is needed now is the implementation of the != 1 arms.

--- new/ln.asm
+++ new/ln.asm
@@ -1,6 +1,9 @@
 .section ".text"
 
 
+.symbol [[extern]] absr
+
+
 ; ln2: f64
 ;
 ; Natural logarithm of 2, hardcoded.
@@ -14,10 +17,66 @@
     return $0.l
 
 
+; ln: t -> t
+.symbol ln
+.label ln
+    frame $1.a
+    subi $0.a, $0.p, 1
+    call $0.l, absr
+
+    li $1.l, 1
+
+    gt $2.l, $0.l, $1.l
+    if $2.l, ln_of_gt2
+
+    lt $2.l, $0.l, $1.l
+    if $2.l, ln_of_lt2
+
+    frame $0.a
+    call $1.l, ln2
+    return $1.l
+
+.label ln_of_gt2
+    frame $1.a
+    copy $0.a, $0.p
+    call $1.l, lngt2
+    return $1.l
+
+.label ln_of_lt2
+    frame $1.a
+    copy $0.a, $0.p
+    call $1.l, lnlt2
+    return $1.l
+
+
+.symbol lngt2
+.label lngt2
+    double $1.l, 2.0
+    return $1.l
+
+
+.symbol lnlt2
+.label lnlt2
+    double $1.l, -2.0
+    return $1.l
+
+
 .symbol [[entry_point]] main
 .label main
     frame $0.a
     call $1.l, ln2
+
+    frame $1.a
+    double $0.a, 2.0
+    call $2.l, ln
+
+    frame $1.a
+    double $0.a, 3.0
+    call $3.l, ln
+
+    frame $1.a
+    double $0.a, 0.5
+    call $4.l, ln
 
     ebreak
     return void
